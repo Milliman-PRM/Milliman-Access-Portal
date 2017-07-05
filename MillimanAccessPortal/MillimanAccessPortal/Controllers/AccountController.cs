6f6ce9ab9ee0dc6a4d7ff6c451aa122c6c4cdd47
Michael Reisz <michael_reisz@yahoo.com>
2017-07-05 11:13:38 -0400

    Get rid of some unnecessary boilerplate html
    Get rid of some boilerplate and start adding some actual markup that we
    will use.

--- MillimanAccessPortal/MillimanAccessPortal/Controllers/AccountController.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Controllers/AccountController.cs
@@ -141,7 +141,7 @@ namespace MillimanAccessPortal.Controllers
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation(4, "User logged out.");
-            return RedirectToAction(nameof(HomeController.Index), "Home");
+            return RedirectToAction(nameof(LoginController.Index), "Login");
         }
 
         //
@@ -469,7 +469,7 @@ namespace MillimanAccessPortal.Controllers
             }
             else
             {
-                return RedirectToAction(nameof(HomeController.Index), "Home");
+                return RedirectToAction(nameof(LoginController.Index), "Login");
             }
         }
 
