2b369a05cf62e3fb1c6cbbefa0389dc1cd325897
Ben Wyatt <Ben.Wyatt@milliman.com>
2017-08-17 15:15:31 -0400

    Initial CI publishing support

---
+++ Publish/CI.bat
@@ -0,0 +1,84 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET GIT_BRANCH=AddPublishProfile_CI
+SET publishTarget=d:\installedApplications\MAP_CI\%git_branch%
+SET AppPool=MAP_CI_%git_branch%
+
+REM Test build before doing anything else
+REM If this build fails, we don't want to do the following (destructive) steps
+cd MillimanAccessPortal
+dotnet build
+
+if !errorlevel! neq 0 (
+	echo Initial test build failed!
+	echo errorlevel was !errorlevel!
+	exit /b !errorlevel!
+)
+
+REM Stop running application pool
+%windir%\system32\inetsrv\appcmd stop apppool %AppPool%
+
+if !errorlevel! neq 0 (
+	echo Failed to stop running IIS application pool!
+	echo errorlevel was !errorlevel!
+	exit /b !errorlevel!
+)
+
+REM Application database migrations
+cd MillimanAccessPortal
+dotnet restore
+dotnet ef database update
+
+if !errorlevel! neq 0 (
+	echo Failed to update application database!
+	echo errorlevel was !errorlevel!
+	exit /b !errorlevel!
+)
+
+REM Logging database migrations
+cd ../AuditLogLib
+REM dotnet restore
+dotnet ef database update
+
+if !errorlevel! neq 0 (
+	echo Failed to update logging database!
+	echo errorlevel was !errorlevel!
+	exit /b !errorlevel!
+)
+
+cd ../MillimanAccessPortal
+
+REM Build & publish files
+dotnet publish -o %publishTarget% 
+
+if !errorlevel! neq 0 (
+	echo Build failed!
+	echo errorlevel was !errorlevel!
+	exit /b !errorlevel!
+)
+
+REM Run Powershell configuration script
+powershell -file "..\..\Publish\CI_Publish.ps1" -executionPolicy Bypass 
+
+REM Output success or failure
+if !errorlevel! neq 0 (
+	echo Publishing failed!
+	echo errorlevel was !errorlevel!
+	echo Publication error status:
+	type ../error.log
+	dir
+	exit /b !errorlevel!
+)
+
+REM Stop running application pool
+%windir%\system32\inetsrv\appcmd start apppool %AppPool%
+
+if !errorlevel! neq 0 (
+	echo Failed to start IIS application pool!
+	echo errorlevel was !errorlevel!
+	exit /b !errorlevel!
+)
+
+echo Publishing completed successfully
+cd ../../
+type urls.log
---
+++ Publish/CI_Publish.ps1
@@ -0,0 +1,63 @@
+﻿# Code Owners: Ben Wyatt, Steve Gredell
+
+### OBJECTIVE:
+#  Run configuration steps for CI builds of Milliman Access Portal
+
+### DEVELOPER NOTES:
+#  
+
+$branchName = $env:GIT_BRANCH
+
+$branchFolder = "D:\installedapplications\map_ci\$branchName\"
+$outputPath = "D:\installedapplications\map_ci\$branchName\error.log"
+$urlFilePath = "D:\installedapplications\map_ci\$branchName\urls.log"
+$urlBase = "https://indy-qvtest01.milliman.com/"
+$errorCode = 0
+
+import-module webadministration
+
+if ($errorCode -eq 0)
+{
+    # Clear URL file, if it exists
+    set-content -LiteralPath $urlFilePath "Published URL:"
+
+    # (Re-)create applications and log deployed URLs to text file
+    try 
+    {
+        $name = "MAP_CI_$branchName"
+        $appPool = Get-ChildItem -Path IIS:\AppPools | where {$_.name -eq $name}
+
+        $ci_username = $env:ephi_username
+        $ci_password = $env:ephi_password
+
+        # Create branch-specific app pool if it doesn't already exist
+        if (-not $appPool)
+        {
+            $command = "C:\windows\system32\inetsrv\appcmd.exe add apppool /name:$name /managedRuntimeVersion:v4.0"
+            invoke-expression $command 
+
+            # Configuring credentials must be done separately from creating the application pool
+            $command = "C:\windows\system32\inetsrv\appcmd.exe set config /section:applicationPools `"/[name='$name'].processModel.identityType:SpecificUser`" `"/[name='$name'].processModel.userName:$ci_username`" `"/[name='$name'].processModel.password:$ci_password`""
+            invoke-expression $command
+        }
+
+        # Database migrations for application
+
+        # Database migrations for logger
+
+        # If the web application already exists, remove it
+        if ((Get-WebApplication $name).Count -gt 0) { Remove-WebApplication -Name $name -Site "Default Web Site" }
+
+        # Create web application
+        New-WebApplication -Name $name -PhysicalPath $branchFolder -Site "Default Web Site" -ApplicationPool "$name"
+        Set-Content -LiteralPath $urlFilePath ($urlBase + "/" + $name + "/")
+    }
+    catch [Exception]
+    {
+        $_.Exception | format-list -force
+        $errorCode = 1
+    }
+}
+
+# Write out the error code
+Set-Content -LiteralPath $outputPath $errorCode
