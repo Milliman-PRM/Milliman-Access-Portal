66cc03fdaa3c624800d790ddc3b916ff2ab33018
Michael Reisz <michael_reisz@yahoo.com>
2017-09-22 13:58:59 -0400

    Wire up the contact form show/hide toggle
    Get the contact form set up to show and hide from the page.

--- MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/site.js
+++ MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/site.js
@@ -1,1 +1,14 @@
-﻿// Write your Javascript code.
+﻿// Contact Form
+$("#contact-button").click(function () {
+    contactFormToggle();
+})
+
+$("#modal-background").click(function (e) {
+    if (e.target.id === 'modal-background' || e.target.id === 'contact-close') {
+        contactFormToggle();
+    }
+})
+
+function contactFormToggle() {
+    $('#modal-background').toggleClass('hide show');
+}
