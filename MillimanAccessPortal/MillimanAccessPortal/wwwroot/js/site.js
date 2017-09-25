e7ac92dbf8597b0db4b21b72069744897f565421
Michael Reisz <michael_reisz@yahoo.com>
2017-09-25 14:13:48 -0400

    Get the contact form working
    Get the contact form hooked up to the ajax call.  This will need to be
    updated to work with the ultimate method that this calls once
    @ben-wyatt's branch has been merged.

--- MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/site.js
+++ MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/site.js
@@ -36,3 +36,36 @@ function resetContactForm() {
     $('#topic-select').val("");
     $('#body-text').val("");
 }
+
+$(function () {
+    var form = $('#contact-form');
+
+    $(form).submit(function (event) {
+        // Stop the browser from performing its default behavior of submitting the form to make this asynchronous
+        event.preventDefault();
+
+        // Serialize the form data.
+        var formData = $(form).serialize();
+
+        // Submit the form using AJAX.
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+        })
+
+        // Success
+        .done(function(response) {
+            toastr["success"]("Your message has been sent");
+            resetContactForm();
+            contactFormToggle();
+        })
+
+        // Failure
+        .fail(function (response) {
+            toastr["error"]("Your message was unable to be delivered");
+        })
+
+    });
+
+});
