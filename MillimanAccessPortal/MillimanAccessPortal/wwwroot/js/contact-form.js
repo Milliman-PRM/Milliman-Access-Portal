f47b50194b7e6deae8e80c361e121ec65d6ef177
Michael Reisz <Michael.Reisz@milliman.com>
2017-09-26 15:31:46 -0400

    Improve the structure of the view compilation
    Pull out the javascript for the contact form and put it in a section in
    the view that gets rendered in the layout.  Also separate the toastr
    options from the contact form javascript.

--- MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/contact-form.js
+++ MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/contact-form.js
@@ -1,32 +1,13 @@
-﻿// Configure toastr options
-toastr.options = {
-    "closeButton": false,
-    "debug": false,
-    "newestOnTop": false,
-    "progressBar": false,
-    "positionClass": "toast-top-right",
-    "preventDuplicates": false,
-    "onclick": null,
-    "showDuration": "300",
-    "hideDuration": "1000",
-    "timeOut": "5000",
-    "extendedTimeOut": "1000",
-    "showEasing": "swing",
-    "hideEasing": "swing",
-    "showMethod": "show",
-    "hideMethod": "hide"
-}
-
-// Contact Form
+﻿// Contact Form
 $("#contact-button").click(function () {
     contactFormToggle();
-})
+});
 
 $("#modal-background").click(function (e) {
     if (e.target.id === 'modal-background' || e.target.id === 'contact-close') {
         contactFormToggle();
     }
-})
+});
 
 function contactFormToggle() {
     $('#modal-background').toggleClass('hide show');
@@ -54,17 +35,17 @@ $(function () {
             data: formData
         })
 
-        // Success
-        .done(function(response) {
-            toastr["success"]("Your message has been sent");
-            resetContactForm();
-            contactFormToggle();
-        })
+            // Success
+            .done(function (response) {
+                toastr["success"]("Your message has been sent");
+                resetContactForm();
+                contactFormToggle();
+            })
 
-        // Failure
-        .fail(function (response) {
-            toastr["error"]("Your message was unable to be delivered");
-        })
+            // Failure
+            .fail(function (response) {
+                toastr["error"]("Your message was unable to be delivered");
+            });
 
     });
 
