a408017070c16805171ec184b9d327df1a873f65
Michael Reisz <Michael.Reisz@milliman.com>
2017-11-08 10:52:46 -0500

    Reconfigure javascript in client admin
    Move the javascript to it's own script to make it a little cleaner.

---
+++ MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/client-admin.js
@@ -0,0 +1,382 @@
+﻿var clientNodeTemplate = $('script[data-template="clientNode"]').html();
+var clientTree;
+
+function getClientTree() {
+    $.ajax({
+        'type': 'GET',
+        'url': 'ClientAdmin/ClientFamilyList/'
+    }).done(function (response) {
+        clientTree = response.clientTree;
+        renderClientTree();
+    }).fail(function (response) {
+        if (response.status == 401) {
+            toastr['error']('You are not authorized to view any clients');
+        }
+        else {
+            toaster['error']('An error has occurred');
+        }
+    }).always(function () {
+        $('div.client-admin-card').on('click', function () {
+            GetClientDetail($(this));
+        });
+        $('div.card-button-background-edit').on('click', function (event) {
+            EditClientDetail($(this).parents('div[data-client-id]'));
+            event.stopPropagation();
+        });
+        $('div.card-button-background-add').on('click', function (event) {
+            newChildClientFormSetup($(this).parents('div[data-client-id]'));
+            event.stopPropagation();
+        });
+    });
+
+}
+
+function GetClientDetail(clientDiv) {
+
+    removeClientInserts()
+
+    var clientId = clientDiv.attr('data-client-id').valueOf();
+
+    if (clientDiv.hasClass('selected') && !clientDiv.hasClass('editing')) {
+        clearSelectedClient();
+        hideClientForm();
+        return false;
+    }
+
+    $.ajax({
+        type: 'GET',
+        url: 'ClientAdmin/ClientDetail/' + clientId,
+        headers: {
+            'RequestVerificationToken': $("input[name='__RequestVerificationToken']").val()
+        },
+    }).done(function (response) {
+        populateClientDetails(response.clientEntity);
+        console.log(response.assignedUsers);
+        // Change the dom to reflect the selected client
+        clearSelectedClient()
+        clientDiv.addClass('selected');
+        // Show the form in readonly mode
+        makeFormReadOnly();
+        showClientForm();
+
+    }).fail(function (response) {
+        toastr["warning"](response.getResponseHeader("Warning"));
+        hideClientForm();
+    })
+};
+
+function EditClientDetail(clientDiv) {
+
+    removeClientInserts()
+
+    var clientId = clientDiv.attr('data-client-id').valueOf();
+
+    $.ajax({
+        type: 'GET',
+        url: 'ClientAdmin/ClientDetail/' + clientId,
+        headers: {
+            'RequestVerificationToken': $("input[name='__RequestVerificationToken']").val()
+        },
+    }).done(function (response) {
+        populateClientDetails(response.clientEntity);
+        // Change the dom to reflect the selected client
+        clearSelectedClient()
+        clientDiv.addClass('selected');
+        clientDiv.addClass('editing');
+        // Show the form in read/write mode
+        makeFormWriteable();
+        $('#client-form #form-buttons-new').hide();
+        $('#client-form #form-buttons-edit').show();
+        $('#undo-changes-button').hide();
+        showClientForm();
+        $('#client-form :input, #client-form select').on('keyup', function () {
+            if ($(this).value != $(this).attr('data-original-value')) {
+                $('#undo-changes-button').show();
+            }
+        })
+    }).fail(function (response) {
+        toastr["warning"](response.getResponseHeader("Warning"));
+        hideClientForm();
+    })
+};
+
+
+function newClientFormSetup() {
+    removeClientInserts()
+    clearFormData();
+    clearSelectedClient();
+    makeFormWriteable();
+    $('#client-form #form-buttons-edit').hide();
+    $('#client-form #form-buttons-new').show();
+    showClientForm();
+}
+
+function newChildClientFormSetup(parentClientDiv) {
+
+    clearFormData();
+
+    var parentClientId = parentClientDiv.attr('data-client-id').valueOf();
+
+    $('#client-form #parentClientId').val(parentClientId);
+
+    removeClientInserts()
+    parentClientDiv.parent().after('<li class="client-insert">' +
+        '<div class="col-xs-offset-2 col-xs-10 client-admin-card">' +
+        '<div class="client-information">' +
+        '<h4 class="client-admin-card-title">New Child Client</h4>' +
+        '</div >' +
+        '</div>' +
+        '</li>');
+
+    clearSelectedClient();
+    makeFormWriteable();
+    $('#client-form #form-buttons-edit').hide();
+    $('#client-form #form-buttons-new').show();
+    showClientForm();
+}
+
+
+function removeClientInserts() {
+    $('#client-tree li.client-insert').remove();
+}
+
+function clearFormData() {
+    $('#client-form :input, #client-form select').removeAttr('data-original-value');
+    $('#client-form :input, #client-form select').val("");
+}
+
+function clearSelectedClient() {
+    $('#client-tree-list div.selected').removeClass('selected');
+    $('#client-tree-list div.editing').removeClass('editing');
+}
+
+function makeFormReadOnly() {
+    $('#client-form :input').attr('readonly', 'readonly');
+    $('#client-form :input, #client-form select').attr('disabled', 'disabled');
+    $('#client-form #form-buttons-new').hide();
+    $('#client-form #form-buttons-edit').hide();
+}
+
+function makeFormWriteable() {
+    $('#client-form :input').removeAttr('readonly');
+    $('#client-form :input, #client-form select').removeAttr('disabled');
+}
+
+function showClientForm() {
+    var showTime = 50;
+    $('#client-info').show(showTime, function () {
+        $('#client-users').show(showTime);
+    });
+}
+
+function hideClientForm() {
+    $('#client-info').hide();
+    $('#client-users').hide();
+}
+
+function populateClientDetails(clientEntity) {
+    $('#client-form :input, #client-form select').removeAttr('data-original-value');
+    $.each(clientEntity, function (key, value) {
+        var ctrl = $('[name=' + key + ']', '#client-info');
+        if (ctrl.is('select')) {
+            ctrl.val(value).change();
+        }
+        else {
+            ctrl.val(value);
+        }
+        ctrl.attr('data-original-value', value);
+    });
+};
+
+function renderClientTree() {
+    $('#client-tree-list').empty();
+    clientTree.forEach(function (rootClient) {
+        renderClientNode(rootClient, 1);
+        $('#client-tree-list').append('<li class="hr col-xs-12"></li>');
+    });
+};
+
+function renderClientNode(client, level) {
+    var template = clientNodeTemplate;
+
+    switch (level) {
+        case 1:
+            template = template.replace(/{{class}}/g, "col-xs-12");
+            break;
+        case 2:
+            template = template.replace(/{{class}}/g, "col-xs-offset-1 col-xs-11");
+            break;
+        default:
+            template = template.replace(/{{class}}/g, "col-xs-offset-2 col-xs-10");
+            break;
+    }
+
+    template = template.replace(/{{header-level}}/g, (level + 1))
+    template = template.replace(/{{id}}/g, client.clientEntity.id);
+    template = template.replace(/{{name}}/g, client.clientEntity.name);
+    if (client.clientEntity.clientCode) {
+        template = template.replace(/{{clientCode}}/g, client.clientEntity.clientCode);
+    }
+    else {
+        template = template.replace(/{{clientCode}}/g, '');
+    }
+    template = template.replace(/{{users}}/g, client.associatedUserCount);
+    template = template.replace(/{{content}}/g, client.associatedContentCount);
+
+
+    // convert template to DOM element for jQuery manipulation
+    var $template = $(template.toString());
+
+    if (!client.canManage) {
+        $('.icon-container', $template).remove();
+        $('.client-admin-card', $template).addClass('disabled');
+    }
+
+    if (client.children.length != 0) {  // Only include the delete button on client nodes without children
+        $('.card-button-background-delete', $template).remove();
+    }
+
+    if (level == 3) {  // Don't include the add child client button on lowest level
+        $('.card-button-background-add', $template).remove();
+    }
+
+    $('#client-tree-list').append($template);
+
+    // Render child nodes
+    if (client.children.length) {
+        client.children.forEach(function (childNode) {
+            renderClientNode(childNode, level + 1);
+        })
+    }
+
+};
+
+function deleteClient(event, id, name) {
+
+    event.stopPropagation();
+
+    bootbox.confirm({
+        title: "Delete " + name + "?",
+        message: "This action can not be undone.  Do you wish to proceed?",
+        className: 'screen-center',
+        backdrop: true,
+        onEscape: true,
+        buttons: {
+            confirm: {
+                label: '<i class="fa fa-check"></i> Confirm',
+                className: 'primary-button btn-danger'
+            },
+            cancel: {
+                label: 'Cancel',
+                className: 'btn-link'
+            }
+        },
+        callback: function (result) {
+            if (result) {
+                bootbox.prompt({
+                    title: "Please provide your password to proceed with deletion",
+                    className: 'screen-center',
+                    inputType: 'password',
+                    backdrop: true,
+                    onEscape: true,
+                    buttons: {
+                        confirm: {
+                            label: '<i class="fa fa-trash"></i> DELETE',
+                            className: 'primary-button btn-danger'
+                        },
+                        cancel: {
+                            label: 'Cancel',
+                            className: 'btn-link'
+                        }
+                    },
+                    callback: function (result) {
+                        if (result) {
+                            removeClientNode(id, name, result);
+                        }
+                        else if (result == "") {
+                            toastr['warning']("Please enter a password");
+                            return false;
+                        }
+                        else {
+                            toastr['warning']("Deletion was canceled");
+
+                        }
+                    }
+                })
+
+                $('.bootbox-input-password').on('keyup', function (e) {
+                    if (e.which === 13) {
+                        $('button[data-bb-handler="confirm"]').trigger('click');
+                    }
+                });
+            }
+        }
+    });
+
+};
+
+function removeClientNode(clientId, clientName, password) {
+    $.ajax({
+        type: 'DELETE',
+        url: 'ClientAdmin/DeleteClient',
+        data: {
+            Id: clientId,
+            Password: password
+        },
+        headers: {
+            'RequestVerificationToken': $("input[name='__RequestVerificationToken']").val()
+        },
+    }).done(function (response) {
+        recursiveClientNodeSearch(clientTree, clientId);
+        renderClientTree();
+        toastr['success'](clientName + " was successfully deleted.");
+    }).fail(function (response) {
+        toastr["warning"](response.getResponseHeader("Warning"));
+    })
+};
+
+function recursiveClientNodeSearch(array, clientId) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].clientEntity.id == clientId) {
+            array.splice(i, 1);
+            return;
+        }
+        if (array[i].children.length > 0) {
+            recursiveClientNodeSearch(array[i].children, clientId);
+        }
+    }
+}
+
+function searchClientTree(searchString) {
+    var searchString = searchString.toUpperCase();
+    var nodes = document.getElementById('client-tree-list').getElementsByTagName('li');
+    var hrSwitch = 0;
+
+    for (i = 0; i < nodes.length; i++) {
+        var title, clientCode;
+        if (nodes[i].getElementsByClassName('client-admin-card-title').length > 0) {
+            title = nodes[i].getElementsByClassName('client-admin-card-title')[0];
+            clientCode = nodes[i].getElementsByClassName('client-admin-card-clientcode')[0];
+            if (title || clientCode) {
+                if (title.innerHTML.toUpperCase().indexOf(searchString) > -1 ||
+                    clientCode.innerHTML.toUpperCase().indexOf(searchString) > -1) {
+                    nodes[i].style.display = "";
+                    hrSwitch = 1;
+                }
+                else {
+                    nodes[i].style.display = "none";
+                }
+            }
+        }
+        else {
+            if (hrSwitch == 0) {
+                nodes[i].style.display = "none";
+            }
+            else {
+                nodes[i].style.display = "";
+
+            }
+            hrSwitch = 0;
+        }
+    }
+}
