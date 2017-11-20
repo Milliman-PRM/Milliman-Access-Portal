0cfc1a513520d3c3c01c64f7fd1e5af5b4a4cb9a
Ben Wyatt <Ben.Wyatt@milliman.com>
2017-11-20 10:08:55 -0500

    Add AzureKeyVaultLib project

---
+++ MillimanAccessPortal/AzureKeyVaultLib/AzureKeyVaultClient.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureKeyVaultLib
+{
+    class AzureKeyVaultClient
+    {
+        public ClientAssertionCertificate AssertionCert { get; set; }
+        public string CertificateThumbprint { get; set; }
+        public string ClientId { get; set; }
+
+        public AzureKeyVaultClient(string _thumbprint, string _clientId)
+        {
+            var clientAssertionCertPfx = CertificateHelper.FindCertificateByThumbprint(CertificateThumbprint);
+            AssertionCert = new ClientAssertionCertificate(ClientId, clientAssertionCertPfx);
+        }
+        
+        public async Task<string> GetAccessToken(string authority, string resource, string scope)
+        {
+            var context = new AuthenticationContext(authority, TokenCache.DefaultShared);
+            var result = await context.AcquireTokenAsync(resource, AssertionCert);
+
+            return result.AccessToken;
+        }
+
+        public async Task<string> GetNamedSecret(string secretURL)
+        {
+            var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetAccessToken));
+            var sec = await kv.GetSecretAsync(secretURL);
+
+            return sec.Value;
+        }
+    }
+
+ }
---
+++ MillimanAccessPortal/AzureKeyVaultLib/CertificateHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AzureKeyVaultLib
+{
+    public class CertificateHelper
+    {
+        public static X509Certificate2 FindCertificateByThumbprint(string findValue)
+        {
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            try
+            {
+                store.Open(OpenFlags.ReadOnly);
+                X509Certificate2Collection col = store.Certificates.Find(X509FindType.FindByThumbprint,
+                    findValue, false); // Don't validate certs, since the test root isn't installed.
+                if (col == null || col.Count == 0)
+                    return null;
+                return col[0];
+            }
+            finally
+            {
+                store.Close();
+            }
+        }
+    }
+}
