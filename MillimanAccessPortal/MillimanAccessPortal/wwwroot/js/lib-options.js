a88694d20ddb675c35435df5abd94e6336167b46
Michael Reisz <Michael.Reisz@milliman.com>
2017-11-21 12:53:48 -0500

    Improve client side email validation
    The default client side validation of email in jqeury validate is pretty bad.  This makes the definition much more strict.

--- MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/lib-options.js
+++ MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/lib-options.js
@@ -17,3 +17,7 @@ toastr.options = {
     "hideMethod": "hide"
 };
 
+// Configure jquery validation overrides
+$.validator.methods.email = function (value, element) {
+    return this.optional(element) || /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value);
+}
