15fca45d0309cbb81814a53d8a91e30427c51744
Ben Wyatt <Ben.Wyatt@milliman.com>
2017-11-28 16:00:32 -0500

    First swing at AzureKeyVault configuration provider
    The certificate is retrieved successfully via this method, but the application is crashing during startup. The error seems to be related to authenticating to Azure Active Directory

--- MillimanAccessPortal/MillimanAccessPortal/Program.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Program.cs
@@ -2,8 +2,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.DependencyInjection;
 using MapDbContextLib.Context;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 
 namespace MillimanAccessPortal
 {
@@ -47,6 +53,30 @@ namespace MillimanAccessPortal
                     .AddJsonFile("smtp.json", optional: false, reloadOnChange: true)
                     .AddJsonFile($"smtp.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
                     ;
+
+                    #region Configure Azure Key Vault
+
+                    // The configuration provider doesn't provide access to values before Build() is executed below. 
+                    // As a result, we need to pull in the AzureKeyVault.json ourselves & parse the values here.
+                    
+                    string json = File.ReadAllText(@"AzureKeyVault.json");
+
+                    Dictionary<string, string> azureKeyVaultConfig = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+                    var store = new X509Store(StoreLocation.LocalMachine);
+                    store.Open(OpenFlags.ReadOnly);
+                    var cert = store.Certificates.Find(X509FindType.FindByThumbprint, azureKeyVaultConfig["AzureCertificateThumbprint"], false);
+
+                    X509Certificate2 certObject = cert.OfType<X509Certificate2>().Single();
+
+                    config.AddAzureKeyVault(
+                        azureKeyVaultConfig["AzureVaultName"],
+                        azureKeyVaultConfig["AzureClientID"],
+                        certObject,
+                        new DefaultKeyVaultSecretManager()
+                        );
+                    
+                    #endregion
 
                     if (hostContext.HostingEnvironment.IsDevelopment())
                     {
