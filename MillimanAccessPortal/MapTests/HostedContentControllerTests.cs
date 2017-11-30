ada0e9f85a30bd088f3f7881a95285372a2708e1
tom.puckett <tom.puckett@milliman.com>
2017-11-30 11:24:06 -0500

    Removed some unused code

--- MillimanAccessPortal/MapTests/HostedContentControllerTests.cs
+++ MillimanAccessPortal/MapTests/HostedContentControllerTests.cs
@@ -1,41 +1,21 @@
 ﻿/*
- * CODE OWNERS: Ben Wyatt
+ * CODE OWNERS: Ben Wyatt, Tom Puckett
  * OBJECTIVE: Perform unit tests against HostedContentController
  * DEVELOPER NOTES: <What future developers need to know.>
  */
 using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
-using System.Security.Claims;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authorization.Infrastructure;
-using Moq;
 using Xunit;
-using MapDbContextLib.Context;
-using MapDbContextLib.Identity;
 using MillimanAccessPortal.Controllers;
-using MillimanAccessPortal.DataQueries;
 using MillimanAccessPortal.Models.HostedContentViewModels;
-using MillimanAccessPortal.Authorization;
 
 namespace MapTests
 {
     public class HostedContentControllerTests
     {
-        /// <summary>
-        /// Constructor sets up requirements and data for the controller being tested.
-        /// </summary>
-        public HostedContentControllerTests()
-        {}
-
         /// <summary>
         /// Test that the Index returns a view containing a list of content items the test user can access.
         /// </summary>
@@ -43,8 +23,6 @@ namespace MapTests
         [Fact]
         public void Index_ReturnsAViewResult()
         {
-            // Reference https://msdn.microsoft.com/en-us/library/dn314429(v=vs.113).aspx
-
             #region Arrange
             // initialize dependencies
             TestInitialization TestResources = new TestInitialization();
@@ -116,73 +94,6 @@ namespace MapTests
             throw new NotImplementedException();
         }
 
-        public Mock<IAuthorizationService> AuthorizationServiceMockExtensionFactory<T1>(AuthorizationResult Result1) 
-            where T1 : MapAuthorizationRequirementBase
-        {
-            var mockRepository = new Moq.MockRepository(Moq.MockBehavior.Strict);
-            var mockFactory = mockRepository.Create<IAuthorizationService>();
-            var ClaimsPrincipal = mockRepository.Create<ClaimsPrincipal>();
-            mockFactory.Setup(x => x.AuthorizeAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<object>(), It.IsAny<T1>())).ReturnsAsync(Result1);
-            return mockFactory;
-        }
-
-        /// <summary>
-        /// To be used where the handler is expected to service 2 separate requirement types in the system under test
-        /// </summary>
-        /// <typeparam name="T1"></typeparam>
-        /// <typeparam name="T2"></typeparam>
-        /// <param name="Result1"></param>
-        /// <param name="Result2"></param>
-        /// <returns></returns>
-        public static Mock<IAuthorizationService> AuthorizationServiceMockExtensionFactory<T1,T2>(AuthorizationResult Result1, AuthorizationResult Result2) 
-            where T1 : MapAuthorizationRequirementBase
-            where T2 : MapAuthorizationRequirementBase
-        {
-            var mockRepository = new Moq.MockRepository(Moq.MockBehavior.Strict);
-            var mockFactory = mockRepository.Create<IAuthorizationService>();
-            var ClaimsPrincipal = mockRepository.Create<ClaimsPrincipal>();
-            mockFactory.Setup(x => x.AuthorizeAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<object>(), It.IsAny<T1>())).ReturnsAsync(Result1);
-            mockFactory.Setup(x => x.AuthorizeAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<object>(), It.IsAny<T2>())).ReturnsAsync(Result2);
-            return mockFactory;
-        }
-
-        public Mock<IAuthorizationService> AuthorizationServiceMockExtensionFactory_Success()
-        {
-            var mockRepository = new Moq.MockRepository(Moq.MockBehavior.Strict);
-            var mockFactory = mockRepository.Create<IAuthorizationService>();
-            var ClaimsPrincipal = mockRepository.Create<ClaimsPrincipal>();
-            mockFactory.Setup(x => x.AuthorizeAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<object>(), It.IsAny<IAuthorizationRequirement>())).ReturnsAsync(AuthorizationResult.Success());
-            return mockFactory;
-        }
-
-        public Mock<IAuthorizationService> AuthorizationServiceMockExtensionFactory_Failure()
-        {
-            var mockRepository = new Moq.MockRepository(Moq.MockBehavior.Strict);
-            var mockFactory = mockRepository.Create<IAuthorizationService>();
-            var ClaimsPrincipal = mockRepository.Create<ClaimsPrincipal>();
-            mockFactory.Setup(x => x.AuthorizeAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<object>(), It.IsAny<IAuthorizationRequirement>())).ReturnsAsync(AuthorizationResult.Failed());
-            return mockFactory;
-        }
-
-        public Mock<IAuthorizationService> AuthorizationServiceMockExtensionFactory_RequirementList_Success()
-        {
-            var mockRepository = new Moq.MockRepository(Moq.MockBehavior.Strict);
-            var mockFactory = mockRepository.Create<IAuthorizationService>();
-            var ClaimsPrincipal = mockRepository.Create<ClaimsPrincipal>();
-            mockFactory.Setup(x => x.AuthorizeAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<object>(), It.IsAny<IEnumerable<MapAuthorizationRequirementBase>>())).ReturnsAsync(AuthorizationResult.Success());
-            return mockFactory;
-        }
-
-        public Mock<IAuthorizationService> AuthorizationServiceMockExtensionFactory_RequirementList_Failure()
-        {
-            var mockRepository = new Moq.MockRepository(Moq.MockBehavior.Strict);
-            var mockFactory = mockRepository.Create<IAuthorizationService>();
-            var ClaimsPrincipal = mockRepository.Create<ClaimsPrincipal>();
-            mockFactory.Setup(x => x.AuthorizeAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<object>(), It.IsAny<IEnumerable<MapAuthorizationRequirementBase>>())).ReturnsAsync(AuthorizationResult.Failed());
-            return mockFactory;
-        }
     }
 }
 
-#if false
-#endif
