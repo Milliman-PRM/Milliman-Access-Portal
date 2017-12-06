ba9bfa23f0ae1a52958b96e718ca6a85436b1e07
tom.puckett <tom.puckett@milliman.com>
2017-12-06 11:12:19 -0500

    Updated generic type list in UserStore mock.

--- MillimanAccessPortal/MapTests/MockUserStore.cs
+++ MillimanAccessPortal/MapTests/MockUserStore.cs
@@ -18,14 +18,14 @@ namespace MapTests
         internal static Mock<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, long>> New(Mock<ApplicationDbContext> Context)
         {
             //Mock<IUserStore<ApplicationUser>> NewStore = new Mock<IUserStore<ApplicationUser>>();
-            Mock<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, long>> NewStore = new Mock<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, long>>();
+            Mock<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, long>> NewStore = new Mock<UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, long>>() {CallBase=true };
 
             // Setup mocked object methods to interact with persisted data
             NewStore.Setup(d => d.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<CancellationToken>())).Callback<ApplicationUser, CancellationToken>((au, ct) => Context.Object.ApplicationUser.Add(au));
-            NewStore.Setup(d => d.FindByIdAsync(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync<string, CancellationToken, IUserStore<ApplicationUser>, ApplicationUser>((id, ct) => Context.Object.ApplicationUser.SingleOrDefault(au => au.Id == long.Parse(id)));
-            NewStore.Setup(d => d.FindByNameAsync(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync<string, CancellationToken, IUserStore<ApplicationUser>, ApplicationUser>((nm, ct) => Context.Object.ApplicationUser.SingleOrDefault(au => au.UserName == nm));
+            NewStore.Setup(d => d.FindByIdAsync(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync<string, CancellationToken, UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, long>, ApplicationUser>((id, ct) => Context.Object.ApplicationUser.SingleOrDefault(au => au.Id == long.Parse(id)));
+            NewStore.Setup(d => d.FindByNameAsync(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync<string, CancellationToken, UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, long>, ApplicationUser>((nm, ct) => Context.Object.ApplicationUser.SingleOrDefault(au => au.UserName == nm));
 
-            NewStore.Setup(d => d.GetUsersForClaimAsync(It.IsAny<Claim>(), It.IsAny<CancellationToken>())).ReturnsAsync<Claim>((Claim claim) => 
+            NewStore.Setup(d => d.GetUsersForClaimAsync(It.IsAny<Claim>(), It.IsAny<CancellationToken>())).ReturnsAsync<Claim, UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, long>, IList<ApplicationUser>>((Claim claim) => 
                                             Context.Object.UserClaims.Join(
                                             Context.Object.Users,
                                             cl => cl.UserId,
