22cc6e368598522d5806edc5edc63febbac9c372
Ben Wyatt <Ben.Wyatt@milliman.com>
2017-12-22 10:58:30 -0500

    Define global regex strings for email and domain

--- MillimanAccessPortal/MapCommonLib/GlobalFunctions.cs
+++ MillimanAccessPortal/MapCommonLib/GlobalFunctions.cs
@@ -8,8 +8,11 @@ namespace MapCommonLib
 {
     public static class GlobalFunctions
     {
-        static Regex EmailAddressValidationRegex = new Regex (@"^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$",
-                                                                RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+
+        public static string emailValRegex = @"^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$";
+        public static string domainValRegex = @"^[A-Za-z0-9.-]+\.[A-Za-z]{2,}$";
+
+        static Regex EmailAddressValidationRegex = new Regex (emailValRegex, RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
 
         public static bool IsValidEmail(string TestAddress)
         {
