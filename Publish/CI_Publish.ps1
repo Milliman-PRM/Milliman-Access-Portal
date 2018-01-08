5c00c6439144469c1d51428643e592aae24ce3e7
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-01-08 17:09:49 -0500

    Qualify MSBuild with full path
    This could (should probably) be resolved by adding the MSBuild directory to the CI machine path

--- Publish/CI_Publish.ps1
+++ Publish/CI_Publish.ps1
@@ -46,7 +46,7 @@ cd MillimanAccessPortal\MillimanAccessPortal
 
 log_statement "Test build before publishing"
 # If this build fails, we don't want to do the subsequent (destructive) steps
-MSBuild /restore
+C:\'Program Files (x86)'\'Microsoft Visual Studio'\2017\Professional\MSBuild\15.0\Bin\MSBuild /restore
 
 if ($LASTEXITCODE -ne 0) {
     log_statement "ERROR: Initial package restore failed"
@@ -63,7 +63,7 @@ if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
 }
 
-MSBuild /target:Clean
+C:\'Program Files (x86)'\'Microsoft Visual Studio'\2017\Professional\MSBuild\15.0\Bin\MSBuild /target:Clean
 
 if ( $LASTEXITCODE -ne 0 ) {
     log_statement "ERROR: Initial test build failed"
