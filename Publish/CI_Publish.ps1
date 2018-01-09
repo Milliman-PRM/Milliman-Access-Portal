5c8b8357ff16fe6fd2811d5dce4f30e2ae51455d
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-01-09 10:02:13 -0500

    Add OutDir parameter to publish

--- Publish/CI_Publish.ps1
+++ Publish/CI_Publish.ps1
@@ -228,7 +228,7 @@ if ($LASTEXITCODE -ne 0) {
 cd ../MillimanAccessPortal
 
 log_statement "Build and publish application files"
-C:\'Program Files (x86)'\'Microsoft Visual Studio'\2017\Professional\MSBuild\15.0\Bin\MSBuild /t:publish
+C:\'Program Files (x86)'\'Microsoft Visual Studio'\2017\Professional\MSBuild\15.0\Bin\MSBuild /t:publish /p:OutDir=$branchFolder\
 
 if ($LASTEXITCODE -ne 0) {
     log_statement "Build failed"
