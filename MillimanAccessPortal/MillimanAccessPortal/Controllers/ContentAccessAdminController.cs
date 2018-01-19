704721057b0646c79a71ac0a6f82f7b18a53ef2d
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-01-19 15:40:54 -0500

    Add action stubs
    The stubs include descriptions, Post/Get markers, preliminary parameters, and empty returns.

--- MillimanAccessPortal/MillimanAccessPortal/Controllers/ContentAccessAdminController.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Controllers/ContentAccessAdminController.cs
@@ -54,5 +54,143 @@ namespace MillimanAccessPortal.Controllers
             Queries = QueriesArg;
             UserManager = UserManagerArg;
         }
+
+        /// <summary>Action for content access administration index.</summary>
+        /// <returns>ViewResult</returns>
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            #region Authorization
+            #endregion
+
+            #region Validation
+            #endregion
+
+            return View();
+        }
+
+        /// <summary>Returns the list of client families visible to the user.</summary>
+        /// <returns>JsonResult</returns>
+        [HttpGet]
+        public async Task<IActionResult> ClientFamilyList()
+        {
+            #region Authorization
+            #endregion
+
+            #region Validation
+            #endregion
+
+            return Json(new { });
+        }
+
+        /// <summary>Returns the root content items available to a client.</summary>
+        /// <param name="ClientId">The client whose root content items are to be returned.</param>
+        /// <returns>JsonResult</returns>
+        [HttpGet]
+        public async Task<IActionResult> RootContentItems(long? ClientId)
+        {
+            #region Authorization
+            #endregion
+
+            #region Validation
+            #endregion
+
+            return Json(new { });
+        }
+
+        /// <summary>Returns the report groups associated with a root content item.</summary>
+        /// <param name="ClientId">The client associated with the root content item.</param>
+        /// <param name="RootContentItemId">The root content item whose report groups are to be returned.</param>
+        /// <returns>JsonResult</returns>
+        [HttpGet]
+        public async Task<IActionResult> ReportGroups(long? ClientId, long? RootContentItemId)
+        {
+            #region Authorization
+            #endregion
+
+            #region Validation
+            #endregion
+
+            return Json(new { });
+        }
+
+        /// <summary>Creates a report group.</summary>
+        /// <param name="ClientId">The client to be assigned to the new report group.</param>
+        /// <param name="RootContentItemId">The root content item to be assigned to the new report group.</param>
+        /// <param name="ReportGroupName">The name of the new report group.</param>
+        /// <returns>JsonResult</returns>
+        [HttpPost]
+        public async Task<IActionResult> CreateReportGroup(long? ClientId, long? RootContentItemId, String ReportGroupName)
+        {
+            #region Authorization
+            #endregion
+
+            #region Validation
+            #endregion
+
+            return Json(new { });
+        }
+
+        /// <summary>Updates the users assigned to a report group.</summary>
+        /// <param name="ReportGroupId">The report group to be updated.</param>
+        /// <param name="Users">The users to be applied to the report group.</param>
+        /// <returns>JsonResult</returns>
+        [HttpPost]
+        public async Task<IActionResult> UpdateReportGroup(long? ReportGroupId, object Users)
+        {
+            #region Authorization
+            #endregion
+
+            #region Validation
+            #endregion
+
+            return Json(new { });
+        }
+
+        /// <summary>Deletes a report group.</summary>
+        /// <param name="ReportGroupId">The report group to be deleted.</param>
+        /// <returns>JsonResult</returns>
+        [HttpPost]
+        public async Task<IActionResult> DeleteReportGroup(long? ReportGroupId)
+        {
+            #region Authorization
+            #endregion
+
+            #region Validation
+            #endregion
+
+            return Json(new { });
+        }
+
+        /// <summary>Returns the selections associated with a report group.</summary>
+        /// <param name="ReportGroupId">The report group whose selections are to be returned.</param>
+        /// <returns>JsonResult</returns>
+        [HttpGet]
+        public async Task<IActionResult> Selections(long? ReportGroupId)
+        {
+            #region Authorization
+            #endregion
+
+            #region Validation
+            #endregion
+
+            return Json(new { });
+        }
+
+        /// <summary>Updates a report group with new selections.</summary>
+        /// <param name="ReportGroupId">The report group whose selections are to be updated.</param>
+        /// <param name="Selections">The selections to be applied to the report group.</param>
+        /// <returns>JsonResult</returns>
+        [HttpPost]
+        public async Task<IActionResult> UpdateSelections(long? ReportGroupId, object Selections)
+        {
+            #region Authorization
+            #endregion
+
+            #region Validation
+            #endregion
+
+            return Json(new { });
+        }
     }
 }
