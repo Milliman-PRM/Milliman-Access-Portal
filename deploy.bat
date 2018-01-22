405e93b6c0a5b069da4520045a65eb39eb394036
Ben Wyatt <Ben.Wyatt@milliman.com>
2018-01-22 13:28:56 -0500

    Add csproj path to msbuild command

--- deploy.bat
+++ deploy.bat
@@ -76,7 +76,7 @@ IF !ERRORLEVEL! NEQ 0 goto error
 :: 2. Build and publish
 echo Publishing site to temp folder with MSBuild
 rem call :ExecuteCmd dotnet publish "%DEPLOYMENT_SOURCE%\MillimanAccessPortal\MillimanAccessPortal\MillimanAccessPortal.csproj" --output "%DEPLOYMENT_TEMP%" --configuration Release
-call :ExecuteCmd MSBuild /t:Restore /t:publish /p:PublishDir=%DEPLOYMENT_TEMP% /verbosity:minimal
+call :ExecuteCmd MSBuild "%DEPLOYMENT_SOURCE%\MillimanAccessPortal\MillimanAccessPortal\MillimanAccessPortal.csproj" /t:Restore /t:publish /p:PublishDir=%DEPLOYMENT_TEMP% /verbosity:minimal
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 3. KuduSync
