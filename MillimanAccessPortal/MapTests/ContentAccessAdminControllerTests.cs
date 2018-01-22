2e4a3b15f003ce6c54e5b6b5ddbf939f19d44c2b
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-01-22 10:10:28 -0500

    Add basic tests

--- MillimanAccessPortal/MapTests/ContentAccessAdminControllerTests.cs
+++ MillimanAccessPortal/MapTests/ContentAccessAdminControllerTests.cs
@@ -1,10 +1,190 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using MillimanAccessPortal.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace MapTests
 {
-    class ContentAccessAdminControllerTests
+    public class ContentAccessAdminControllerTests
     {
+        internal TestInitialization TestResources { get; set; }
+
+        /// <summary>
+        /// Constructor is called for each test execution
+        /// </summary>
+        public ContentAccessAdminControllerTests()
+        {
+            TestResources = new TestInitialization();
+            TestResources.GenerateTestData(new DataSelection[] { DataSelection.Basic });
+        }
+
+        /// <summary>Common controller constructor to be used by all tests</summary>
+        /// <param name="UserName"></param>
+        /// <returns>ContentAccessAdminController</returns>
+        public async Task<ContentAccessAdminController> GetControllerForUser(string UserName)
+        {
+            ContentAccessAdminController testController = new ContentAccessAdminController(
+                TestResources.DbContextObject,
+                TestResources.AuditLoggerObject,
+                TestResources.AuthorizationService,
+                TestResources.LoggerFactory,
+                TestResources.QueriesObj,
+                TestResources.UserManagerObject
+                );
+
+            // Generating ControllerContext will throw a NullReferenceException if the provided user does not exist
+            testController.ControllerContext = TestInitialization.GenerateControllerContext(UserAsUserName: (await TestResources.UserManagerObject.FindByNameAsync(UserName)).UserName);
+            testController.HttpContext.Session = new MockSession();
+
+            return testController;
+        }
+
+        [Fact]
+        public async Task Index_ReturnsView()
+        {
+            #region Arrange
+            ContentAccessAdminController controller = await GetControllerForUser("ClientAdmin1");
+            #endregion
+
+            #region Act
+            var view = await controller.Index();
+            #endregion
+
+            #region Assert
+            Assert.IsType<ViewResult>(view);
+            #endregion
+        }
+
+        [Fact]
+        public async Task ClientFamilyList_ReturnsJson()
+        {
+            #region Arrange
+            ContentAccessAdminController controller = await GetControllerForUser("ClientAdmin1");
+            #endregion
+
+            #region Act
+            var view = await controller.ClientFamilyList();
+            #endregion
+
+            #region Assert
+            Assert.IsType<JsonResult>(view);
+            #endregion
+        }
+
+        [Fact]
+        public async Task RootContentItems_ReturnsJson()
+        {
+            #region Arrange
+            ContentAccessAdminController controller = await GetControllerForUser("ClientAdmin1");
+            #endregion
+
+            #region Act
+            var view = await controller.RootContentItems(0);
+            #endregion
+
+            #region Assert
+            Assert.IsType<JsonResult>(view);
+            #endregion
+        }
+
+        [Fact]
+        public async Task ReportGroups_ReturnsJson()
+        {
+            #region Arrange
+            ContentAccessAdminController controller = await GetControllerForUser("ClientAdmin1");
+            #endregion
+
+            #region Act
+            var view = await controller.ReportGroups(0, 0);
+            #endregion
+
+            #region Assert
+            Assert.IsType<JsonResult>(view);
+            #endregion
+        }
+
+        [Fact]
+        public async Task CreateReportGroup_ReturnsJson()
+        {
+            #region Arrange
+            ContentAccessAdminController controller = await GetControllerForUser("ClientAdmin1");
+            #endregion
+
+            #region Act
+            var view = await controller.CreateReportGroup(0, 0, "");
+            #endregion
+
+            #region Assert
+            Assert.IsType<JsonResult>(view);
+            #endregion
+        }
+
+        [Fact]
+        public async Task UpdateReportGroup_ReturnsJson()
+        {
+            #region Arrange
+            ContentAccessAdminController controller = await GetControllerForUser("ClientAdmin1");
+            #endregion
+
+            #region Act
+            var view = await controller.UpdateReportGroup(0, null);
+            #endregion
+
+            #region Assert
+            Assert.IsType<JsonResult>(view);
+            #endregion
+        }
+
+        [Fact]
+        public async Task DeleteReportGroup_ReturnsJson()
+        {
+            #region Arrange
+            ContentAccessAdminController controller = await GetControllerForUser("ClientAdmin1");
+            #endregion
+
+            #region Act
+            var view = await controller.DeleteReportGroup(0);
+            #endregion
+
+            #region Assert
+            Assert.IsType<JsonResult>(view);
+            #endregion
+        }
+
+        [Fact]
+        public async Task Selections_ReturnsJson()
+        {
+            #region Arrange
+            ContentAccessAdminController controller = await GetControllerForUser("ClientAdmin1");
+            #endregion
+
+            #region Act
+            var view = await controller.Selections(0);
+            #endregion
+
+            #region Assert
+            Assert.IsType<JsonResult>(view);
+            #endregion
+        }
+
+        [Fact]
+        public async Task UpdateSelections_ReturnsJson()
+        {
+            #region Arrange
+            ContentAccessAdminController controller = await GetControllerForUser("ClientAdmin1");
+            #endregion
+
+            #region Act
+            var view = await controller.UpdateSelections(0, null);
+            #endregion
+
+            #region Assert
+            Assert.IsType<JsonResult>(view);
+            #endregion
+        }
+
     }
 }
