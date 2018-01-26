05e6c67086408c8c9fbe3385fa270117f1cedc6d
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-01-26 12:53:09 -0500

    Create root content item view models

---
+++ MillimanAccessPortal/MillimanAccessPortal/Models/ContentAccessAdminViewModels/ContentAccessAdminRootContentItemListViewModel.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * CODE OWNERS: Joseph Sweeney
+ * OBJECTIVE:
+ * DEVELOPER NOTES:
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MapCommonLib;
+using MapDbContextLib.Context;
+using MapDbContextLib.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace MillimanAccessPortal.Models.ContentAccessAdminViewModels
+{
+    public class ContentAccessAdminRootContentItemListViewModel
+    {
+        public List<ContentAccessAdminRootContentItemDetailViewModel> RootContentItemList;
+        public long RelevantRootContentItemId { get; set; } = -1;
+    }
+}
