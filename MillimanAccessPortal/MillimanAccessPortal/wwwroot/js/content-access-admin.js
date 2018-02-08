72a63073ced4d9030d8c5c63f0cfe61446ed3eb8
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-02-08 10:59:45 -0500

    Add selection group list

--- MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/content-access-admin.js
+++ MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/content-access-admin.js
@@ -6,22 +6,32 @@ var ajaxStatus = {
 var SHOW_DURATION = 50;
 
 var clientCardClickHandler;
-
+var rootContentItemCardClickHandler;
 
 // Helper functions (TODO: consider moving to separate file)
 
 function clearClientSelection() {
   $('#client-tree .card-container').removeAttr('editing selected');
 }
+function clearRootContentItemSelection() {
+  $('#root-content .card-container').removeAttr('editing selected');
+}
 
 function showClientDetails() {
   // TODO: consider using nested divs for better hiding
   $('#root-content').show(SHOW_DURATION);
 }
+function showRootContentItemDetails() {
+  $('#selection-groups').show(SHOW_DURATION);
+}
 
 function hideClientDetails() {
   // TODO: consider using nested divs for better hiding
   $('#root-content').hide(SHOW_DURATION);
+  $('#selection-groups').hide(SHOW_DURATION);
+  $('#selections').hide(SHOW_DURATION);
+}
+function hideRootContentItemDetails() {
   $('#selection-groups').hide(SHOW_DURATION);
   $('#selections').hide(SHOW_DURATION);
 }
@@ -31,6 +41,12 @@ function openClientCard($clientCard) {
   $clientCard.attr('selected', '');
   getRootContentItemList($clientCard);
   showClientDetails();
+}
+function openRootContentItemCard($rootContentItemCard) {
+  clearRootContentItemSelection();
+  $rootContentItemCard.attr('selected', '');
+  getSelectionGroupList($rootContentItemCard);
+  showRootContentItemDetails();
 }
 
 
@@ -83,7 +99,7 @@ function renderRootContentItem(rootContentItem) {
       ])
       .attr({ 'data-root-content-item-id': rootContentItem.RootContentItemEntity.Id })
       .click(function onClick() {
-        console.log('Root content item ' + rootContentItem.RootContentItemEntity.ContentName + ' clicked.');
+        rootContentItemCardClickHandler($(this));
       })
     .primaryInfo(rootContentItem.RootContentItemEntity.ContentName)
     .secondaryInfo(rootContentItem.RootContentItemEntity.ContentType.Name)
@@ -95,6 +111,29 @@ function renderRootContentItem(rootContentItem) {
   /* eslint-enable indent */
 
   $('#root-content-list').append($card);
+}
+function renderSelectionGroup(selectionGroup) {
+  var userInfo = $.map(selectionGroup.MemberList, function toString(member) {
+    return [member.FirstName + ' ' + member.LastName, member.Email, member.UserName];
+  }).reduce(function concat(acc, cur) {
+    return acc.concat(cur);
+  }, []);
+  /* eslint-disable indent */
+  var $card = Card
+    .newCard()
+    .container()
+      .searchString(userInfo.concat([selectionGroup.SelectionGroupEntity.GroupName]))
+      .attr({ 'data-selection-group-id': selectionGroup.SelectionGroupEntity.Id })
+      .click(function onClick() {
+        console.log('Selection group "' + selectionGroup.SelectionGroupEntity.GroupName + '" clicked.');
+      })
+    .primaryInfo(selectionGroup.SelectionGroupEntity.GroupName)
+    .cardStat('#action-icon-users', selectionGroup.MemberList.length)
+      .tooltip('Members')
+    .build();
+  /* eslint-enable indent */
+
+  $('#selection-groups-list').append($card);
 }
 
 function renderClientTree(clientTreeList, clientId) {
@@ -111,7 +150,6 @@ function renderClientTree(clientTreeList, clientId) {
     $('[data-client-id="' + clientId + '"]').click();
   }
 }
-
 function renderRootContentItemList(rootContentItemList, rootContentItemId) {
   var $rootContentItemList = $('#root-content-list');
   $rootContentItemList.empty();
@@ -122,6 +160,18 @@ function renderRootContentItemList(rootContentItemList, rootContentItemId) {
 
   if (rootContentItemId) {
     $('[data-root-content-item-id="' + rootContentItemId + '"]').click();
+  }
+}
+function renderSelectionGroupList(selectionGroupList, selectionGroupId) {
+  var $selectionGroupList = $('#selection-groups-list');
+  $selectionGroupList.empty();
+  selectionGroupList.forEach(function render(selectionGroup) {
+    renderSelectionGroup(selectionGroup);
+  });
+  $selectionGroupList.find('.tooltip').tooltipster();
+
+  if (selectionGroupId) {
+    $('[data-selection-group-id="' + selectionGroupId + '"]').click();
   }
 }
 
@@ -145,7 +195,6 @@ function getClientTree() {
     }
   });
 }
-
 function getRootContentItemList($clientCard) {
   var clientId = $clientCard.attr('data-client-id');
   $('#root-content .loading-wrapper').show();
@@ -161,6 +210,30 @@ function getRootContentItemList($clientCard) {
     $('#root-content .loading-wrapper').hide();
   }).fail(function onFail(response) {
     $('#root-content .loading-wrapper').hide();
+    if (response.getResponseHeader('Warning')) {
+      toastr.warning(response.getResponseHeader('Warning'));
+    } else {
+      toastr.error('An error has occurred');
+    }
+  });
+}
+function getSelectionGroupList($rootContentItemCard) {
+  var clientId = $('#client-tree [selected]').attr('data-client-id');
+  var rootContentItemId = $rootContentItemCard.attr('data-root-content-item-id');
+  $('#selection-groups .loading-wrapper').show();
+  ajaxStatus.getRootContentItemList = [clientId, rootContentItemId];
+  $.ajax({
+    type: 'GET',
+    url: 'ContentAccessAdmin/SelectionGroups',
+    data: {
+      ClientId: clientId,
+      RootContentItemId: rootContentItemId
+    }
+  }).done(function onDone(response) {
+    renderSelectionGroupList(response.SelectionGroupList);
+    $('#selection-groups .loading-wrapper').hide();
+  }).fail(function onFail(response) {
+    $('#selection-groups .loading-wrapper').hide();
     if (response.getResponseHeader('Warning')) {
       toastr.warning(response.getResponseHeader('Warning'));
     } else {
@@ -185,6 +258,21 @@ clientCardClickHandler = function clientCardClickHandler_($clickedCard) {
     }
   } else {
     openClientCard($clickedCard);
+  }
+};
+rootContentItemCardClickHandler = function rootContentItemCardClickHandler_($clickedCard) {
+  var $rootContent = $('#root-content');
+  var sameCard = ($clickedCard[0] === $rootContent.find('[selected]')[0]);
+  if ($rootContent.has('[selected]').length) {
+    // TODO: wrap if-else with confirmAndReset
+    if (sameCard) {
+      clearRootContentItemSelection();
+      hideRootContentItemDetails();
+    } else {
+      openRootContentItemCard($clickedCard);
+    }
+  } else {
+    openRootContentItemCard($clickedCard);
   }
 };
 
