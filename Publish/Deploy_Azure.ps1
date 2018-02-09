2e40cb74546eb34a760468e7a29b2e3078a5bbac
cFB9JefHyQDwwNDZt0q4uEki63ewf0hoG7GYbvpqunjnRAPtjfFwzbY6jRCQ <Ben.Wyatt@milliman.com>
2018-02-09 10:42:53 -0500

    Use MSBuild 15 for the fake build
    I'm also allowing it to output now. I'll make it silent again in a future commit.

--- Publish/Deploy_Azure.ps1
+++ Publish/Deploy_Azure.ps1
@@ -97,8 +97,7 @@ if ($LASTEXITCODE -ne 0) {
 }
 
 # Do a build intended to fail, to cause the WebCompiler folder to be created
-$silentBuild = invoke-expression "MSBuild /verbosity:minimal" | out-string
-
+start-process $MSbuild15Path -ArgumentList"/verbosity:minimal" -wait
 
 #region Web Compiler setup
 log_statement "Looking for Web Compiler"
