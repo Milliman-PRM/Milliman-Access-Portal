dfcf79187df0f268a9c6475b739fe3641ef39904
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-02-09 13:59:51 -0500

    Make Card a constructor function

--- MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/card.js
+++ MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/card.js
@@ -6,13 +6,9 @@ var Card;
   var findComponent;
   var html;
 
-  Card = {
-
-    settings: {
-    },
-
+  Card = function () {
     /* eslint-disable indent */
-    templates: {
+    this.templates = {
       container: {
         selector: '.card-container',
         html: [
@@ -173,167 +169,162 @@ var Card;
           '</div>'
         ].join('')
       }
-    },
+    };
     /* eslint-enable indent */
+  };
 
-    vars: {
-    },
+  Card.prototype.attr = function (value, selector) {
+    var $component = findComponent(this.vars.lastComponent, selector);
+    $component.attr(value);
+    return this;
+  };
 
-    init: function () {
-    },
+  Card.prototype.prop = function (value, selector) {
+    var $component = findComponent(this.vars.lastComponent, selector);
+    $component.prop(value);
+    return this;
+  };
 
-    attr: function (value, selector) {
-      var $component = findComponent(this.vars.lastComponent, selector);
-      $component.attr(value);
-      return this;
-    },
+  Card.prototype.class = function (value, selector) {
+    var $component = findComponent(this.vars.lastComponent, selector);
+    $component.addClass(value);
+    return this;
+  };
 
-    prop: function (value, selector) {
-      var $component = findComponent(this.vars.lastComponent, selector);
-      $component.prop(value);
-      return this;
-    },
+  Card.prototype.tooltip = function (value, selector) {
+    var $component = findComponent(this.vars.lastComponent, selector);
+    $component.addClass('tooltip');
+    $component.attr('title', value);
+    return this;
+  };
 
-    class: function (value, selector) {
-      var $component = findComponent(this.vars.lastComponent, selector);
-      $component.addClass(value);
-      return this;
-    },
-
-    tooltip: function (value, selector) {
-      var $component = findComponent(this.vars.lastComponent, selector);
-      $component.addClass('tooltip');
-      $component.attr('title', value);
-      return this;
-    },
-
-    click: function (onClick, selector) {
-      var $component = findComponent(this.vars.lastComponent, selector);
-      $component.click(onClick);
-      return this;
-    },
+  Card.prototype.click = function (onClick, selector) {
+    var $component = findComponent(this.vars.lastComponent, selector);
+    $component.click(onClick);
+    return this;
+  };
 
 
-    newCard: function () {
-      this.vars.$card = $(this.templates.container.html);
-      this.vars.lastComponent = '';
-      return this;
-    },
+  Card.prototype.newCard = function () {
+    this.vars.$card = $(this.templates.container.html);
+    this.vars.lastComponent = '';
+    return this;
+  };
 
-    container: function (searchTerms, clientId, userId, canManage) {
-      var searchString;
-      this.vars.canManage = canManage;
-      this.vars.lastComponent = 'container';
-      if (searchTerms) {
-        searchString = $.map(searchTerms, function (term) {
-          return (term || '').toUpperCase();
-        }).join('|');
-        this.attr({ 'data-search-string': searchString });
-      }
-      if (clientId) this.attr({ 'data-client-id': clientId });
-      if (userId) this.attr({ 'data-user-id': userId });
-      if (typeof canManage === 'boolean' && !this.vars.canManage) this.attr({ disabled: '' });
-      return this;
-    },
+  Card.prototype.container = function (searchTerms, clientId, userId, canManage) {
+    var searchString;
+    this.vars.canManage = canManage;
+    this.vars.lastComponent = 'container';
+    if (searchTerms) {
+      searchString = $.map(searchTerms, function (term) {
+        return (term || '').toUpperCase();
+      }).join('|');
+      this.attr({ 'data-search-string': searchString });
+    }
+    if (clientId) this.attr({ 'data-client-id': clientId });
+    if (userId) this.attr({ 'data-user-id': userId });
+    if (typeof canManage === 'boolean' && !this.vars.canManage) this.attr({ disabled: '' });
+    return this;
+  };
 
-    main: function () {
-      this.vars.lastComponent = 'main';
-      verify(['main']);
-      return this;
-    },
+  Card.prototype.main = function () {
+    this.vars.lastComponent = 'main';
+    verify(['main']);
+    return this;
+  };
 
-    primaryInfo: function (text) {
-      this.vars.lastComponent = 'primary';
-      add(['main', 'text', 'primary']);
-      return html(text);
-    },
+  Card.prototype.primaryInfo = function (text) {
+    this.vars.lastComponent = 'primary';
+    add(['main', 'text', 'primary']);
+    return html(text);
+  };
 
-    secondaryInfo: function (text) {
-      this.vars.lastComponent = 'secondary';
-      add(['main', 'text', 'secondary']);
-      return html(text);
-    },
+  Card.prototype.secondaryInfo = function (text) {
+    this.vars.lastComponent = 'secondary';
+    add(['main', 'text', 'secondary']);
+    return html(text);
+  };
 
-    info: function (textList) {
-      this.vars.lastComponent = '';
-      textList.reverse();
-      this.primaryInfo(textList.pop());
-      while (textList.length) {
-        this.secondaryInfo(textList.pop());
-      }
-      return this;
-    },
+  Card.prototype.info = function (textList) {
+    this.vars.lastComponent = '';
+    textList.reverse();
+    this.primaryInfo(textList.pop());
+    while (textList.length) {
+      this.secondaryInfo(textList.pop());
+    }
+    return this;
+  };
 
-    action: function () {
-      this.vars.lastComponent = 'action';
-      verify(['main', 'text', 'action']);
-      return this;
-    },
+  Card.prototype.action = function () {
+    this.vars.lastComponent = 'action';
+    verify(['main', 'text', 'action']);
+    return this;
+  };
 
-    actionText: function (text) {
-      this.vars.lastComponent = 'actionText';
-      add(['main', 'text', 'action', 'actionText']);
-      return html(text);
-    },
+  Card.prototype.actionText = function (text) {
+    this.vars.lastComponent = 'actionText';
+    add(['main', 'text', 'action', 'actionText']);
+    return html(text);
+  };
 
-    icon: function (iconName) {
-      this.vars.lastComponent = 'icon';
-      add(['main', 'icons', 'icon']);
-      return this.attr({ href: iconName }, '[href]');
-    },
+  Card.prototype.icon = function (iconName) {
+    this.vars.lastComponent = 'icon';
+    add(['main', 'icons', 'icon']);
+    return this.attr({ href: iconName }, '[href]');
+  };
 
-    actionIcon: function (iconName) {
-      this.vars.lastComponent = 'icon';
-      add(['main', 'text', 'action', 'icon']);
-      return this.attr({ href: iconName }, '[href]');
-    },
+  Card.prototype.actionIcon = function (iconName) {
+    this.vars.lastComponent = 'icon';
+    add(['main', 'text', 'action', 'icon']);
+    return this.attr({ href: iconName }, '[href]');
+  };
 
-    cardStat: function (iconName, value) {
-      this.vars.lastComponent = 'stat';
-      add(['main', 'stats', 'stat']);
-      this.attr({ href: iconName }, '[href]');
-      return html(value, '.card-stat-value');
-    },
+  Card.prototype.cardStat = function (iconName, value) {
+    this.vars.lastComponent = 'stat';
+    add(['main', 'stats', 'stat']);
+    this.attr({ href: iconName }, '[href]');
+    return html(value, '.card-stat-value');
+  };
 
-    sideButton: function (iconName) {
-      if (!this.vars.canManage) {
-        this.vars.lastComponent = 'button';
-        return this;
-      }
+  Card.prototype.sideButton = function (iconName) {
+    if (!this.vars.canManage) {
       this.vars.lastComponent = 'button';
-      add(['main', 'side', 'button']);
-      return this.attr({ href: iconName }, '[href]');
-    },
+      return this;
+    }
+    this.vars.lastComponent = 'button';
+    add(['main', 'side', 'button']);
+    return this.attr({ href: iconName }, '[href]');
+  };
 
-    expansionLabel: function (label) {
-      this.vars.lastComponent = 'expansionLabel';
-      verify(['expansion', 'expansionLabel']);
-      return html(label);
-    },
+  Card.prototype.expansionLabel = function (label) {
+    this.vars.lastComponent = 'expansionLabel';
+    verify(['expansion', 'expansionLabel']);
+    return html(label);
+  };
 
-    expansionButton: function (iconName) {
-      this.vars.lastComponent = 'bottom';
-      verify(['expansion', 'bottom']);
-      return this.attr({ href: iconName }, '[href]');
-    },
+  Card.prototype.expansionButton = function (iconName) {
+    this.vars.lastComponent = 'bottom';
+    verify(['expansion', 'bottom']);
+    return this.attr({ href: iconName }, '[href]');
+  };
 
-    roleExpansion: function () {
-      var onClick = function (event) {
-        event.stopPropagation();
-        $(this).closest('.card-container')
-          .find('div.card-expansion-container')
-          .attr('maximized', function (index, attr) {
-            if (attr === '') {
-              $(this).find('.tooltip').tooltipster('content', 'Expand user card');
-              return null;
-            }
-            $(this).find('.tooltip').tooltipster('content', 'Collapse user card');
-            return '';
-          });
-        showRelevantUserActionIcons();
-      };
-      this.vars.lastComponent = '';
-      /* eslint-disable indent */
+  Card.prototype.roleExpansion = function () {
+    var onClick = function (event) {
+      event.stopPropagation();
+      $(this).closest('.card-container')
+        .find('div.card-expansion-container')
+        .attr('maximized', function (index, attr) {
+          if (attr === '') {
+            $(this).find('.tooltip').tooltipster('content', 'Expand user card');
+            return null;
+          }
+          $(this).find('.tooltip').tooltipster('content', 'Collapse user card');
+          return '';
+        });
+      showRelevantUserActionIcons();
+    };
+    this.vars.lastComponent = '';
+    /* eslint-disable indent */
       return this
         .expansionLabel('User roles')
         .expansionButton('#action-icon-expand-card')
@@ -342,49 +333,49 @@ var Card;
         .main()
           .click(onClick);
       /* eslint-enable indent */
-    },
+  };
 
-    roleToggle: function (roleEnum, roleName, roleAssigned) {
-      var userId = this.vars.$card
-        .find(this.templates.container.selector)
-        .attr('data-user-id');
-      var id = 'user-role-' + userId + '-' + roleEnum;
-      this.vars.lastComponent = 'toggle';
-      add(['expansion', 'toggle']);
-      if (!this.vars.canManage) this.attr({ disabled: '' }, '.toggle-switch-checkbox');
-      return html(roleName, '.switch-label')
-        .attr({
-          'data-role-enum': roleEnum,
-          name: id,
-          id: id
-        }, '.toggle-switch-checkbox')
-        .attr({ for: id }, '.toggle-switch-label')
-        .prop({ checked: roleAssigned }, '.toggle-switch-checkbox');
-    },
+  Card.prototype.roleToggle = function (roleEnum, roleName, roleAssigned) {
+    var userId = this.vars.$card
+      .find(this.templates.container.selector)
+      .attr('data-user-id');
+    var id = 'user-role-' + userId + '-' + roleEnum;
+    this.vars.lastComponent = 'toggle';
+    add(['expansion', 'toggle']);
+    if (!this.vars.canManage) this.attr({ disabled: '' }, '.toggle-switch-checkbox');
+    return html(roleName, '.switch-label')
+      .attr({
+        'data-role-enum': roleEnum,
+        name: id,
+        id: id
+      }, '.toggle-switch-checkbox')
+      .attr({ for: id }, '.toggle-switch-label')
+      .prop({ checked: roleAssigned }, '.toggle-switch-checkbox');
+  };
 
-    roleToggles: function (roles) {
-      var self = this;
-      var onClick = function (event) {
-        userCardRoleToggleClickHandler(event);
-      };
-      this.vars.lastComponent = '';
-      $.each(roles, function (index, assignment) {
-        self.roleToggle(
-          assignment.RoleEnum,
-          assignment.RoleDisplayValue,
-          assignment.IsAssigned
-        ).click(onClick, '.toggle-switch-checkbox');
-      });
-      return this;
-    },
+  Card.prototype.roleToggles = function (roles) {
+    var self = this;
+    var onClick = function (event) {
+      userCardRoleToggleClickHandler(event);
+    };
+    this.vars.lastComponent = '';
+    $.each(roles, function (index, assignment) {
+      self.roleToggle(
+        assignment.RoleEnum,
+        assignment.RoleDisplayValue,
+        assignment.IsAssigned
+      ).click(onClick, '.toggle-switch-checkbox');
+    });
+    return this;
+  };
 
-    build: function () {
-      this.vars.$card.find('stub').remove();
-      return this.vars.$card;
-    },
+  Card.prototype.build = function () {
+    this.vars.$card.find('stub').remove();
+    return this.vars.$card;
+  };
 
-    buildNewClient: function () {
-      /* eslint-disable indent */
+  Card.prototype.buildNewClient = function () {
+    /* eslint-disable indent */
       return this
         .newCard()
         .container()
@@ -395,10 +386,10 @@ var Card;
         .actionText('New Client')
         .build();
       /* eslint-enable indent */
-    },
+  };
 
-    buildNewChildClient: function (level) {
-      /* eslint-disable indent */
+  Card.prototype.buildNewChildClient = function (level) {
+    /* eslint-disable indent */
       return this
         .newCard()
           .class('client-insert')
@@ -413,10 +404,10 @@ var Card;
           .class('indent-level-' + level, '.card-body-primary-text')
         .build();
       /* eslint-enable indent */
-    },
+  };
 
-    buildAddUser: function () {
-      /* eslint-disable indent */
+  Card.prototype.buildAddUser = function () {
+    /* eslint-disable indent */
       return this
         .newCard()
         .container()
@@ -427,9 +418,8 @@ var Card;
         .actionText('Add User')
         .build();
       /* eslint-enable indent */
-    }
-
   };
+
   verify = function (path) {
     var prevSelector = Card.templates.container.selector;
     $.each(path, function () {
@@ -444,6 +434,7 @@ var Card;
     });
     return prevSelector;
   };
+
   add = function (path) {
     var newElement = path.pop();
     var prevSelector = verify(path);
@@ -452,6 +443,7 @@ var Card;
       .replaceWith(Card.templates[newElement].html);
     return prevSelector + ' > ' + Card.templates[newElement].selector;
   };
+
   findComponent = function (component, selector) {
     var $component = Card.templates[component]
       ? Card.vars.$card
@@ -461,6 +453,7 @@ var Card;
     var $subcomponent = $component.find(selector);
     return $subcomponent.length ? $subcomponent : $component;
   };
+
   html = function (value, selector) {
     var $component = findComponent(Card.vars.lastComponent, selector);
     $component.html(value);
