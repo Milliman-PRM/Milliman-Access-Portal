7c53674001074ed4053b105783f0d1aa4e8d61ad
Ben Wyatt <Ben.Wyatt@milliman.com>
2018-02-12 08:21:54 -0500

    Try removing credential before setting it

--- Publish/CI_Publish.ps1
+++ Publish/CI_Publish.ps1
@@ -354,6 +354,11 @@ else
 
 #region Create Windows credential store object for deployment
 
+$command = "$credManagerPath -DelCred -Target `"git:$RemoteUrl`""
+start-process "powershell.exe" -ArgumentList "-Command `"$command`"" -wait -RedirectStandardOutput "$env:temp\output.txt" -redirectstandarderror "$env:temp\error.txt"
+log_statement "Attempted to delete an existing credential, if one exists. Return code was $LASTEXITCODE."
+log_output
+
 $command = "$credManagerPath -AddCred -Target `"git:$RemoteUrl`" -User `"$gitUser`" -pass `"$gitPassword`""
 start-process "powershell.exe" -ArgumentList "-Command `"$command`"" -wait -RedirectStandardOutput "$env:temp\output.txt" -redirectstandarderror "$env:temp\error.txt"
 if ($? -eq $false)
