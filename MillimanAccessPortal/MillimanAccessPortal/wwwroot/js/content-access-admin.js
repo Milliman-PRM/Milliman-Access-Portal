95c6bb40cbc539218ad06eedbf841a296b681406
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-02-19 11:34:13 -0500

    Rearrange content access admin

--- MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/content-access-admin.js
+++ MillimanAccessPortal/MillimanAccessPortal/wwwroot/js/content-access-admin.js
@@ -1,10 +1,52 @@
 /* global card, shared */
 
-var ajaxStatus = {
-  getRootContentItemList: -1
-};
+function renderSelectionGroup(selectionGroup) {
+  var $card = new card.SelectionGroupCard(
+    selectionGroup.SelectionGroupEntity,
+    selectionGroup.MemberList,
+    function () { console.log('Selection group clicked.'); },
+    function () { console.log('Delete group button clicked.'); },
+    function () { console.log('Add/remove user button clicked.'); }
+  );
+  $('#selection-groups ul.admin-panel-content').append($card.build());
+}
+function renderSelectionGroupList(response, selectionGroupId) {
+  var $selectionGroupList = $('#selection-groups ul.admin-panel-content');
+  $selectionGroupList.empty();
+  response.SelectionGroupList.forEach(renderSelectionGroup);
+  $selectionGroupList.find('.tooltip').tooltipster();
 
-// Render functions
+  if (selectionGroupId) {
+    $('[data-selection-group-id="' + selectionGroupId + '"]').click();
+  }
+}
+
+
+function renderRootContentItem(rootContentItem) {
+  var $card = new card.RootContentItemCard(
+    rootContentItem.RootContentItemEntity,
+    rootContentItem.GroupCount,
+    rootContentItem.EligibleUserCount,
+    shared.wrapCardCallback(shared.get(
+      'ContentAccessAdmin/SelectionGroups',
+      renderSelectionGroupList
+    ))
+  );
+
+  $('#root-content-items ul.admin-panel-content').append($card.build());
+}
+function renderRootContentItemList(response, rootContentItemId) {
+  var $rootContentItemList = $('#root-content-items ul.admin-panel-content');
+  $rootContentItemList.empty();
+  response.RootContentItemList.forEach(renderRootContentItem);
+  $rootContentItemList.find('.tooltip').tooltipster();
+
+  if (rootContentItemId) {
+    $('[data-root-content-item-id="' + rootContentItemId + '"]').click();
+  }
+}
+
+
 function renderClientNode(client, level) {
   var $card = new card.ClientCard(
     client.ClientDetailModel.ClientEntity,
@@ -26,33 +68,6 @@ function renderClientNode(client, level) {
     });
   }
 }
-
-function renderRootContentItem(rootContentItem) {
-  var $card = new card.RootContentItemCard(
-    rootContentItem.RootContentItemEntity,
-    rootContentItem.GroupCount,
-    rootContentItem.EligibleUserCount,
-    shared.wrapCardCallback(shared.get(
-      'ContentAccessAdmin/SelectionGroups',
-      renderSelectionGroupList
-    ))
-  );
-
-  $('#root-content-items ul.admin-panel-content').append($card.build());
-}
-
-function renderSelectionGroup(selectionGroup) {
-  var $card = new card.SelectionGroupCard(
-    selectionGroup.SelectionGroupEntity,
-    selectionGroup.MemberList,
-    function () { console.log('Selection group clicked.'); },
-    function () { console.log('Delete group button clicked.'); },
-    function () { console.log('Add/remove user button clicked.'); }
-  );
-
-  $('#selection-groups ul.admin-panel-content').append($card.build());
-}
-
 function renderClientTree(response, clientId) {
   var $clientTreeList = $('#client-tree ul.admin-panel-content');
   $clientTreeList.empty();
@@ -67,53 +82,13 @@ function renderClientTree(response, clientId) {
     $('[data-client-id="' + clientId + '"]').click();
   }
 }
-function renderRootContentItemList(response, rootContentItemId) {
-  var $rootContentItemList = $('#root-content-items ul.admin-panel-content');
-  $rootContentItemList.empty();
-  response.RootContentItemList.forEach(renderRootContentItem);
-  $rootContentItemList.find('.tooltip').tooltipster();
 
-  if (rootContentItemId) {
-    $('[data-root-content-item-id="' + rootContentItemId + '"]').click();
-  }
-}
-function renderSelectionGroupList(response, selectionGroupId) {
-  var $selectionGroupList = $('#selection-groups ul.admin-panel-content');
-  $selectionGroupList.empty();
-  response.SelectionGroupList.forEach(renderSelectionGroup);
-  $selectionGroupList.find('.tooltip').tooltipster();
-
-  if (selectionGroupId) {
-    $('[data-selection-group-id="' + selectionGroupId + '"]').click();
-  }
-}
-
-
-// AJAX functions
-function getClientTree() {
-  $('#client-tree .loading-wrapper').show();
-  $.ajax({
-    type: 'GET',
-    url: 'ContentAccessAdmin/ClientFamilyList'
-  }).done(function onDone(response) {
-    renderClientTree(response.ClientTreeList);
-    $('#client-tree .loading-wrapper').hide();
-  }).fail(function onFail(response) {
-    $('#client-tree .loading-wrapper').hide();
-    if (response.getResponseHeader('Warning')) {
-      toastr.warning(response.getResponseHeader('Warning'));
-    } else {
-      toastr.error('An error has occurred');
-    }
-  });
-}
 
 $(document).ready(function () {
-  // getClientTree();
   (shared.get(
     'ContentAccessAdmin/ClientFamilyList',
     renderClientTree
-  ))();
+  )());
 
   $('.action-icon-expand').click(shared.expandAll.listener);
   $('.action-icon-collapse').click(shared.collapseAll.listener);
