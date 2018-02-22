cf51274226db8e92943e78a89d3bdc94b256488d
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-02-22 11:07:29 -0500

    Rename and rearrange private members

--- MillimanAccessPortal/MillimanAccessPortal/BuildTimeProperties.cs
+++ MillimanAccessPortal/MillimanAccessPortal/BuildTimeProperties.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿
+
+/*
  * CODE OWNERS: Tom Puckett
  * OBJECTIVE: Provides build time information for use at run time
  * DEVELOPER NOTES: This C# code is generated at each build from a T4 script of <same named file>.tt
