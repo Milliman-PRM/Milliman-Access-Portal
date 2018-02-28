376261542e2cdf2dee5220f6cd3c0fd388375a6a
Michael Reisz <Michael.Reisz@milliman.com>
2018-02-28 09:25:20 -0500

    Add a view model to populate the Account Settings form
    Add a new view model that will be used to populate the Account Settings form and it's associated jquery validation rules.

---
+++ MillimanAccessPortal/MillimanAccessPortal/Models/AccountViewModels/AccountSettingsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MillimanAccessPortal.Models.AccountViewModels
+{
+    public class AccountSettingsViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string Phone { get; set; }
+
+        public string Employer { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+}
