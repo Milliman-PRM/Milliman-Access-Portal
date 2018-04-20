90157570f4aaacdb00ce4cd3969cc696d1d384c6
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-04-20 14:19:35 -0400

    Adjust tests to reflect recent changes

--- MillimanAccessPortal/MapTests/ContentPublishingControllerTests.cs
+++ MillimanAccessPortal/MapTests/ContentPublishingControllerTests.cs
@@ -4,12 +4,9 @@
  * DEVELOPER NOTES: 
  */
 
-using MapCommonLib;
 using Microsoft.AspNetCore.Mvc;
 using MillimanAccessPortal.Controllers;
-using MillimanAccessPortal.Models.ContentPublicationViewModels;
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -36,7 +33,6 @@ namespace MapTests
                 TestResources.AuditLoggerObject,
                 TestResources.AuthorizationService,
                 TestResources.DbContextObject,
-                TestResources.UploadHelperObject,
                 TestResources.LoggerFactory,
                 TestResources.QueriesObj
                 );
@@ -71,85 +67,11 @@ namespace MapTests
             #endregion
         }
 
-        [Theory]
-        [InlineData("random.dat", "2339ebed070fd30a869a22193ef2f76284ed333b")]
-        public void GetFileChecksum_Match(string fileName, string checksum)
-        {
-            #region Arrange
-            var filePath = Path.Combine("../../../TestData", fileName);
-            #endregion
-
-            #region Act
-            var fileChecksum = GlobalFunctions.GetFileChecksum(filePath).ToLower();
-            #endregion
-
-            #region Assert
-            Assert.Equal(fileChecksum, checksum);
-            #endregion
-        }
-
-        private ResumableInfo BuildResumableInfo(string fileName, string checksum, ulong size, uint chunkNumber)
-        {
-            const uint chunkSize = (1024 * 1024);
-            var resumableData = new ResumableInfo
-            {
-                ChunkNumber = chunkNumber,
-                TotalChunks = ((uint) size) / chunkSize,
-                TotalSize = size,
-                FileName = fileName,
-                UID = $"{String.Join('_', fileName.Split('.'))}-{checksum}",
-            };
-            resumableData.ChunkSize = (resumableData.ChunkNumber == resumableData.TotalChunks)
-                ? ((uint) size) % chunkSize + chunkSize
-                : chunkSize;
-            return resumableData;
-        }
-
-        [Theory]
-        [InlineData("nonexistant_random.dat", "2339ebed070fd30a869a22193ef2f76284ed333b", 2097152, 1)]
-        [InlineData("incomplete_random.dat", "2339ebed070fd30a869a22193ef2f76284ed333b", 2097152, 1)]
-        [InlineData("random.dat", "2339ebed070fd30a869a22193ef2f76284ed333b", 2097152, 2)]
-        public async Task ChunkStatus_NotFound(string fileName, string checksum, ulong size, uint chunkNumber)
+        [Fact]
+        public async Task RequestContentPublication_Ok()
         {
             #region Arrange
             ContentPublishingController controller = await GetControllerForUser("test1");
-            var resumableInfo = BuildResumableInfo(fileName, checksum, size, chunkNumber);
-            #endregion
-
-            #region Act
-            var view = controller.ChunkStatus(resumableInfo);
-            #endregion
-
-            #region Assert
-            Assert.IsType<JsonResult>(view);
-            #endregion
-        }
-
-        [Theory]
-        [InlineData("random.dat", "2339ebed070fd30a869a22193ef2f76284ed333b", 2097152, 1)]
-        public async Task ChunkStatus_Ok(string fileName, string checksum, ulong size, uint chunkNumber)
-        {
-            #region Arrange
-            ContentPublishingController controller = await GetControllerForUser("test1");
-            var resumableInfo = BuildResumableInfo(fileName, checksum, size, chunkNumber);
-            #endregion
-
-            #region Act
-            var view = controller.ChunkStatus(resumableInfo);
-            #endregion
-
-            #region Assert
-            Assert.IsType<JsonResult>(view);
-            #endregion
-        }
-
-        [Theory]
-        [InlineData("random.dat", "2339ebed070fd30a869a22193ef2f76284ed333b", 2097152, 1)]
-        public async Task RequestContentPublication_Ok(string fileName, string checksum, ulong size, uint chunkNumber)
-        {
-            #region Arrange
-            ContentPublishingController controller = await GetControllerForUser("test1");
-            var resumableInfo = BuildResumableInfo(fileName, checksum, size, chunkNumber);
             #endregion
 
             #region Act
