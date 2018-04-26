669124cca7c01614cfc7ef8aeb76bd222966af56
Tom Puckett <Tom.Puckett@milliman.com>
2018-04-26 17:08:49 -0400

    Additional test case.

--- ReductionServer/ContentReductionServiceTests/QvReductionRunnerTests.cs
+++ ReductionServer/ContentReductionServiceTests/QvReductionRunnerTests.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
+using MapDbContextLib.Context;
 using ContentReductionLib.ReductionRunners;
 using TestResourcesLib;
 
@@ -19,51 +20,12 @@ namespace ContentReductionServiceTests
         [Fact]
         public void SuccessfulHierarchyOnly()
         {
-            var MockContext = MockMapDbContext.New(InitializeTests.InitializeWithQueuedStatus).Object;
-            var DbTask = MockContext.ContentReductionTask.Single(t => t.Id == new Guid(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1));
+            ApplicationDbContext MockContext = MockMapDbContext.New(InitializeTests.InitializeWithUnspecifiedStatus).Object;
+
+            // Modify the task to be tested
+            ContentReductionTask DbTask = MockContext.ContentReductionTask.Single(t => t.Id == new Guid(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1));
             DbTask.TaskAction = MapDbContextLib.Context.TaskActionEnum.HierarchyOnly;
-
-            #region Arrange
-            QvReductionRunner TestRunner = new QvReductionRunner
-            {
-                JobDetail = (ReductionJobDetail)DbTask,
-            };
-            TestRunner.SetTestAuditLogger(MockAuditLogger.New().Object);
-
-            CancellationTokenSource CancelTokenSource = new CancellationTokenSource();
-            #endregion
-
-            #region Act
-            Task<ReductionJobDetail> MonitorTask = TestRunner.Execute(CancelTokenSource.Token);
-            Task.WaitAll(new Task[] { MonitorTask }, new TimeSpan(0, 3, 0));
-            var TaskReturn = MonitorTask.Result.Result;
-            #endregion
-
-            #region Assert
-            Assert.NotNull(TaskReturn);
-            Assert.IsType<ReductionJobDetail.ReductionJobResult>(TaskReturn);
-            Assert.Equal<TaskStatus>(TaskStatus.RanToCompletion, MonitorTask.Status);
-            Assert.False(MonitorTask.IsCanceled);
-            Assert.False(MonitorTask.IsFaulted);
-            Assert.Equal(JobStatusEnum.Success, TaskReturn.Status);
-
-            Assert.NotNull(TaskReturn.MasterContentHierarchy);
-            Assert.Equal(3, TaskReturn.MasterContentHierarchy.Fields.Count);
-            Assert.Single(TaskReturn.MasterContentHierarchy.Fields[0].FieldValues); // using .Equal for expected value 1 gives warning
-            Assert.Equal(7, TaskReturn.MasterContentHierarchy.Fields[1].FieldValues.Count);
-            Assert.Equal(54, TaskReturn.MasterContentHierarchy.Fields[2].FieldValues.Count);
-
-            Assert.Null(TaskReturn.ReducedContentHierarchy);
-            Assert.True(string.IsNullOrWhiteSpace(TaskReturn.ReducedContentFilePath));
-            Assert.True(string.IsNullOrWhiteSpace(TaskReturn.ReducedContentFileChecksum));
-            #endregion
-        }
-
-        [Fact]
-        public void SuccessfulHierarchyAndReduction()
-        {
-            var MockContext = MockMapDbContext.New(InitializeTests.InitializeWithQueuedStatus).Object;
-            var DbTask = MockContext.ContentReductionTask.Single(t => t.Id == new Guid(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1));
+            DbTask.ReductionStatus = MapDbContextLib.Context.ReductionStatusEnum.Queued;
 
             #region Arrange
             QvReductionRunner TestRunner = new QvReductionRunner
@@ -80,6 +42,52 @@ namespace ContentReductionServiceTests
             Task.WaitAll(new Task[] { MonitorTask }, new TimeSpan(0, 3, 0));
             var TaskResult = MonitorTask.Result.Result;
             var TaskRequest = MonitorTask.Result.Request;
+            #endregion
+
+            #region Assert
+            Assert.NotNull(TaskResult);
+            Assert.IsType<ReductionJobDetail.ReductionJobResult>(TaskResult);
+            Assert.Equal<TaskStatus>(TaskStatus.RanToCompletion, MonitorTask.Status);
+            Assert.False(MonitorTask.IsCanceled);
+            Assert.False(MonitorTask.IsFaulted);
+            Assert.Equal(JobStatusEnum.Success, TaskResult.Status);
+
+            Assert.NotNull(TaskResult.MasterContentHierarchy);
+            Assert.Equal(3, TaskResult.MasterContentHierarchy.Fields.Count);
+            Assert.Single(TaskResult.MasterContentHierarchy.Fields[0].FieldValues); // using .Equal for expected value 1 gives warning
+            Assert.Equal(7, TaskResult.MasterContentHierarchy.Fields[1].FieldValues.Count);
+            Assert.Equal(54, TaskResult.MasterContentHierarchy.Fields[2].FieldValues.Count);
+
+            Assert.Null(TaskResult.ReducedContentHierarchy);
+            Assert.True(string.IsNullOrWhiteSpace(TaskResult.ReducedContentFilePath));
+            Assert.True(string.IsNullOrWhiteSpace(TaskResult.ReducedContentFileChecksum));
+            #endregion
+        }
+
+        [Fact]
+        public void SuccessfulHierarchyAndReduction()
+        {
+            ApplicationDbContext MockContext = MockMapDbContext.New(InitializeTests.InitializeWithUnspecifiedStatus).Object;
+
+            // Modify the task to be tested
+            ContentReductionTask DbTask = MockContext.ContentReductionTask.Single(t => t.Id == new Guid(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1));
+            DbTask.ReductionStatus = MapDbContextLib.Context.ReductionStatusEnum.Queued;
+
+            #region Arrange
+            QvReductionRunner TestRunner = new QvReductionRunner
+            {
+                JobDetail = (ReductionJobDetail)DbTask,
+            };
+            TestRunner.SetTestAuditLogger(MockAuditLogger.New().Object);
+
+            CancellationTokenSource CancelTokenSource = new CancellationTokenSource();
+            #endregion
+
+            #region Act
+            Task<ReductionJobDetail> MonitorTask = TestRunner.Execute(CancelTokenSource.Token);
+            Task.WaitAll(new Task[] { MonitorTask }, new TimeSpan(0, 3, 0));
+            var TaskRequest = MonitorTask.Result.Request;
+            var TaskResult = MonitorTask.Result.Result;
             #endregion
 
             #region Assert
@@ -112,5 +120,54 @@ namespace ContentReductionServiceTests
             #endregion
         }
 
+        [Fact]
+        public void InvalidFieldName()
+        {
+            ApplicationDbContext MockContext = MockMapDbContext.New(InitializeTests.InitializeWithUnspecifiedStatus).Object;
+
+            // Modify the task to be tested
+            ContentReductionTask DbTask = MockContext.ContentReductionTask.Single(t => t.Id == new Guid(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2));
+            DbTask.ReductionStatus = MapDbContextLib.Context.ReductionStatusEnum.Queued;
+
+            #region Arrange
+            QvReductionRunner TestRunner = new QvReductionRunner
+            {
+                JobDetail = (ReductionJobDetail)DbTask,
+            };
+            TestRunner.SetTestAuditLogger(MockAuditLogger.New().Object);
+
+            CancellationTokenSource CancelTokenSource = new CancellationTokenSource();
+            #endregion
+
+            #region Act
+            Task<ReductionJobDetail> MonitorTask = TestRunner.Execute(CancelTokenSource.Token);
+            Task.WaitAll(new Task[] { MonitorTask }, new TimeSpan(0, 3, 0));
+            var TaskRequest = MonitorTask.Result.Request;
+            var TaskResult = MonitorTask.Result.Result;
+            #endregion
+
+            #region Assert
+            Assert.NotNull(TaskResult);
+            Assert.IsType<ReductionJobDetail.ReductionJobResult>(TaskResult);
+            Assert.IsType<ReductionJobDetail.ReductionJobRequest>(TaskRequest);
+            Assert.Equal<TaskStatus>(TaskStatus.RanToCompletion, MonitorTask.Status);
+            Assert.False(MonitorTask.IsCanceled);
+            Assert.False(MonitorTask.IsFaulted);
+            Assert.Equal(JobStatusEnum.Error, TaskResult.Status);
+
+            Assert.NotNull(TaskResult.MasterContentHierarchy);
+            Assert.Equal(3, TaskResult.MasterContentHierarchy.Fields.Count);
+            Assert.Single(TaskResult.MasterContentHierarchy.Fields[0].FieldValues); // using .Equal for expected value 1 gives warning
+            Assert.Equal(7, TaskResult.MasterContentHierarchy.Fields[1].FieldValues.Count);
+            Assert.Equal(54, TaskResult.MasterContentHierarchy.Fields[2].FieldValues.Count);
+
+            Assert.Null(TaskResult.ReducedContentHierarchy);
+
+            Assert.True(string.IsNullOrWhiteSpace(TaskResult.ReducedContentFilePath));
+
+            Assert.True(string.IsNullOrWhiteSpace(TaskResult.ReducedContentFileChecksum));
+            // Can't know an expected checksum value because it's different every time. 
+            #endregion
+        }
     }
 }
