c061462b2a5dc55026c2478680feb6711448ca68
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-05-01 12:21:40 -0400

    Read files as ArrayBuffer

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/upload/file-scanner.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/upload/file-scanner.ts
@@ -1,3 +1,5 @@
+import { Promise } from 'es6-promise';
+
 interface FileReaderOnLoadEventTarget extends EventTarget {
   result: Blob | ArrayBuffer;
 }
@@ -16,6 +18,23 @@ class FileSlicer {
   }
   public get progress(): number {
     return Math.min(this.offset / this.file.size, 1);
+  }
+}
+
+class ArrayBufferSlicer {
+  private offset: number = 0;
+  constructor(readonly buffer: ArrayBuffer, readonly chunkSize: number) {
+  }
+  public next(): ArrayBuffer {
+    const chunk = this.buffer.slice(this.offset, this.offset + this.chunkSize);
+    this.offset += this.chunkSize;
+    return chunk;
+  }
+  public isEmpty(): boolean {
+    return this.offset >= this.buffer.byteLength;
+  }
+  public get progress(): number {
+    return Math.min(this.offset / this.buffer.byteLength, 1);
   }
 }
 
@@ -41,16 +60,20 @@ export class FileScanner {
           return;
         }
         const chunk = (event.target as FileReaderOnLoadEventTarget).result;
-        chunkLoadedCallback(chunk);
+        const chunkSlicer = new ArrayBufferSlicer(chunk, 1024);
+        for (let slice = chunkSlicer.next(); slice.byteLength > 0; slice = chunkSlicer.next()) {
+          const binaryString = String.fromCharCode.apply(null, new Uint8Array(slice));
+          chunkLoadedCallback(binaryString);
+        }
         if (this.slicer.isEmpty()) {
           this.active = false;
           resolve(this.slicer.file.size);
         } else {
-          this.reader.readAsBinaryString(this.slicer.next());
+          this.reader.readAsArrayBuffer(this.slicer.next());
         }
       };
       this.reader.onerror = () => reject;
-      this.reader.readAsBinaryString(this.slicer.next());
+      this.reader.readAsArrayBuffer(this.slicer.next());
     });
   }
   public get progress() {
