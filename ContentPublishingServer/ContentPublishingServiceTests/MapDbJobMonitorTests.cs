6a78bf0a592a0f567db96317123f201aa02394ef
Tom Puckett <Tom.Puckett@milliman.com>
2018-05-16 14:25:58 -0400

    More diagnostics on publish server test failures

--- ContentPublishingServer/ContentPublishingServiceTests/MapDbJobMonitorTests.cs
+++ ContentPublishingServer/ContentPublishingServiceTests/MapDbJobMonitorTests.cs
@@ -34,7 +34,6 @@ namespace ContentPublishingServiceTests
             #endregion
 
             #region Assert
-            System.Console.Out.WriteLine($"First assert, MonitorTask.Status is {MonitorTask.Status}");
             Assert.Equal<TaskStatus>(TaskStatus.Running, MonitorTask.Status);
             #endregion
 
@@ -45,17 +44,14 @@ namespace ContentPublishingServiceTests
             {
                 await MonitorTask;  // await rethrows anything that is thrown from the task
             }
-            catch (AggregateException)  // This is thrown when a task is cancelled
+            catch (OperationCanceledException)  // This is thrown when a task is cancelled
             { }
             DateTime CancelEndTime = DateTime.UtcNow;
             #endregion
 
             #region Assert again
-            System.Console.Out.WriteLine($"Second assert, MonitorTask.Status is {MonitorTask.Status}");
             Assert.Equal<TaskStatus>(TaskStatus.Canceled, MonitorTask.Status);
-            System.Console.Out.WriteLine($"Second assert, MonitorTask.IsCanceled is {MonitorTask.IsCanceled}");
             Assert.True(MonitorTask.IsCanceled);
-            System.Console.Out.WriteLine($"Second assert, CancelStart is {CancelStartTime}, CancelEnd is {CancelEndTime}, difference is {CancelEndTime-CancelStartTime}");
             Assert.True(CancelEndTime - CancelStartTime < new TimeSpan(0,0,30), "MapDbJobMonitor took too long to be canceled while idle");
             #endregion
         }
