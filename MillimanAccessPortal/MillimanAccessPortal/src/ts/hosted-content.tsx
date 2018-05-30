27bd6293f9e26ebe7d8ed0bf95a15bb1cc480b58
Michael Reisz <Michael.Reisz@milliman.com>
2018-05-30 11:30:43 -0400

    Get a very basic React component to build
    Get the very basics of React working on the Authorized Content view.

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/hosted-content.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/hosted-content.tsx
@@ -1,18 +1,24 @@
 require('jquery');
-require('jquery-mask-plugin');
 require('jquery-validation');
 require('jquery-validation-unobtrusive');
 require('./navbar');
 require('toastr');
 require('tooltipster');
 require('vex-js');
-require('./card');
-require('./dialog');
-require('./lib-options');
-require('./shared');
+//require('./card');
+//require('./dialog');
+//require('./lib-options');
+//require('./shared');
+//# sourceMappingURL=hosted-content.js.map
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import HostedContentApp from './hosted-content/hosted-content-app';
 
 require('bootstrap/scss/bootstrap-reboot.scss');
 require('toastr/toastr.scss');
 require('tooltipster/src/css/tooltipster.css');
 require('vex-js/sass/vex.sass');
 require('../scss/map.scss');
+
+ReactDOM.render(<HostedContentApp />, document.getElementById('content-container'));
---
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/hosted-content/hosted-content-app.tsx
@@ -0,0 +1,28 @@
+﻿import * as React from 'react';
+import { Component } from 'react';
+//import ContentCard from './content-card';
+
+class HostedContentApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contentItems: []
+    };
+  }
+
+  componentDidMount() {
+    const buttonSounds = [];
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>It's Working!</h1>
+      </div>
+    );
+  }
+
+
+}
+
+export default HostedContentApp;
