955b8859748732218b59ca2f29644a26eb55f205
Michael Reisz <Michael.Reisz@milliman.com>
2018-06-07 14:59:23 -0400

    Find middle ground with #344
    This brings in some of the elements from #344 such as breaking out the interfaces, adding a component for icons and filter bar, but it keeps some of the simplicity of the original pull request.

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/authorized-content/content-card.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/authorized-content/content-card.tsx
@@ -1,66 +1,40 @@
 ﻿import * as React from 'react';
 import { Component } from 'react';
 import '../../../scss/react/authorized-content/content-card.scss';
+import { ContentItem, Filterable } from './interfaces';
+import { ActionIcon } from '../shared-components/action-icon';
 
 require('tooltipster');
 require('tooltipster/src/css/tooltipster.css');
 
-interface ContentCardProps {
-  key: string,
-  name: string,
-  contentURL: string,
-  description: string,
-  thumbnailURL?: string,
-  userguideURL?: string,
-  releasenNotesURL?: string,
-}
 
-class ContentCard extends Component<ContentCardProps, {}> {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    let releasenNotesIcon = this.props.releasenNotesURL ?
-      (
-        <a href={this.props.releasenNotesURL} className="tooltip" title="View Release Notes">
-          <svg className="content-card-icon">
-            <use xlinkHref="#navbar-user-guide" />
-          </svg>
-        </a>
-      ) : null;
-    let userguideIcon = this.props.userguideURL ?
-      (
-        <a href={this.props.userguideURL} className="tooltip" title="View Userguide">
-          <svg className="content-card-icon">
-            <use xlinkHref="#navbar-user-guide" />
-          </svg>
-        </a>
-      ) : null;
-    let thumbnailImage = this.props.thumbnailURL ?
-      (
-        <img className="content-card-image" src={this.props.thumbnailURL} alt={this.props.name} />
-      ) : null;
-
+interface ContentCardProps extends ContentItem { }
+export class ContentCard extends Component<ContentCardProps, {}> {
+  public render() {
     return (
-      <div className="content-card-container">
-        <div className="content-card" onClick={() => window.location.href = this.props.contentURL}>
-          <div className="content-card-header">
-            <h2 className="content-card-title">{this.props.name}</h2>
-            <div className="content-card-icons">
-              {releasenNotesIcon}
-              {userguideIcon}
+        <div className="content-card-container">
+          <div className="content-card" onClick={() => window.location.href = this.props.contentURL}>
+            <div className="content-card-header">
+              <h2 className="content-card-title">{this.props.name}</h2>
+              <div className="content-card-icons">
+                <ActionIcon action={() => window.location.href = this.props.releaseNotesURL} title="View Release Notes" icon="navbar-user-guide" />
+                <ActionIcon action={() => window.location.href = this.props.userguideURL} title="View Userguide" icon="navbar-user-guide" />
+              </div>
+            </div>
+            <div className="content-card-body">
+              {this.props.imageURL &&
+                <img
+                  className="content-card-image"
+                  src={this.props.imageURL}
+                  alt={this.props.name}
+                />
+              }
+              <p className="content-card-description">
+                {this.props.description}
+              </p>
             </div>
           </div>
-          <div className="content-card-body">
-            {thumbnailImage}
-            <p className="content-card-description">{this.props.description}</p>
-          </div>
         </div>
-      </div>
-    );
+      );
   }
-
 }
-
-export default ContentCard;
---
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/authorized-content/interfaces.ts
@@ -0,0 +1,23 @@
+﻿export interface Filterable {
+  filterString: string;
+}
+
+export interface ContentItem {
+  id: number;
+  name: string;
+  description: string;
+  contentURL: string;
+  imageURL?: string;
+  userguideURL?: string;
+  releaseNotesURL?: string;
+}
+
+export interface ContentItemGroup {
+  id: number;
+  name: string;
+  items: ContentItem[];
+}
+
+export interface ContentItemGroupList {
+  groups: ContentItemGroup[];
+}
