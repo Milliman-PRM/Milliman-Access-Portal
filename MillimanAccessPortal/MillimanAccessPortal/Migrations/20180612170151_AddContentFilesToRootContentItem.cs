d4b3802e80b21d62e4a2014f0fd23d24680acb92
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-06-26 11:11:14 -0400

    Recommit changes that were lost in 5e2338f
    These changes were originally made as commits 3cf89b4 - 15d1d65.

---
+++ MillimanAccessPortal/MillimanAccessPortal/Migrations/20180612170151_AddContentFilesToRootContentItem.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MillimanAccessPortal.Migrations
+{
+    public partial class AddContentFilesToRootContentItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContentFiles",
+                table: "RootContentItem",
+                type: "jsonb",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentFiles",
+                table: "RootContentItem");
+        }
+    }
+}
--- MillimanAccessPortal/MillimanAccessPortal/Models/AuthorizedContentViewModels/AuthorizedContentViewModel.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Models/AuthorizedContentViewModels/AuthorizedContentViewModel.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using MapDbContextLib.Context;
+using MapDbContextLib.Identity;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace MillimanAccessPortal.Models.AuthorizedContentViewModels
 {
@@ -11,22 +12,58 @@ namespace MillimanAccessPortal.Models.AuthorizedContentViewModels
     /// </summary>
     public class AuthorizedContentViewModel
     {
-        public class ParentClientTree
+        public static AuthorizedContentViewModel Build(ApplicationDbContext dbContext, ApplicationUser user)
         {
-            public long Id;
-            public long? ParentId;
-            public string Name;
+            // All selection groups of which the current user is a member 
+            var selectionGroupsQuery = dbContext.UserInSelectionGroup
+                .Where(usg => usg.UserId == user.Id)
+                .Select(usg => usg.SelectionGroup);
+
+            var selectionGroups = selectionGroupsQuery
+                .Include(sg => sg.RootContentItem)
+                .ToList();
+            var clients = selectionGroupsQuery
+                .Select(sg => sg.RootContentItem.Client)
+                .ToHashSet();
+
+            return new AuthorizedContentViewModel
+            {
+                ItemGroups = clients.Select(c => new ContentItemGroup
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    Items = selectionGroups.Where(sg => sg.RootContentItem.ClientId == c.Id).Select(sg => new ContentItem
+                    {
+                        Id = sg.Id,
+                        Name = sg.RootContentItem.ContentName,
+                        Description = sg.RootContentItem.Description,
+                        ImageURL = sg.RootContentItem.ContentFilesList.SingleOrDefault(f => f.FilePurpose == "Thumbnail")?.FullPath,
+                        ContentURL = sg.ContentInstanceUrl,
+                        UserguideURL = sg.RootContentItem.ContentFilesList.SingleOrDefault(f => f.FilePurpose == "UserGuide")?.FullPath,
+                        ReleaseNotesURL = sg.RootContentItem.ContentFilesList.SingleOrDefault(f => f.FilePurpose == "ReleaseNotes")?.FullPath,
+                    }).ToList(),
+                }).ToList(),
+            };
         }
 
-        public long UserGroupId { get; set; }
+        public List<ContentItemGroup> ItemGroups { get; set; }
+    }
 
-        public string Url { get; set; }
+    public class ContentItemGroup
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public List<ContentItem> Items { get; set; }
+    }
 
-        public string ContentName { get; set; }
-
-        /// <summary>
-        /// First element should be the root level Client, list order traces down to the Client directly referenced by the SelectionGroup
-        /// </summary>
-        public List<ParentClientTree> ClientList { get; set; }
+    public class ContentItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+        public string ContentURL { get; set; }
+        public string UserguideURL { get; set; }
+        public string ReleaseNotesURL { get; set; }
     }
 }
