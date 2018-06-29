35628e0f5591b663fc008c1adfa0de752fb4790f
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-06-29 10:07:33 -0400

    Add missing semicolon

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/login.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/login.ts
@@ -4,4 +4,4 @@ require('jquery-validation-unobtrusive');
 require('bootstrap/scss/bootstrap-reboot.scss');
 require('../scss/map.scss');
 
-require('../images/map-logo.svg')
+require('../images/map-logo.svg');
