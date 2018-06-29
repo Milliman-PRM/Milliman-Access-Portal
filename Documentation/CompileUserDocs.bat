46a5783b997c22d32e85fe6ef5ef69ef8840e326
Michael Reisz <Michael.Reisz@milliman.com>
2018-06-29 10:33:47 -0400

    Update the compilation script
    Take advantage of the header, yaml, and css files during compilation.

--- Documentation/CompileUserDocs.bat
+++ Documentation/CompileUserDocs.bat
@@ -14,7 +14,7 @@ if not exist "../MillimanAccessPortal/MillimanAccessPortal/wwwroot/Documentation
 REM Compile Release Notes
 pandoc --self-contained --metadata=pagetitle:"Milliman Access Portal - Release Notes" -o "../MillimanAccessPortal/MillimanAccessPortal/wwwroot/Documentation/ReleaseNotes.html" "ReleaseNotes.md"
 
-REM Compile all user guides
+Compile all user guides
 for %%x in (
 	AuthorizedContent
 	AccountSettings
@@ -25,13 +25,8 @@ for %%x in (
 	) do (
 		echo Compiling documentation for %%x
 		
-		REM echo Compiling HTML version
-		REM pandoc -o "%%x_Header.html" "%%x_Header.md" 
-		REM pandoc --self-contained --css=".\CSS\style.css" -B "%%x_Header.html" --toc -o "%%x.html" "%%x.yaml" "%%x.md"
-		REM DEL "%%x_Header.html"
-
-		echo Compiling PDF version
-		pandoc -o "%%x_Header.tex" "%%x_Header.md"
-		pandoc -s -V geometry:margin=1in --variable mainfont="Arial" --variable linkcolor="Blue" --pdf-engine=xelatex -B "%%x_Header.tex" --toc -o "../MillimanAccessPortal/MillimanAccessPortal/wwwroot/Documentation/%%x.pdf" "%%x.md"
-		DEL "%%x_Header.tex"
+		echo Compiling HTML version
+		pandoc -o "%%x_Header.html" "%%x_Header.md" 
+		pandoc --self-contained --css=".\CSS\style.css" -B "%%x_Header.html" --toc -o "../MillimanAccessPortal/MillimanAccessPortal/wwwroot/Documentation/%%x.html" "%%x.yaml" "%%x.md"
+		DEL "%%x_Header.html"
 	)
