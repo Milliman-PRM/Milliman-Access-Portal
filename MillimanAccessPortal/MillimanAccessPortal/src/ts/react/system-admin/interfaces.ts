dad8f89ba395396273b8236891055254a3000ff3
Michael Reisz <Michael.Reisz@milliman.com>
2018-07-03 11:40:25 -0400

    Fix format of the interface for System Admin State

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/system-admin/interfaces.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/system-admin/interfaces.ts
@@ -1,9 +1,9 @@
 ﻿export interface SystemAdminState {
-  primaryColContent: string,
-  primaryColSelection?: string,
-  primaryColFilter?: string,
-  secondaryColContent?: string,
-  secondaryColSelection?: string,
-  secondaryColFilter?: string,
-  addUserDialog: boolean
+  primaryColContent: string;
+  primaryColSelection?: string;
+  primaryColFilter?: string;
+  secondaryColContent?: string;
+  secondaryColSelection?: string;
+  secondaryColFilter?: string;
+  addUserDialog: boolean;
 }
