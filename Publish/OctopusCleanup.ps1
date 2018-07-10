257aaf9898e8a4e465083bd8bb883835bd0a8aa3
Ben Wyatt <Ben.Wyatt@milliman.com>
2018-07-10 11:00:50 -0400

    Create OctopusCleanup script

---
+++ Publish/OctopusCleanup.ps1
@@ -0,0 +1,44 @@
+# Code Owners: Ben Wyatt, Steve Gredell
+
+### OBJECTIVE:
+#  Deploy Milliman Access Portal CI builds to Azure
+
+### DEVELOPER NOTES:
+#
+
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$SiteName,
+    [Parameter(Mandatory=$true)]
+    [string]$AppName,
+    [Parameter(Mandatory=$true)]
+    [string]$PoolName,
+    [Parameter(Mandatory=$true)]
+    [string]$FolderToRemove
+)
+
+Import-Module WebAdministration
+
+# Remove web app
+
+$webApp = Get-WebApplication -Name "$AppName" -site "$SiteName"
+
+if ($webApp)
+{
+    write-output "Removing web application"
+    Remove-WebApplication -Name "$AppName" -Site "$SiteName"
+}
+
+# Delete deployed files
+
+if (test-path $FolderToRemove)
+{
+    write-output "Deleting files"
+    Get-ChildItem -Path $FolderToRemove -Recurse | Remove-Item
+
+    Remove-Item $FolderToRemove
+}
+
+# Remove app pool
+
+Remove-WebAppPool -name $PoolName
