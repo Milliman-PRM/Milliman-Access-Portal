7029f3cbcbdfd140049a72ea2e28ce4151edfdd8
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-07-12 14:12:08 -0400

    Create AuditLog migration

--- MillimanAccessPortal/AuditLogLib/Migrations/20180712180957_RemoveSummary.Designer.cs
+++ MillimanAccessPortal/AuditLogLib/Migrations/20180712180957_RemoveSummary.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace AuditLogLib.Migrations
 {
     [DbContext(typeof(AuditLogDbContext))]
-    partial class AuditLogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180712180957_RemoveSummary")]
+    partial class RemoveSummary
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
