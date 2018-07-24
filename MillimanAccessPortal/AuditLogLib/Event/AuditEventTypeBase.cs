58ac52827d2a243208d632be06279599aac7c381
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-07-24 13:30:34 -0400

    Log relative path
    There is a lot of hard coded logic in this commit, but it was determined to be a worthwhile tradeoff for the reduced logging.

--- MillimanAccessPortal/AuditLogLib/Event/AuditEventTypeBase.cs
+++ MillimanAccessPortal/AuditLogLib/Event/AuditEventTypeBase.cs
@@ -1,11 +1,36 @@
 using System;
+using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace AuditLogLib.Event
 {
     public abstract class AuditEventTypeBase
     {
+        private static string _baseName = null;
+        private static string BaseName
+        {
+            get
+            {
+                if (_baseName == null)
+                {
+                    _baseName = GetBaseName();
+                }
+                return _baseName;
+            }
+        }
+
         protected readonly int id;
         protected readonly string name;
+
+        private static string GetBaseName([CallerFilePath] string callerPath = "")
+        {
+            var depth = 4;  // based on this file's location within the repository
+            for (var i = 0; i < depth; i += 1)
+            {
+                callerPath = Path.GetDirectoryName(callerPath);
+            }
+            return callerPath;
+        }
 
         public AuditEventTypeBase(int id, string name)
         {
@@ -15,11 +40,15 @@ namespace AuditLogLib.Event
 
         protected AuditEvent ToEvent(string callerName, string callerPath, int callerLine)
         {
+            // Path.GetRelativePath is not available in .NET Standard 2.0
+            // Use a naive approach instead that works for the common case
+            var relativeCallerPath = callerPath.Replace(BaseName, "");
+
             return new AuditEvent
             {
                 TimeStampUtc = DateTime.UtcNow,
                 EventType = name,
-                Source = $"{callerPath}:{callerLine} {callerName}",
+                Source = $"{relativeCallerPath}:{callerLine} {callerName}",
             };
         }
 
