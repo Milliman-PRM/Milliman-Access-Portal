bfba5e76e5bf617fd359bab124aeb43b14561227
Michael Reisz <Michael.Reisz@milliman.com>
2018-07-30 22:02:16 -0400

    Reconfigure the Password Reset page
    Reconfigure the stock reset password view and model.

--- MillimanAccessPortal/MillimanAccessPortal/Models/AccountViewModels/ResetPasswordViewModel.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Models/AccountViewModels/ResetPasswordViewModel.cs
@@ -9,19 +9,19 @@ namespace MillimanAccessPortal.Models.AccountViewModels
     public class ResetPasswordViewModel
     {
         [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+        public long UserID { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        public string Token { get; set; }
+
+        [Required]
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Confirm Password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
-
-        public string Code { get; set; }
+        public string ConfirmNewPassword { get; set; }
     }
 }
