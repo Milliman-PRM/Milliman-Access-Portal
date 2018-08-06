ddb2a39cdf13e9370ee3ee1a790a4b484b2877e4
Ben Wyatt <Ben.Wyatt@milliman.com>
2018-08-06 15:58:43 -0400

    Stub implementation of PasswordHistory class
    Provide the outline for the structure to be provided to the new password history property

---
+++ MillimanAccessPortal/MapDbContextLib/Models/PasswordHistory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapDbContextLib.Models
+{
+    public class PasswordHistory
+    {
+
+        public string uniqueSalt { get; set; }
+
+        public string algorithmUsed { get; set; }
+
+        public string hash { get; set; }
+
+        public DateTimeOffset dateSet {get; set;}
+
+        /// <summary>
+        /// When a password is provided to the constructor, 
+        /// automatically build the other properties
+        /// </summary>
+        /// <param name="passwordArg"></param>
+        public PasswordHistory(string passwordArg)
+        {
+            dateSet = DateTimeOffset.UtcNow;
+
+            uniqueSalt = generateSalt();
+
+            hash = calculateHash(uniqueSalt, passwordArg);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string generateSalt()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string calculateHash (string saltArg, string passwordArg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
