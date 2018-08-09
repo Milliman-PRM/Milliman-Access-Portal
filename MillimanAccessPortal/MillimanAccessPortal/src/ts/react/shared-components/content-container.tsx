c1a9a00b3df36e70b9c06630793e10d4fac5caf1
Michael Reisz <Michael.Reisz@milliman.com>
2018-08-09 14:05:32 -0400

    Change the close text to an icon

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/content-container.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/content-container.tsx
@@ -4,11 +4,28 @@ import * as React from 'react';
 
 import { ContentContainerProps } from './interfaces';
 
+import 'tooltipster';
+import 'tooltipster/src/css/tooltipster.css';
+
+require('../../../images/cancel.svg');
+
+
 export class ContentContainer extends React.Component<ContentContainerProps, {}> {
   public render() {
     return (
       <div id="iframe-container">
-        <span id="close-content-container" onClick={() => this.props.closeAction(null)}>Close</span>
+        <div
+          id="close-content-container"
+          className='tooltip'
+          title="Close"
+          onClick={(event) => {
+            event.stopPropagation();
+            this.props.closeAction(null);
+          }}>
+          <svg>
+            <use xlinkHref='#cancel' />
+          </svg>
+        </div>
         <iframe id="content-iframe" src={this.props.contentURL}></iframe>
       </div>
     );
