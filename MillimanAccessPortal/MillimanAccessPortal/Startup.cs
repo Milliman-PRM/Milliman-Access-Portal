6e18b6186f74f99bce49256734f1e29b3b066e68
Ben Wyatt <Ben.Wyatt@milliman.com>
2018-08-14 09:31:50 -0400

    Retrieve password history time limit from configuration

--- MillimanAccessPortal/MillimanAccessPortal/Startup.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Startup.cs
@@ -70,14 +70,16 @@ namespace MillimanAccessPortal
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(appConnectionString, b => b.MigrationsAssembly("MillimanAccessPortal")));
             #endregion
-            
+
+            int passwordHistoryDays = Configuration.GetValue<int>("PasswordHistoryValidatorDays");
+
             // Do not add AuditLogDbContext.  This context should be protected from direct access.  Use the api class instead.  -TP
 
             services.AddIdentity<ApplicationUser, ApplicationRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders()
                 .AddTop100000PasswordValidator<ApplicationUser>()
-                .AddPasswordValidator<PasswordRecentDaysValidator<ApplicationUser>>();
+                .AddRecentPasswordInDaysValidator<ApplicationUser>(passwordHistoryDays);
 
             services.Configure<PasswordHasherOptions>(options => options.IterationCount = 100_000);
 
