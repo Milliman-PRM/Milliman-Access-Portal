80dd41b6d04cf35b7c1a3039a089e4996e248a14
Ben Wyatt <Ben.Wyatt@milliman.com>
2018-08-14 09:31:38 -0400

    Create IdentityBuilderExtensions class

---
+++ MillimanAccessPortal/MillimanAccessPortal/Services/IdentityBuilderExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using MapDbContextLib.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MillimanAccessPortal.Services
+{
+    public static class IdentityBuilderExtensions
+    {
+        public static IdentityBuilder AddRecentPasswordCountValidator<TUser>(this IdentityBuilder builder, int numberOfPasswordsArg)
+            where TUser : ApplicationUser
+        {
+            if (builder == null) { throw new ArgumentNullException(nameof(builder)); }
+
+            var validator = new PasswordRecentNumberValidator<TUser>();
+            validator.numberOfPasswords = numberOfPasswordsArg;
+
+            builder.Services.AddSingleton(typeof(IPasswordValidator<>).MakeGenericType(builder.UserType), validator);
+
+            return builder;
+        }
+
+        public static IdentityBuilder AddRecentPasswordInDaysValidator<TUser>(this IdentityBuilder builder, int numberOfDaysArg)
+            where TUser : ApplicationUser
+        {
+            if (builder == null) { throw new ArgumentNullException(nameof(builder)); }
+
+            var validator = new PasswordRecentDaysValidator<TUser>();
+            validator.numberOfDays = numberOfDaysArg;
+
+            builder.Services.AddSingleton(typeof(IPasswordValidator<>).MakeGenericType(builder.UserType), validator);
+
+            return builder;
+        }
+
+        public static IdentityBuilder AddPasswordEverUsedValidator<TUser>(this IdentityBuilder builder)
+            where TUser : ApplicationUser
+        {
+            if (builder == null) { throw new ArgumentNullException(nameof(builder)); }
+
+            return builder.AddPasswordValidator<PasswordHistoryValidator<TUser>>();
+        }
+    }
+}
