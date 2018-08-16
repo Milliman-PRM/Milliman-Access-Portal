a01ca9987e99a77334d62d91996753108e93b034
Ben Wyatt <Ben.Wyatt@milliman.com>
2018-08-16 14:17:48 -0400

    Refactor PasswordIsEmailOrUsername validator
    Allows covering the first setting of a password - likely only applies to /Account/Register

--- MillimanAccessPortal/MillimanAccessPortal/Services/PasswordIsEmailOrUsernameValidator.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Services/PasswordIsEmailOrUsernameValidator.cs
@@ -14,11 +14,25 @@ namespace MillimanAccessPortal.Services
         {
             string upperPassword = password.ToUpper();
             
-            if (upperPassword.Contains(user.NormalizedEmail) || upperPassword.Contains(user.NormalizedUserName))
+            // This case covers the user's initial password - May only be used by ~/Account/Register
+            if (string.IsNullOrWhiteSpace(user.NormalizedEmail) || string.IsNullOrWhiteSpace(user.NormalizedUserName))
+            {
+                if (upperPassword.Contains(user.Email.ToUpper()))
+                {
+                    var result = IdentityResult.Failed(new IdentityError
+                    {
+                        Code = "Password Contains Email or Username",
+                        Description = $"Your password cannot contain your email address or username"
+                    });
+
+                    return Task.FromResult(result);
+                }
+            }
+            else if (upperPassword.Contains(user.NormalizedEmail) || upperPassword.Contains(user.NormalizedUserName))
             {
                 var result = IdentityResult.Failed(new IdentityError
                 {
-                    Code = "Password Reuse",
+                    Code = "Password Contains Email or Username",
                     Description = $"Your password cannot contain your email address or username"
                 });
 
