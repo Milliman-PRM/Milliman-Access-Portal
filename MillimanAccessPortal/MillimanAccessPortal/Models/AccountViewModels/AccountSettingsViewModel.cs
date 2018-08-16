d30289a6f1f083dabcc4001811a9d8c3bee94447
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-08-16 16:01:15 -0400

    Send username with account and password updates

--- MillimanAccessPortal/MillimanAccessPortal/Models/AccountViewModels/AccountSettingsViewModel.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Models/AccountViewModels/AccountSettingsViewModel.cs
@@ -8,29 +8,23 @@ namespace MillimanAccessPortal.Models.AccountViewModels
 {
     public class AccountSettingsViewModel
     {
-        [Required]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
-        [Required]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
-        [Required]
         [Display(Name = "Username")]
         public string UserName { get; set; }
 
-        [Required]
         [EmailAddress]
         [Display(Name = "Email Address")]
         public string Email { get; set; }
 
-        [Required]
         [Phone]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
 
-        [Required]
         public string Employer { get; set; }
 
         [DataType(DataType.Password)]
