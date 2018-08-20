86edfd5faeec9c2b8497185047bb5765b27ff4d0
Michael Reisz <Michael.Reisz@milliman.com>
2018-08-20 09:05:35 -0400

    Fetch data for the navbar.
    Use the fetch API to fetch the necessary data during the componentDidMount life cycle hook.

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/navbar.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/navbar.tsx
@@ -1,10 +1,30 @@
 ﻿import '../../../scss/react/shared-components/navbar.scss';
 
 import * as React from 'react';
+import 'whatwg-fetch';
+import 'promise-polyfill/src/polyfill';
 
 import { NavBarProps, NavBarState } from './interfaces';
 
-export class ActionIcon extends React.Component<NavBarProps, NavBarState> {
+export class NavBar extends React.Component<NavBarProps, NavBarState> {
+  public constructor(props) {
+    super(props);
+    this.state = {
+      NavBarElements: [],
+    }
+  }  
+
+  public componentDidMount() {
+    const elements = fetch('/Account/NavBarElements', {
+      credentials: 'include'
+      }).then(response => response.json()
+       ).then(json => {
+         this.setState({ NavBarElements: json });
+      }).catch(ex => {
+        console.log('parsing failed', ex)
+      });
+  }
+  
   public render() {
     return (
       <nav>
