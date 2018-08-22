2738a4ddbc38c71c603063c881566818852dfccb
Michael Reisz <Michael.Reisz@milliman.com>
2018-08-22 10:28:56 -0400

    Fix a build issue with the system admin controller tests

--- MillimanAccessPortal/MapTests/SystemAdminControllerTests.cs
+++ MillimanAccessPortal/MapTests/SystemAdminControllerTests.cs
@@ -45,7 +45,9 @@ namespace MapTests
                 _testResources.MessageQueueServicesObject,
                 _testResources.LoggerFactory,
                 _testResources.AuditLoggerObject,
-                _testResources.QueriesObj);
+                _testResources.QueriesObj,
+                _testResources.AuthorizationService,
+                _testResources.ConfigurationObject);
 
             var testController = new SystemAdminController(
                 accountController,
