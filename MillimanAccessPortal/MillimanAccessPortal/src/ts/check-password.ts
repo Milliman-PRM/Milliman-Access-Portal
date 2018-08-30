5b1d4fccc9075c4e88ec3184167d2a56d5adf7e0
Michael Reisz <Michael.Reisz@milliman.com>
2018-08-30 15:22:03 -0400

    Prevent calling again when the input is already validated and unchanged

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/check-password.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/check-password.ts
@@ -12,28 +12,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const proposedPassword = (newPasswordInput as HTMLInputElement).value;
 
     if (proposedPassword) {
-      const antiforgeryToken = document.querySelector('input[name="__RequestVerificationToken"]').getAttribute('value');
+      if (!newPasswordInput.hasAttribute('Validated')) {
+        const antiforgeryToken = document.querySelector('input[name="__RequestVerificationToken"]').getAttribute('value');
 
-      fetch('/Account/CheckPasswordValidity', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'RequestVerificationToken': antiforgeryToken,
-        },
-        credentials: 'same-origin',
-        body: JSON.stringify({ ProposedPassword: proposedPassword }),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            toastr.warning(response.headers.get('Warning') || 'Unknown error');
-            newPasswordInput.removeAttribute('Validated');
-            newPasswordInput.setAttribute('Warning', 'Warning');
-          } else {
-            newPasswordInput.removeAttribute('Warning');
-            newPasswordInput.setAttribute('Validated', 'Validated');
-          }
-        });
+        fetch('/Account/CheckPasswordValidity', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'RequestVerificationToken': antiforgeryToken,
+          },
+          credentials: 'same-origin',
+          body: JSON.stringify({ ProposedPassword: proposedPassword }),
+        })
+          .then((response) => {
+            if (!response.ok) {
+              toastr.warning(response.headers.get('Warning') || 'Unknown error');
+              newPasswordInput.removeAttribute('Validated');
+              newPasswordInput.setAttribute('Warning', 'Warning');
+            } else {
+              newPasswordInput.removeAttribute('Warning');
+              newPasswordInput.setAttribute('Validated', 'Validated');
+            }
+          });
+      }
     } else {
       newPasswordInput.removeAttribute('Validated');
       newPasswordInput.removeAttribute('Warning');
