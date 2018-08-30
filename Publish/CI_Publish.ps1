0cb94adea2a087ad4885d15be41479147a8f25cb
Ben Wyatt <Ben.Wyatt@milliman.com>
2018-08-30 09:44:53 -0400

    Grab re-named variable for branch name

--- Publish/CI_Publish.ps1
+++ Publish/CI_Publish.ps1
@@ -117,7 +117,7 @@ Get-Variable |%{ "Name : {0}`r`nValue: {1}`r`n" -f $_.Name,$_.Value }
 Get-ChildItem env:
 
 #region Configure environment properties
-$BranchName = $env:git_branch # Will be used in the version string of the octopus package & appended to database names
+$BranchName = $env:git_branch_name # Will be used in the version string of the octopus package & appended to database names
 
 $gitExePath = "git"
 $psqlExePath = "L:\Hotware\Postgresql\v9.6.2\psql.exe"
