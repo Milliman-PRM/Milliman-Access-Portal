3e22e7c96c2264be8d018b6e8d27645201bfe0e9
Michael Reisz <Michael.Reisz@milliman.com>
2018-08-30 10:58:18 -0400

    Add a check-password script to do password checking
    This script will perform password checking.

---
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/check-password.ts
@@ -0,0 +1,55 @@
+﻿import toastr = require('toastr');
+
+import './lib-options';
+
+require('toastr/toastr.scss');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const newPasswordInput = document.getElementById('NewPassword');
+  const confirmPasswordInput = document.getElementById('ConfirmNewPassword');
+
+  newPasswordInput.addEventListener('blur', () => {
+    const proposedPassword = (newPasswordInput as HTMLInputElement).value;
+
+    if (proposedPassword) {
+      const antiforgeryToken = document.querySelector('input[name="__RequestVerificationToken"]').getAttribute('value');
+
+      fetch('/Account/CheckPasswordValidity', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'RequestVerificationToken': antiforgeryToken,
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify({ ProposedPassword: proposedPassword }),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            toastr.warning(response.headers.get('Warning') || 'Unknown error');
+            newPasswordInput.removeAttribute('Validated');
+            newPasswordInput.setAttribute('Warning', 'Warning');
+            newPasswordInput.focus();
+          } else {
+            newPasswordInput.removeAttribute('Warning');
+            newPasswordInput.setAttribute('Validated', 'Validated');
+          }
+        });
+    } else {
+      newPasswordInput.removeAttribute('Validated');
+      newPasswordInput.removeAttribute('Warning');
+    }
+  });
+
+  confirmPasswordInput.addEventListener('keyup', () => {
+    const proposedPassword = (newPasswordInput as HTMLInputElement).value;
+    const confirmPassword = (confirmPasswordInput as HTMLInputElement).value;
+
+    if (confirmPassword === proposedPassword && newPasswordInput.hasAttribute('Validated')) {
+      confirmPasswordInput.setAttribute('Validated', 'Validated');
+    } else {
+      confirmPasswordInput.removeAttribute('Validated');
+    }
+  });
+
+});
