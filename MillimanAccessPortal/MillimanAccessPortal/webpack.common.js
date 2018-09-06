0c8f6ce1cd24d6d0536a87e7ad5440d3d584d4d0
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-09-06 11:41:50 -0400

    Add development and production webpack configurations
    Missed these in ff29e1f thanks to .gitignore

---
+++ MillimanAccessPortal/MillimanAccessPortal/webpack.common.js
@@ -0,0 +1,103 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+
+module.exports = {
+  entry: {
+    'create-initial-user': './src/js/create-initial-user.js',
+    'forgot-password': './src/js/forgot-password.js',
+    'forgot-password-confirmation': './src/js/forgot-password-confirmation.js',
+    'reset-password': './src/js/reset-password.js',
+    'enable-account': './src/js/enable-account.js',
+    'account-settings': './src/js/account-settings.js',
+    'client-admin': './src/js/client-admin.js',
+    'content-access-admin': './src/js/content-access-admin/index.js',
+    'content-publishing': './src/js/content-publishing/index.js',
+    'authorized-content': './src/js/react/authorized-content/index.js',
+    'login': './src/js/login.js',
+    'system-admin': './src/js/react/system-admin/index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-sprite-loader',
+            options: {
+              extract: false
+            }
+          },
+          {
+            loader: 'svgo-loader',
+            options: {
+              plugins: [
+                { removeTitle: true },
+                { convertColors: { currentColor: true } },
+                { convertPathData: false },
+                { cleanupIDs: { remove: false }}
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'less-loader' },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'wwwroot', 'js'),
+    filename: '[name].bundle.js',
+    publicPath: '/wwwroot/',
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: 'src/favicon.ico',
+        to: '../favicon.ico',
+      },
+      {
+        from: 'src/images/default_content_images/',
+        to: '../images/',
+        flatten: true,
+      },
+    ]),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+    new webpack.NamedModulesPlugin(),
+    new SpriteLoaderPlugin({
+      plainSprite: true
+    }),
+  ],
+  resolve: {
+    extensions: [
+      '.webpack.js',
+      '.web.js',
+      '.js',
+    ],
+  },
+};
