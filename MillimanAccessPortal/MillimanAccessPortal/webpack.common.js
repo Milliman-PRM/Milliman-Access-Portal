85e4e70adec40447e58c2e1f3f99cdc9736202da
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-09-10 17:03:26 -0400

    Template razor pages

--- MillimanAccessPortal/MillimanAccessPortal/webpack.common.js
+++ MillimanAccessPortal/MillimanAccessPortal/webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -22,7 +23,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'wwwroot', 'js'),
     filename: '[name].bundle.js',
-    publicPath: '/wwwroot/',
+    publicPath: '~/js/',
   },
   module: {
     rules: [
@@ -69,6 +70,79 @@ module.exports = {
     new webpack.NamedModulesPlugin(),
     new SpriteLoaderPlugin({
       plainSprite: true
+    }),
+    // Separate plugin instance for each entry point
+    new HtmlWebpackPlugin({
+      filename: '../../Views/Account/CreateInitialUser.cshtml',
+      template: 'Views/Account/templates/CreateInitialUser.cshtml',
+      inject: false,
+      chunks: [ 'commons', 'create-initial-user' ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../../Views/Account/ForgotPassword.cshtml',
+      template: 'Views/Account/templates/ForgotPassword.cshtml',
+      inject: false,
+      chunks: [ 'commons', 'forgot-password' ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../../Views/Account/ForgotPasswordConfirmation.cshtml',
+      template: 'Views/Account/templates/ForgotPasswordConfirmation.cshtml',
+      inject: false,
+      chunks: [ 'commons', 'forgot-password-confirmation' ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../../Views/Account/ResetPassword.cshtml',
+      template: 'Views/Account/templates/ResetPassword.cshtml',
+      inject: false,
+      chunks: [ 'commons', 'reset-password' ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../../Views/Account/EnableAccount.cshtml',
+      template: 'Views/Account/templates/EnableAccount.cshtml',
+      inject: false,
+      chunks: [ 'commons', 'enable-account' ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../../Views/Account/AccountSettings.cshtml',
+      template: 'Views/Account/templates/AccountSettings.cshtml',
+      inject: false,
+      chunks: [ 'commons', 'account-settings' ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../../Views/ClientAdmin/Index.cshtml',
+      template: 'Views/ClientAdmin/templates/Index.cshtml',
+      inject: false,
+      chunks: [ 'commons', 'client-admin' ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../../Views/ContentAccessAdmin/Index.cshtml',
+      template: 'Views/ContentAccessAdmin/templates/Index.cshtml',
+      inject: false,
+      chunks: [ 'commons', 'content-access-admin' ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../../Views/ContentPublishing/Index.cshtml',
+      template: 'Views/ContentPublishing/templates/Index.cshtml',
+      inject: false,
+      chunks: [ 'commons', 'content-publishing' ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../../Views/AuthorizedContent/Index.cshtml',
+      template: 'Views/AuthorizedContent/templates/Index.cshtml',
+      inject: false,
+      chunks: [ 'commons', 'authorized-content' ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../../Views/Account/Login.cshtml',
+      template: 'Views/Account/templates/Login.cshtml',
+      inject: false,
+      chunks: [ 'commons', 'login' ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: '../../Views/SystemAdmin/Index.cshtml',
+      template: 'Views/SystemAdmin/templates/Index.cshtml',
+      inject: false,
+      chunks: [ 'commons', 'system-admin' ],
     }),
   ],
   resolve: {
