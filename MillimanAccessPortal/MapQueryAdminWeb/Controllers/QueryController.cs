e8f1889ce98bed02cd4fa4220e50a0bf8997a581
Ben Wyatt <Ben.Wyatt@milliman.com>
2018-09-18 15:32:23 -0400

    Begin setup for browser-based query GUI

---
+++ MillimanAccessPortal/MapQueryAdminWeb/Controllers/QueryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuditLogLib.Services;
+using MapQueryAdminWeb.Pages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MapQueryAdminWeb.Controllers
+{
+    public class QueryController : Controller
+    {
+        public IAuditLogger _logger;
+
+        public QueryController (IAuditLogger loggerArg)
+        {
+            _logger = loggerArg;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(QueryModel model)
+        {
+
+
+            return View();
+        }
+    }
+}
