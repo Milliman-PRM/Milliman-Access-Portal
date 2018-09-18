ffafbb88fd38a9731020043f4dbc30eb6c62011b
Ben Wyatt <Ben.Wyatt@milliman.com>
2018-09-18 09:33:49 -0400

    Implement building connection strings from configuration

--- MillimanAccessPortal/MapQueryAdmin/Utilities.cs
+++ MillimanAccessPortal/MapQueryAdmin/Utilities.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Configuration;
+using System;
+using Npgsql;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MapQueryAdmin
 {
@@ -38,13 +42,83 @@ namespace MapQueryAdmin
 
         /// <summary>
         /// Returns the application database's connection string, with the username and password replaced with the credentials provided by the user.
+        /// If the database name is set in an environment variable, use that database name instead
         /// </summary>
         /// <param name="username">The username to be used in the connection string</param>
         /// <param name="password">The user's postgresql password</param>
         /// <returns></returns>
         public static string getAppDbConnectionString(string username, string password)
         {
-            throw new NotImplementedException();
+            ConfigurationRoot config = getConfiguration();
+
+            NpgsqlConnectionStringBuilder builder = new NpgsqlConnectionStringBuilder(config.GetConnectionString("DefaultConnection"));
+
+            // Set database name if it exists in environment variables
+            string dbNameVar = Environment.GetEnvironmentVariable("APP_DATABASE_NAME"); // Returns null if the variable is undefined
+            if (!string.IsNullOrWhiteSpace(dbNameVar))
+            {
+                builder.Database = dbNameVar;
+            }
+
+            // Set username & password
+            builder.Username = username;
+            builder.Password = password;
+
+            return builder.ConnectionString;
+        }
+
+        /// <summary>
+        /// Gets a copy of the audit log database connection string
+        /// If the database name is set in an environment variable, use that name
+        /// </summary>
+        /// <returns></returns>
+        public static string getAuditLogDbConnectionString()
+        {
+            ConfigurationRoot config = getConfiguration();
+
+            // Set database name if it exists in environment variables
+            string dbNameVar = Environment.GetEnvironmentVariable("AUDIT_LOG_DATABASE_NAME"); // Returns null if the variable is undefined
+            if (!string.IsNullOrWhiteSpace(dbNameVar))
+            {
+                NpgsqlConnectionStringBuilder builder = new NpgsqlConnectionStringBuilder(config.GetConnectionString("AuditLogConnectionString"));
+                builder.Database = dbNameVar;
+                return builder.ConnectionString;
+            }
+            else
+            {
+                return config.GetConnectionString("AuditLogConnectionString");
+            }
+        }
+
+        /// <summary>
+        /// Builds a configuration object and returns it to the caller
+        /// </summary>
+        /// <returns></returns>
+        public static ConfigurationRoot getConfiguration()
+        {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+
+            builder.AddJsonFile("appsettings.json", false)
+                .AddJsonFile($"appsettings.{getEnvironmentName()}.json", true)
+                .AddJsonFile($"AzureKeyVault.{getEnvironmentName()}.json", true);
+
+            // If a key vault URL was defined, add Azure Key Vault configuration values
+            IConfigurationRoot root = builder.Build();
+            string vaultUrl = root["AzureVaultName"];
+            if (!string.IsNullOrWhiteSpace(vaultUrl))
+            {
+                var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+                store.Open(OpenFlags.ReadOnly);
+                var cert = store.Certificates.Find(X509FindType.FindByThumbprint, root["AzureCertificateThumbprint"], false);
+
+                builder.AddAzureKeyVault(
+                    root["AzureVaultName"],
+                    root["AzureClientID"],
+                    cert.OfType<X509Certificate2>().Single()
+                    );
+            }
+
+            return builder.Build() as ConfigurationRoot;
         }
     }
 }
