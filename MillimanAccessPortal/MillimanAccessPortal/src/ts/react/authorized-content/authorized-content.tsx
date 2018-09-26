9f3c4988c9cf202402cab99e7868bd81f3983275
Michael Reisz <Michael.Reisz@milliman.com>
2018-09-26 13:57:26 -0400

    Add a comment explaining why we are handling IE differently.

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/authorized-content/authorized-content.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/authorized-content/authorized-content.tsx
@@ -47,13 +47,13 @@ export class AuthorizedContent extends React.Component<{}, AuthorizedContentStat
   }
 
   public selectContentItem = (contentURL: string) => {
-
     this.setState({ selectedContentURL: contentURL }, () => {
       const display = (this.state.selectedContentURL) ? 'none' : null;
       document.getElementById('page-header').style.display = display;
       document.getElementById('page-footer').style.display = display;
       document.getElementById('authorized-content-container').style.display = display;
       if (!contentURL) {
+        // IE doesn't support popstate on URL hashchange so we need to treat it differently here.
         const is_ie = navigator.userAgent.indexOf("MSIE ") > -1 || navigator.userAgent.indexOf("Trident/") > -1;
         if (!is_ie) {
           history.back();
