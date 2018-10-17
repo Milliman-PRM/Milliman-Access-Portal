05a2e144aa55fe103702eb97d92e1507fa3d7853
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-10-17 14:26:40 -0400

    Prevent ForgotPassword from sometimes displaying unique message
    This was a security issue.

--- MillimanAccessPortal/MillimanAccessPortal/Controllers/AccountController.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Controllers/AccountController.cs
@@ -535,14 +535,6 @@ namespace MillimanAccessPortal.Controllers
                     {
                         await SendPasswordResetEmail(user, Url);
                     }
-                    else
-                    {
-                        string EmailBodyText = "Welcome to Milliman Access Portal.  Below is an activation link for your account";
-                        Task DontWaitForMe = Task.Run(() => SendNewAccountWelcomeEmail(user, Url, EmailBodyText));
-
-                        string UserMsg = "Your Milliman Access Portal account has not yet been activated.  A new account welcome email is being sent to you now.  Please use the link in that email to activate your account.";
-                        return View("Message", UserMsg);
-                    }
                 }
                 else
                 {
