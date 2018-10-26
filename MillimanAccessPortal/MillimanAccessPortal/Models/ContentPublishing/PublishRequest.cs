808bc742e9594894ffc7b8920e66bb5ba06b30a3
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-10-26 15:22:54 -0400

    Give file arrays default values
    If the arrays do not have default values, then they will be null instead of empty when they are not supplied. Forms cannot supply empty arrays.

--- MillimanAccessPortal/MillimanAccessPortal/Models/ContentPublishing/PublishRequest.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Models/ContentPublishing/PublishRequest.cs
@@ -15,9 +15,9 @@ namespace MillimanAccessPortal.Models.ContentPublishing
         public Guid RootContentItemId { get; set; }
 
         [Required]
-        public UploadedRelatedFile[] NewRelatedFiles { get; set; }
+        public UploadedRelatedFile[] NewRelatedFiles { get; set; } = new UploadedRelatedFile[0];
 
         [Required]
-        public string[] DeleteFilePurposes { get; set; }
+        public string[] DeleteFilePurposes { get; set; } = new string[0];
     }
 }
