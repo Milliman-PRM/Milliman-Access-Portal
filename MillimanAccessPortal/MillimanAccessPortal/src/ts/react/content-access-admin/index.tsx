d5b6b191f9fd9c31c860f078f988189b4adb65d3
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-10-31 10:50:03 -0400

    Add React index for content access admin

---
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/content-access-admin/index.tsx
@@ -0,0 +1,21 @@
+﻿import '../../../images/map-logo.svg';
+import '../../../scss/map.scss';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { StatusMonitor } from '../../status-monitor';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <div />,
+    document.getElementById('content-container'),
+  );
+});
+
+// const statusMonitor = new StatusMonitor('/Account/SessionStatus', () => null, 60000);
+// statusMonitor.start();
+
+if (module.hot) {
+  module.hot.accept();
+}
