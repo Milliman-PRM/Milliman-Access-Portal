f47216196f09066db1f5e2efbeb1c4140d21b178
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-11-01 14:27:25 -0400

    Move ApplyRetryOperation to separate file
    This function was not file system-specific, so move it out of FileSystemUtil.cs.

--- MillimanAccessPortal/MapCommonLib/FileSystemUtil.cs
+++ MillimanAccessPortal/MapCommonLib/FileSystemUtil.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Threading;
+﻿using System.IO;
 
 namespace MapCommonLib
 {
@@ -15,7 +13,7 @@ namespace MapCommonLib
         /// <param name="baseIntervalMs">Time to wait after initial attempt</param>
         public static void DeleteFileWithRetry(string path, int attempts = 5, int baseIntervalMs = 500)
         {
-            ApplyRetryOperation<IOException, string>(File.Delete, attempts, baseIntervalMs, path);
+            StaticUtil.ApplyRetryOperation<IOException, string>(File.Delete, attempts, baseIntervalMs, path);
         }
 
         /// <summary>
@@ -27,61 +25,7 @@ namespace MapCommonLib
         /// <param name="baseIntervalMs">Time to wait after initial attempt</param>
         public static void DeleteDirectoryWithRetry(string path, int attempts = 5, int baseIntervalMs = 500)
         {
-            ApplyRetryOperation<IOException, string>(p => Directory.Delete(p, true), attempts, baseIntervalMs, path);
-        }
-
-        /// <summary>
-        /// Represents a retriable operation that throws an exception when it should be retried
-        /// </summary>
-        /// <param name="opArg"></param>
-        private delegate void RetryOperation<TException, TArg>(TArg opArg) where TException : Exception;
-
-        /// <summary>
-        /// Try to perform a retriable operation until it succeeds.
-        /// Time before retry increases after each attempt. Retry intervals form a triangular sequence.
-        /// </summary>
-        /// <typeparam name="TException">
-        /// Exception type to monitor. When operation throws an exception of this type, it will be retried.
-        /// </typeparam>
-        /// <typeparam name="TArg"></typeparam>
-        /// <param name="operation">
-        /// Retriable operation to try until success. Only retried when TException is thrown.
-        /// </param>
-        /// <param name="attempts">Times to try the operation before giving up</param>
-        /// <param name="baseIntervalMs">Time to wait after initial attempt</param>
-        /// <param name="opArg">Passed to operation delegate</param>
-        /// <summary>
-        private static void ApplyRetryOperation<TException, TArg>(
-            RetryOperation<TException, TArg> operation, int attempts, int baseIntervalMs, TArg opArg)
-            where TException : Exception
-        {
-            int retryInterval = 0;
-            int attemptNo = 0;
-
-            while (true)
-            {
-                try
-                {
-                    operation(opArg);
-                    break;
-                }
-                catch (TException)
-                {
-                    attemptNo += 1;
-
-                    int attemptsLeft = attempts - attemptNo;
-                    if (attemptsLeft == 0)
-                    {
-                        throw;
-                    }
-                    GlobalFunctions.TraceWriteLine(
-                        $"Failed to apply retry operation with argument '{opArg}'; "
-                        + $"retrying {attemptsLeft} more times...");
-
-                    retryInterval += attemptNo;
-                    Thread.Sleep(retryInterval * baseIntervalMs);
-                }
-            }
+            StaticUtil.ApplyRetryOperation<IOException, string>(p => Directory.Delete(p, true), attempts, baseIntervalMs, path);
         }
     }
 }
