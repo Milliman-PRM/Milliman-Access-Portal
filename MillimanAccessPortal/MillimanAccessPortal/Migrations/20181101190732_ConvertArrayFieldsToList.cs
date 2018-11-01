ad8c52dc8357a4e294f2741f50c33bb68a9b1617
tom.puckett <tom.puckett@milliman.com>
2018-11-01 15:14:34 -0400

    New migration due to updated EF/PG package

---
+++ MillimanAccessPortal/MillimanAccessPortal/Migrations/20181101190732_ConvertArrayFieldsToList.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MillimanAccessPortal.Migrations
+{
+    public partial class ConvertArrayFieldsToList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ContentReductionTask_ContentPublicationRequest_ContentPublicationRequestId",
+                table: "ContentReductionTask");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ContentReductionTask_ContentPublicationRequest_ContentPubli~",
+                table: "ContentReductionTask",
+                column: "ContentPublicationRequestId",
+                principalTable: "ContentPublicationRequest",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ContentReductionTask_ContentPublicationRequest_ContentPubli~",
+                table: "ContentReductionTask");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ContentReductionTask_ContentPublicationRequest_ContentPublicationRequestId",
+                table: "ContentReductionTask",
+                column: "ContentPublicationRequestId",
+                principalTable: "ContentPublicationRequest",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
