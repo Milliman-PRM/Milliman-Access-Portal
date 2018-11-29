0b161b41e3f123986f591f751fcda4f5f03437c2
Michael Reisz <Michael.Reisz@milliman.com>
2018-11-29 16:05:28 -0500

    Move 502 one more time.

--- MillimanAccessPortal/MillimanAccessPortal/webpack.common.js
+++ MillimanAccessPortal/MillimanAccessPortal/webpack.common.js
@@ -67,7 +67,7 @@ module.exports = {
         flatten: true,
       },
       {
-        from: 'src/Error/502.html',
+        from: 'src/html/Error/502.html',
         to: 'Error/502.html',
       },
       {
