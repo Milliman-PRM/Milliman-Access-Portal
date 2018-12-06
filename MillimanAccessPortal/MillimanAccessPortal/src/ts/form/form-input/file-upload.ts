0f6aacbe39d915c40c3f3a9d58256255fb8b581e
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2018-12-06 16:05:49 -0500

    Use match() instead of endsWith() for string evaluation

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/form/form-input/file-upload.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/form/form-input/file-upload.ts
@@ -161,7 +161,7 @@ export class FileUploadInput extends FormInput {
 
   protected validFn = () => {
     const uploadValid = (this.upload && this.upload.valid());
-    const deleteValid = this.value.endsWith('delete') || undefined;
+    const deleteValid = this.value.match(/delete$/) !== null || undefined;
     // return undefined only if there is no upload object and the input is not deletable
     return deleteValid || uploadValid;
   }
