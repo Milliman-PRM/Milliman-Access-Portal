c092eb47182f709fe0f3a2a92c7c302095553089
tom.puckett <tom.puckett@milliman.com>
2018-12-14 10:50:11 -0500

    db migration for nullable FK

---
+++ MillimanAccessPortal/MillimanAccessPortal/Migrations/20181214154719_NullableFkReductionTaskToSelectionGroup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MillimanAccessPortal.Migrations
+{
+    public partial class NullableFkReductionTaskToSelectionGroup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ContentReductionTask_SelectionGroup_SelectionGroupId",
+                table: "ContentReductionTask");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "SelectionGroupId",
+                table: "ContentReductionTask",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ContentReductionTask_SelectionGroup_SelectionGroupId",
+                table: "ContentReductionTask",
+                column: "SelectionGroupId",
+                principalTable: "SelectionGroup",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ContentReductionTask_SelectionGroup_SelectionGroupId",
+                table: "ContentReductionTask");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "SelectionGroupId",
+                table: "ContentReductionTask",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ContentReductionTask_SelectionGroup_SelectionGroupId",
+                table: "ContentReductionTask",
+                column: "SelectionGroupId",
+                principalTable: "SelectionGroup",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
