01d81e76cd9fc8ac2d7b9f33a3b3f5ec4cfbddc8
tom.puckett <tom.puckett@milliman.com>
2018-12-18 12:31:43 -0500

    Revise migration NullableFkReductionTaskToSelectionGroup

---
+++ MillimanAccessPortal/MillimanAccessPortal/Migrations/20181218172624_NullableFkReductionTaskToSelectionGroup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MillimanAccessPortal.Migrations
+{
+    public partial class NullableFkReductionTaskToSelectionGroup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "SelectionGroupId",
+                table: "ContentReductionTask",
+                nullable: true,
+                oldClrType: typeof(Guid));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "SelectionGroupId",
+                table: "ContentReductionTask",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+        }
+    }
+}
