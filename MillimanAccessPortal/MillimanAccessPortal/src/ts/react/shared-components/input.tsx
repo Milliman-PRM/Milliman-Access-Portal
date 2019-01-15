5f3b71ac36c52a33f7bf50d80d0a23b5d129f971
Michael Reisz <Michael.Reisz@milliman.com>
2019-01-15 10:03:29 -0500

    Create an input component
    This component will be used as part of the forthcoming form-base component.

---
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/input.tsx
@@ -0,0 +1,26 @@
+﻿import '../../../scss/react/shared-components/input.scss';
+
+import * as React from 'react';
+
+interface InputProps {
+  name: string;
+  label?: string;
+  error?: string;
+  inputIcon?: string;
+}
+
+const Input = ({ name, label, error, inputIcon, ...rest }: InputProps) => {
+  return (
+    <div className="form-input-container">
+      {label && <label className="form-input-label" htmlFor={name}>{label}</label>}
+      <div className="form-element-container">
+        {inputIcon && <svg className="filter-icon"><use xlinkHref="#{inputIcon}" /></svg>}
+        <input name={name} id={name} className="form-input" {...rest} />
+        {error && <svg className="form-element-error-icon"><use xlinkHref="#warning" /></svg>}
+      </div>
+      {error && <div className="form-element-error-message">{error}</div>}
+    </div>
+  );
+};
+
+export default Input;
