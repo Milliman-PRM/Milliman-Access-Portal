76e3f85f668bfaee6816dd33885de00234b2b6aa
Michael Reisz <Michael.Reisz@milliman.com>
2019-01-22 15:34:30 -0500

    Remove some fluff from base form class

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/form.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/form.tsx
@@ -1,21 +1,23 @@
 ﻿import * as React from 'react';
 const Joi = require('joi-browser');
 import 'promise-polyfill/dist/polyfill';
-import Input from "./input";
 
-import { OptionalInputProps, InputProps } from '../shared-components/interfaces';
-
-class Form extends React.Component {
-  state = {
-    data: {},
-    errors: {}
+export interface BaseFormState {
+  data: {
+    [id: string]: string | boolean;
   };
+  errors: {
+    [id: string]: string;
+  };
+}
+
+export class Form<TProps, TState extends BaseFormState> extends React.Component<TProps, TState> {
 
   schema = {};
 
   componentDidMount() {
     const requestVerificationToken = document.querySelector('input[name="__RequestVerificationToken"]').getAttribute('value');
-    const data = { ...this.state.data };
+    const data = { ...(this.state.data as object) };
     data["__RequestVerificationToken"] = requestVerificationToken;
     this.schema["__RequestVerificationToken"] = Joi.string().required();
     this.setState({ data })
@@ -26,7 +28,9 @@ class Form extends React.Component {
   validate = () => {
     const options = { abortEarly: false };
     const { error } = Joi.validate(this.state.data, this.schema, options);
-    if (!error) return null;
+    if (!error) {
+      return null;
+    }
 
     const errors = {};
     for (let item of error.details) errors[item.path[0]] = item.message;
@@ -51,18 +55,18 @@ class Form extends React.Component {
   };
 
   handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
+    const errors = { ...(this.state.errors as object) };
     const errorMessage = this.validateProperty(input);
     if (!errorMessage) delete errors[input.name];
 
-    const data = { ...this.state.data };
+    const data = { ...(this.state.data as object) };
     data[input.name] = input.value;
 
     this.setState({ data, errors });
   }
 
   handleBlur = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
+    const errors = { ...(this.state.errors as object) };
     const errorMessage = this.validateProperty(input);
     if (errorMessage && input.value !== "") errors[input.name] = errorMessage;
     else delete errors[input.name];
@@ -78,32 +82,4 @@ class Form extends React.Component {
       </button>
     );
   }
-
-  renderInput(name: string, label: string, type: string, attr: OptionalInputProps) {
-    const { data, errors } = this.state;
-
-    return (
-      <Input
-        name={name}
-        label={label}
-        type={type}
-        error={errors[name]}
-        value={data[name]}
-        onChange={this.handleChange}
-        onBlur={this.handleBlur}
-        {...attr}
-      />
-    );
-  }
-
-  renderRequestVerificationToken() {
-    const requestVerificationToken = this.state.data["__RequestVerificationToken"];
-    if (requestVerificationToken) {
-      return (
-        <input name="__RequestVerificationToken" type="hidden" value={this.state.data["__RequestVerificationToken"]} />
-      )
-    }
-  }
 }
-
-export default Form;
