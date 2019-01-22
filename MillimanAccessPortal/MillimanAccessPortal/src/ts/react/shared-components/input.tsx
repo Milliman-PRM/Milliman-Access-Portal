da1528d9a71a2f704a34c6ed2554e359a7e10329
Michael Reisz <Michael.Reisz@milliman.com>
2019-01-22 15:34:03 -0500

    Input improvements

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/input.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/input.tsx
@@ -1,28 +1,39 @@
-﻿import '../../../images/icons/error.svg';
-import '../../../images/icons/show-password.svg';
+﻿import '../../../images/icons/show-password.svg';
 import '../../../images/icons/hide-password.svg';
 
 import '../../../scss/react/shared-components/input.scss';
 
 import * as React from 'react';
-import { InputProps } from './interfaces';
 
-const Input = ({ name, label, placeholderText = null, error = null, inputIcon = null, actionIcon = null, actionIconEvent = null, type = "text", ...rest }: InputProps) => {
-  return (
-    <div className="form-element-container">
-      <div className={"form-element" + (error ? " error" : "")}>
-        {inputIcon && <div className="input-icon-label"><svg className="input-icon"><use xlinkHref={`#${inputIcon}`} /></svg></div>}
-        <div className="form-input-container">
-          <input name={name} id={name} type={type} className="form-input" placeholder={ placeholderText || label } {...rest} />
-          <label className="form-input-label" htmlFor={name}>{label}</label>
+interface InputProps {
+  name: string;
+  label: string;
+  type: string;
+  value: string;
+  onChange: (currentTarget: React.FormEvent<HTMLInputElement>) => void;
+  onBlur: (currentTarget: React.FormEvent<HTMLInputElement>) => void;
+  error: string;
+  placeholderText?: string;
+  autoFocus?: boolean;
+  inputIcon?: string;
+}
+
+export class Input extends React.Component<InputProps, {}> {
+
+  public render() {
+    const { name, label, error, inputIcon, placeholderText, children, ...rest } = this.props;
+    return (
+      <div className="form-element-container">
+        <div className={"form-element" + (error ? " error" : "")}>
+          {inputIcon && <div className="input-icon-label"><svg className="input-icon"><use xlinkHref={`#${inputIcon}`} /></svg></div>}
+          <div className="form-input-container">
+            <input name={name} id={name} className="form-input" placeholder={placeholderText || label} {...rest} />
+            <label className="form-input-label" htmlFor={name}>{label}</label>
+          </div>
+          {children}
         </div>
-        {actionIcon && <div className="action-icon-label" onClick={actionIconEvent}><svg className="action-icon"><use xlinkHref={`#${actionIcon}`} /></svg></div>}
+        {error && <div className="error-message">{error}</div>}
       </div>
-      {error && <div className="error-message">{error}</div>}
-    </div>
-  );
-};
-
-export default Input;
-
-//        {error && <svg className="error-icon"><use xlinkHref="#error" /></svg>}
+    );
+  }
+}
