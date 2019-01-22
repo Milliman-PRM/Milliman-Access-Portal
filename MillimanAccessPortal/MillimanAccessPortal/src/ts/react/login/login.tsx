d6eeafeeafb6449f4fc89b8486bcdbd2421840e5
Michael Reisz <Michael.Reisz@milliman.com>
2019-01-22 15:35:03 -0500

    Clean up the login component

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/login/login.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/login/login.tsx
@@ -4,28 +4,19 @@ import * as React from 'react';
 
 import { getData } from '../../shared';
 
-import LoginForm from './login-form';
-import Input from '../shared-components/input';
+import { LoginForm } from './login-form';
+import { Input } from '../shared-components/input';
 
 
 import '../../../images/map-logo.svg';
-import '../../../images/icons/user.svg';
-import '../../../images/icons/password.svg';
 import '../../../scss/map.scss';
 
 
 export class Login extends React.Component<{}, {} > {
-  public constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
   public render() {
     return (
       <>
-        <div id="login-splash-panel">
-        </div>
+        <div id="login-splash-panel" />
         <div id="login-wrapper">
           <section id="login-container">
             <div id="login-form-container">
@@ -42,38 +33,3 @@ export class Login extends React.Component<{}, {} > {
     )
   }
 }
-
-
-//<div className = "login-form-input-container" >
-//  <input name="Username" type="text" value={this.state.Username} onChange={this.handleChange} onBlur={this.handleBlur} placeholder="Username" autoFocus />
-//</div>
-//<div className="login-form-input-container">
-//  <input name="Password" type="password" value={this.state.Password} onChange={this.handleChange} placeholder="Password" />
-//</div>
-
-
-
-//<form id="login-form" autoComplete="off" action="/Account/Login?returnurl=%2F" method="post">
-//  <Input
-//    name="Username"
-//    label="Username"
-//    placeholderText="Username"
-//    inputIcon="user"
-//    value={this.state.Username}
-//    handleChange={this.handleChange}
-//    type="text"
-//  />
-//  <Input
-//    name="Password"
-//    label="Password"
-//    inputIcon="password"
-//    value={this.state.Password}
-//    handleChange={this.handleChange}
-//    type="password"
-//  />
-//  <input name="__RequestVerificationToken" type="hidden" value={this.state.RequestValidationToken} />
-//  <div className="login-form-button-container">
-//    <a href="/Account/ForgotPassword" className="link-button">Forgot Password</a>
-//    <button type="submit" className="blue-button">Login</button>
-//  </div>
-//</form>
