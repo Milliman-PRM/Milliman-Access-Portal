c8cc635b420037b5386da8e0f4312214f2b72d59
Michael Reisz <Michael.Reisz@milliman.com>
2019-01-22 15:35:48 -0500

    Refactor the login form component

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/login/login-form.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/login/login-form.tsx
@@ -1,12 +1,21 @@
 ﻿import * as React from 'react';
 const Joi = require('joi-browser');
-import Form from "../shared-components/form";
+import { Form } from "../shared-components/form";
 import { getData, postData } from "../../shared";
 
-import '../../../images/icons/login.svg';
-//import { LoginFormState } from './interfaces';
+import { BaseFormState } from '../shared-components/form';
+import { Input } from '../shared-components/input';
 
-class LoginForm extends Form {
+import '../../../images/icons/user.svg';
+import '../../../images/icons/password.svg';
+import '../../../images/icons/login.svg';
+
+interface LoginFormState extends BaseFormState {
+  userConfirmed: boolean;
+  loginWarning: string;
+}
+
+export class LoginForm extends Form<{}, LoginFormState> {
 
   state = {
     userConfirmed: false,
@@ -25,7 +34,7 @@ class LoginForm extends Form {
       .label("Password")
   };
 
-  checkUser = e => {
+  checkUser = (e) => {
     e.preventDefault();
 
     const errors = { ...this.state.errors };
@@ -41,7 +50,7 @@ class LoginForm extends Form {
     this.setState({ userConfirmed: true });
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = (e: React.MouseEvent<HTMLButtonElement> | React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const errors = this.validate();
@@ -51,7 +60,7 @@ class LoginForm extends Form {
     postData(window.location.href, this.state.data, true)
       .then(response => {
         console.log(response);
-        let loginWarning = response.headers.get('Warning');
+        const loginWarning = response.headers.get('Warning');
         if (loginWarning) {
           const data = { ...this.state.data };
           data["password"] = "";
@@ -66,20 +75,63 @@ class LoginForm extends Form {
   };
 
   render() {
+    const { userConfirmed, loginWarning } = this.state;
     return (
       <form onSubmit={!this.state.userConfirmed ? this.checkUser : this.handleSubmit}>
         {!this.state.userConfirmed ?
-          this.renderInput("username", "Username", "text", { inputIcon: 'user', actionIcon: 'login', actionIconEvent: this.checkUser, autoFocus: true }) :
-          this.renderInput("username", "Username", "text", { inputIcon: 'user' })
+          (
+            <Input
+              name="username"
+              label="Username"
+              type="text"
+              value={this.state.data["username"]}
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
+              error={this.state.errors["username"]}
+              autoFocus={true}
+              inputIcon="user"
+            >
+              <div className="action-icon-label" onClick={this.checkUser}>
+                <svg className="action-icon"><use xlinkHref="#login" /></svg>
+              </div>
+            </Input>
+          ) : (
+            <Input
+              name="username"
+              label="Username"
+              type="text"
+              value={this.state.data["username"]}
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
+              error={this.state.errors["username"]}
+              inputIcon="user" />
+          )
         }
-        {this.state.userConfirmed && this.renderInput("password", "Password", "password", { inputIcon: 'password', autoFocus: true })}
-        {this.state.userConfirmed && this.renderRequestVerificationToken()}
-        {this.state.loginWarning && <div className="login-warning">{this.state.loginWarning}</div>}
-        {this.state.userConfirmed && this.renderButton("Forgot Password", false, () => { window.location.href = "/Account/ForgotPassword" })}
-        {this.state.userConfirmed && this.renderButton("Login")}
+        {userConfirmed &&
+          <>
+            <Input
+              name="password"
+              label="Password"
+              type="password"
+              value={this.state.data["password"]}
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
+              error={this.state.errors["password"]}
+              autoFocus={true}
+              inputIcon="password"
+            />
+            {loginWarning && <div className="login-warning">{this.state.loginWarning}</div>}
+            <a href="/Account/ForgotPassword" className="link-button">Forgot Password</a>
+            <button
+              type="submit"
+              disabled={(this.validate()) ? true : false}
+              className="blue-button"
+              onClick={this.handleSubmit}>
+              Login
+            </button>
+          </>
+        }
       </form>
     );
   }
 }
-
-export default LoginForm;
