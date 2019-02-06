e9edc5aefef269fc0f9de2c9da711c5a883188a7
Michael Reisz <Michael.Reisz@milliman.com>
2019-02-06 10:07:24 -0500

    Switch to SVG button spinner
    Switch to using an SVG as the button spinner.

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/button-spinner.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/button-spinner.tsx
@@ -2,8 +2,10 @@ import '../../../scss/react/shared-components/button-spinner.scss';
 
 import * as React from 'react';
 
+import '../../../images/icons/loading-spinner.svg';
+
 export class ButtonSpinner extends React.Component {
   public render() {
-    return <div className="spinner-small-r" />;
+    return <svg className="loading-spinner"><use xlinkHref="#loading-spinner" /></svg>;
   }
 }
