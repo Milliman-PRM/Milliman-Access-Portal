07873d7305b90838ba949f1432a78b85e0675820
Joseph Sweeney <Joseph.Sweeney@milliman.com>
2019-03-05 14:57:21 -0500

    Add doc comments for all redux code

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/content-access-admin/redux/action-creators.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/content-access-admin/redux/action-creators.ts
@@ -5,6 +5,10 @@ import { ErrorAction, RequestAction, ResponseAction, TSError } from './actions';
 
 export type ActionWithoutType<T> = Pick<T, Exclude<keyof T, 'type'>>;
 
+/**
+ * Create an action creator for a generic action.
+ * @param type Action type
+ */
 function createActionCreator<T extends Action>(type: T['type']): (actionProps: ActionWithoutType<T>) => T {
   return (actionProps: ActionWithoutType<T>) => Object.assign(
     {} as T,  // TypeScript can't infer T from its parts because it is generic
@@ -12,6 +16,10 @@ function createActionCreator<T extends Action>(type: T['type']): (actionProps: A
     actionProps,
   );
 }
+/**
+ * Create an action creator for an action that contains a request property.
+ * @param type Action type
+ */
 function createRequestActionCreator<T extends RequestAction>(type: T['type']):
   (request: T['request']) => T {
   return (request: T['request']) => Object.assign(
@@ -20,6 +28,11 @@ function createRequestActionCreator<T extends RequestAction>(type: T['type']):
     { request },
   );
 }
+/**
+ * Create an action creator for an action that contains a response property.
+ * This function is exported for use by sagas.
+ * @param type Action type
+ */
 export function createResponseActionCreator<T extends ResponseAction>(type: T['type']):
   (response: T['response']) => T {
   return (response: T['response']) => Object.assign(
@@ -28,6 +41,11 @@ export function createResponseActionCreator<T extends ResponseAction>(type: T['t
     { response },
   );
 }
+/**
+ * Create an action creator for an action that contains an error property.
+ * This function is exported for use by sagas.
+ * @param type Action type
+ */
 export function createErrorActionCreator<T extends ErrorAction>(type: T['type']):
   (error: TSError) => T {
   return (error: TSError) => Object.assign(
@@ -80,9 +98,9 @@ export const openDeleteGroupModal =
 export const closeDeleteGroupModal =
   createActionCreator<AccessActions.CloseDeleteGroupModal>('CLOSE_DELETE_GROUP_MODAL');
 export const openInvalidateModal =
-  createActionCreator<AccessActions.OpenInvalidateModal>('OPEN_INVALIDATE_MODAL');
+  createActionCreator<AccessActions.OpenInactiveModal>('OPEN_INACTIVE_MODAL');
 export const closeInvalidateModal =
-  createActionCreator<AccessActions.CloseInvalidateModal>('CLOSE_INVALIDATE_MODAL');
+  createActionCreator<AccessActions.CloseInactiveModal>('CLOSE_INACTIVE_MODAL');
 
 export const setGroupEditingOn =
   createActionCreator<AccessActions.SetGroupEditingOn>('SET_GROUP_EDITING_ON');
