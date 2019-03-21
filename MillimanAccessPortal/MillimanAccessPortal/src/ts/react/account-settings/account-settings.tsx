616ca00fad6f912aa58d5a65dcfaf8b7025b1f2d
Michael Reisz <Michael.Reisz@milliman.com>
2019-03-20 22:45:41 -0500

    Remove some legacy form divs to begin restructuring

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/account-settings/account-settings.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/account-settings/account-settings.tsx
@@ -85,119 +85,103 @@ class AccountSettings extends React.Component<AccountSettingsProps & typeof Acco
       <>
         <div className="form-section" data-section="username">
           <h4 className="form-section-title">User Information</h4>
-          <div className="form-input-container" style={{ marginBottom: 0 }}>
-            <div className="form-input htmlForm-input-text flex-item-12-12">
-              <div>
-                <Input
-                  name="username"
-                  label="Username"
-                  type="text"
-                  value={username}
-                  onChange={() => { return; }}
-                  onBlur={() => { return; }}
-                  error={null}
-                  readOnly={true}
-                />
-              </div>
-            </div>
-          </div>
+          <Input
+            name="username"
+            label="Username"
+            type="text"
+            value={username}
+            onChange={() => { return; }}
+            onBlur={() => { return; }}
+            error={null}
+            readOnly={true}
+          />
         </div>
         <div className="form-section" data-section="account">
-          <div className="form-input-container">
-            <div className="form-input form-input-text flex-item-for-tablet-up-6-12">
-              <Input
-                name="firstName"
-                label="First"
-                type="text"
-                value={firstName}
-                onChange={({ currentTarget: target }: React.FormEvent<HTMLInputElement>) => {
-                  this.props.setPendingTextInputValue({
-                    inputName: 'firstName',
-                    value: target.value,
-                  });
-                  this.props.validateInputUser({
-                    value: {
-                      ...this.props.inputs,
-                      firstName: target.value,
-                    },
-                    inputName: 'firstName',
-                  });
-                }}
-                autoFocus={true}
-                onBlur={() => { return; }}
-                error={this.props.valid.firstName.valid ? null : this.props.valid.firstName.message}
-              />
-            </div>
-            <div className="form-input form-input-text flex-item-for-tablet-up-6-12">
-              <Input
-                name="lastName"
-                label="Last"
-                type="text"
-                value={lastName}
-                onChange={({ currentTarget: target }: React.FormEvent<HTMLInputElement>) => {
-                  this.props.setPendingTextInputValue({
-                    inputName: 'lastName',
-                    value: target.value,
-                  });
-                  this.props.validateInputUser({
-                    value: {
-                      ...this.props.inputs,
-                      lastName: target.value,
-                    },
-                    inputName: 'lastName',
-                  });
-                }}
-                onBlur={() => { return; }}
-                error={this.props.valid.lastName.valid ? null : this.props.valid.lastName.message}
-              />
-            </div>
-            <div className="form-input form-input-text flex-item-for-tablet-up-4-12">
-              <Input
-                name="phone"
-                label="Phone Number"
-                type="phone"
-                value={phone}
-                onChange={({ currentTarget: target }: React.FormEvent<HTMLInputElement>) => {
-                  this.props.setPendingTextInputValue({
-                    inputName: 'phone',
-                    value: target.value,
-                  });
-                  this.props.validateInputUser({
-                    value: {
-                      ...this.props.inputs,
-                      phone: target.value,
-                    },
-                    inputName: 'phone',
-                  });
-                }}
-                onBlur={() => { return; }}
-                error={this.props.valid.phone.valid ? null : this.props.valid.phone.message}
-              />
-            </div>
-            <div className="form-input form-input-text flex-item-for-tablet-up-8-12">
-              <Input
-                name="employer"
-                label="Employer"
-                type="text"
-                value={employer}
-                onChange={({ currentTarget: target }: React.FormEvent<HTMLInputElement>) => {
-                  this.props.setPendingTextInputValue({
-                    inputName: 'employer',
-                    value: target.value,
-                  });
-                  this.props.validateInputUser({
-                    value: {
-                      ...this.props.inputs,
-                      employer: target.value,
-                    },
-                    inputName: 'employer',
-                  });
-                }}
-                onBlur={() => { return; }}
-                error={this.props.valid.employer.valid ? null : this.props.valid.employer.message}
-              />
-            </div>
-          </div>
+          <Input
+            name="firstName"
+            label="First"
+            type="text"
+            value={firstName}
+            onChange={({ currentTarget: target }: React.FormEvent<HTMLInputElement>) => {
+              this.props.setPendingTextInputValue({
+                inputName: 'firstName',
+                value: target.value,
+              });
+              this.props.validateInputUser({
+                value: {
+                  ...this.props.inputs,
+                  firstName: target.value,
+                },
+                inputName: 'firstName',
+              });
+            }}
+            autoFocus={true}
+            onBlur={() => { return; }}
+            error={this.props.valid.firstName.valid ? null : this.props.valid.firstName.message}
+          />
+          <Input
+            name="lastName"
+            label="Last"
+            type="text"
+            value={lastName}
+            onChange={({ currentTarget: target }: React.FormEvent<HTMLInputElement>) => {
+              this.props.setPendingTextInputValue({
+                inputName: 'lastName',
+                value: target.value,
+              });
+              this.props.validateInputUser({
+                value: {
+                  ...this.props.inputs,
+                  lastName: target.value,
+                },
+                inputName: 'lastName',
+              });
+            }}
+            onBlur={() => { return; }}
+            error={this.props.valid.lastName.valid ? null : this.props.valid.lastName.message}
+          />
+          <Input
+            name="phone"
+            label="Phone Number"
+            type="phone"
+            value={phone}
+            onChange={({ currentTarget: target }: React.FormEvent<HTMLInputElement>) => {
+              this.props.setPendingTextInputValue({
+                inputName: 'phone',
+                value: target.value,
+              });
+              this.props.validateInputUser({
+                value: {
+                  ...this.props.inputs,
+                  phone: target.value,
+                },
+                inputName: 'phone',
+              });
+            }}
+            onBlur={() => { return; }}
+            error={this.props.valid.phone.valid ? null : this.props.valid.phone.message}
+          />
+          <Input
+            name="employer"
+            label="Employer"
+            type="text"
+            value={employer}
+            onChange={({ currentTarget: target }: React.FormEvent<HTMLInputElement>) => {
+              this.props.setPendingTextInputValue({
+                inputName: 'employer',
+                value: target.value,
+              });
+              this.props.validateInputUser({
+                value: {
+                  ...this.props.inputs,
+                  employer: target.value,
+                },
+                inputName: 'employer',
+              });
+            }}
+            onBlur={() => { return; }}
+            error={this.props.valid.employer.valid ? null : this.props.valid.employer.message}
+          />
         </div>
       </>
     );
@@ -210,89 +194,81 @@ class AccountSettings extends React.Component<AccountSettingsProps & typeof Acco
     ? (
       <div className="form-section">
         <h4 className="form-section-title">Update Password</h4>
-        <div className="form-input-container">
-          <div className="form-input htmlForm-input-text flex-item-12-12">
-            <Input
-              name="currentPassword"
-              label="Current Password"
-              type="password"
-              value={currentPassword}
-              onChange={({ currentTarget: target }: React.FormEvent<HTMLInputElement>) => {
-                this.props.setPendingTextInputValue({
-                  inputName: 'current',
-                  value: target.value,
-                });
-                this.props.validateInputPassword({
-                  value: {
-                    current: target.value,
-                    new: this.props.inputs.newPassword,
-                    confirm: this.props.inputs.confirmPassword,
-                  },
-                  inputName: 'current',
-                });
-              }}
-              onBlur={() => { return; }}
-              error={this.props.valid.currentPassword.valid || !this.props.anyPasswordInputModified
-                ? null
-                : this.props.valid.currentPassword.message
-              }
-            />
-          </div>
-          <div className="form-input htmlForm-input-text flex-item-12-12">
-            <Input
-              name="newPassword"
-              label="New Password"
-              type="password"
-              value={newPassword}
-              onChange={({ currentTarget: target }: React.FormEvent<HTMLInputElement>) => {
-                this.props.setPendingTextInputValue({
-                  inputName: 'new',
-                  value: target.value,
-                });
-                this.props.validateInputPassword({
-                  value: {
-                    current: this.props.inputs.currentPassword,
-                    new: target.value,
-                    confirm: this.props.inputs.confirmPassword,
-                  },
-                  inputName: 'new',
-                });
-              }}
-              onBlur={() => { return; }}
-              error={this.props.valid.newPassword.valid || !this.props.anyPasswordInputModified
-                ? null
-                : this.props.valid.newPassword.message
-              }
-            />
-          </div>
-          <div className="form-input htmlForm-input-text flex-item-12-12">
-            <Input
-              name="confirmPassword"
-              label="Confirm Password"
-              type="password"
-              value={confirmPassword}
-              onChange={({ currentTarget: target }: React.FormEvent<HTMLInputElement>) => {
-                this.props.setPendingTextInputValue({
-                  inputName: 'confirm',
-                  value: target.value,
-                });
-                this.props.validateInputPassword({
-                  value: {
-                    current: this.props.inputs.currentPassword,
-                    new: this.props.inputs.newPassword,
-                    confirm: target.value,
-                  },
-                  inputName: 'confirm',
-                });
-              }}
-              onBlur={() => { return; }}
-              error={this.props.valid.newPassword.valid || !this.props.anyPasswordInputModified
-                ? null
-                : this.props.valid.confirmPassword.message
-              }
-            />
-          </div>
-        </div>
+        <Input
+          name="currentPassword"
+          label="Current Password"
+          type="password"
+          value={currentPassword}
+          onChange={({ currentTarget: target }: React.FormEvent<HTMLInputElement>) => {
+            this.props.setPendingTextInputValue({
+              inputName: 'current',
+              value: target.value,
+            });
+            this.props.validateInputPassword({
+              value: {
+                current: target.value,
+                new: this.props.inputs.newPassword,
+                confirm: this.props.inputs.confirmPassword,
+              },
+              inputName: 'current',
+            });
+          }}
+          onBlur={() => { return; }}
+          error={this.props.valid.currentPassword.valid || !this.props.anyPasswordInputModified
+            ? null
+            : this.props.valid.currentPassword.message
+          }
+        />
+        <Input
+          name="newPassword"
+          label="New Password"
+          type="password"
+          value={newPassword}
+          onChange={({ currentTarget: target }: React.FormEvent<HTMLInputElement>) => {
+            this.props.setPendingTextInputValue({
+              inputName: 'new',
+              value: target.value,
+            });
+            this.props.validateInputPassword({
+              value: {
+                current: this.props.inputs.currentPassword,
+                new: target.value,
+                confirm: this.props.inputs.confirmPassword,
+              },
+              inputName: 'new',
+            });
+          }}
+          onBlur={() => { return; }}
+          error={this.props.valid.newPassword.valid || !this.props.anyPasswordInputModified
+            ? null
+            : this.props.valid.newPassword.message
+          }
+        />
+        <Input
+          name="confirmPassword"
+          label="Confirm Password"
+          type="password"
+          value={confirmPassword}
+          onChange={({ currentTarget: target }: React.FormEvent<HTMLInputElement>) => {
+            this.props.setPendingTextInputValue({
+              inputName: 'confirm',
+              value: target.value,
+            });
+            this.props.validateInputPassword({
+              value: {
+                current: this.props.inputs.currentPassword,
+                new: this.props.inputs.newPassword,
+                confirm: target.value,
+              },
+              inputName: 'confirm',
+            });
+          }}
+          onBlur={() => { return; }}
+          error={this.props.valid.newPassword.valid || !this.props.anyPasswordInputModified
+            ? null
+            : this.props.valid.confirmPassword.message
+          }
+        />
       </div>
     )
     : null;
