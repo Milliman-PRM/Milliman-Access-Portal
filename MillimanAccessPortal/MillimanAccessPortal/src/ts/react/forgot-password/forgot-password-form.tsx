edb14d87cb1530fabea4080b44e00225ffb04e2a
Kelsie Stevenson <Kelsie.Stevenson@milliman.com>
2019-04-23 13:36:05 -0400

    update the forgot password component

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/forgot-password/forgot-password-form.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/forgot-password/forgot-password-form.tsx
@@ -1,7 +1,55 @@
 import * as React from 'react';
+import * as Yup from 'yup';
 
-export class ForgotPasswordForm extends React.Component {
-  public render() {
-    return <></>;
+import { BaseFormState, Form } from '../shared-components/form';
+import { Input } from '../shared-components/input';
+
+import '../../../images/icons/user.svg';
+
+export class ForgotPasswordForm extends Form<{}, BaseFormState> {
+  protected schema = Yup.object({
+    username: Yup.string()
+      .email()
+      .required()
+      .label('Email'),
+  });
+
+  public constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      data: { username: '' },
+      errors: {},
+      formIsValid: false,
+    };
   }
-}
+
+  public render() {
+    const { data, errors, formIsValid } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <Input
+          name="username"
+          label="Username"
+          type="text"
+          value={data.username}
+          onChange={this.handleChange}
+          onBlur={this.handleBlur}
+          error={errors.username}
+          autoFocus={true}
+          inputIcon="user"
+        />
+        <div className='button-container'>
+          <button
+            type='submit'
+            disabled={formIsValid}
+            className="blue-button"
+          >
+            Reset Password
+          </button>
+        </div>
+      </form>
+    );
+  };
+};
