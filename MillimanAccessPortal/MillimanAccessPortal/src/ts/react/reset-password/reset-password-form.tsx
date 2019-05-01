db1001e17d494872dfdf514a5670fcd0e02673c2
Kelsie Stevenson <Kelsie.Stevenson@milliman.com>
2019-05-01 15:59:13 -0400

    make sure to actually render
    combined this into one render since it wasn't too big anyway

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/reset-password/reset-password-form.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/reset-password/reset-password-form.tsx
@@ -29,39 +29,34 @@ export class ResetPasswordForm extends Form<{}, BaseFormState> {
     };
   }
 
-  private renderResetPasswordForm() {
+  public render() {
+    const { data, errors, formIsValid } = this.state;
+
     return (
       <div className="form-content-container flex-item-for-tablet-up-10-12 flex-item-for-desktop-up-5-12">
         <form autoComplete="off">
-          {this.renderPasswordSection()}
+          <div className="form-section">
+            <h3 className="form-section-title">Reset your Password</h3>
+            <Input
+              name="newPassword"
+              label="New Password"
+              type="password"
+              value={data.newPassword}
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
+              error={errors.newPassword}
+            />
+            <Input
+              name="confirmPassword"
+              label="Confirm Password"
+              type="password"
+              value={data.confirmPassword}
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
+              error={errors.confirmPassword}
+            />
+          </div>
         </form>
-      </div>
-    );
-  }
-
-  private renderPasswordSection() {
-    const { data, errors, formIsValid } = this.state;
-    return (
-      <div className="form-section">
-        <h3 className="form-section-title">Reset your Password</h3>
-        <Input
-          name="newPassword"
-          label="New Password"
-          type="password"
-          value={data.newPassword}
-          onChange={this.handleChange}
-          onBlur={this.handleBlur}
-          error={errors.newPassword}
-        />
-        <Input
-          name="confirmPassword"
-          label="Confirm Password"
-          type="password"
-          value={data.confirmPassword}
-          onChange={this.handleChange}
-          onBlur={this.handleBlur}
-          error={errors.confirmPassword}
-        />
       </div>
     );
   }
