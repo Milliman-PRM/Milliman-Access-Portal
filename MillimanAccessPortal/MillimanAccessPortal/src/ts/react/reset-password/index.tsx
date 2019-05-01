ab0ce35d810f26b4563942fe5d8bbc79a686e78e
Kelsie Stevenson <Kelsie.Stevenson@milliman.com>
2019-05-01 10:00:10 -0400

    set up scaffolding for reset password form
    make sure it is all hooked up

---
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/reset-password/index.tsx
@@ -0,0 +1,25 @@
+declare function require(moduleName: string): any;
+
+import '../../../images/map-logo.svg';
+import '../../../scss/map.scss';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { ResetPasswordForm as Root } from './reset-password-form';
+
+let ResetPasswordForm: typeof Root = require('./reset-password-form')
+  .ResetPasswordForm;
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <ResetPasswordForm />,
+    document.getElementById('content-container')
+  );
+});
+
+if (module.hot) {
+  module.hot.accept(['./reset-password-form'], () => {
+    ResetPasswordForm = require('./reset-password-form').ResetPasswordForm;
+  });
+}
---
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/reset-password/reset-password-form.tsx
@@ -0,0 +1,8 @@
+
+import * as React from 'react';
+
+export class ResetPasswordForm extends React.Component {
+  public render() {
+    return <></>;
+  }
+}
