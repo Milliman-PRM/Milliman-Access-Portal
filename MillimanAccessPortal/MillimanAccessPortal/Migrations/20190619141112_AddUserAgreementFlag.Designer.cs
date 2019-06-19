2b6e5479d07164c1db8952cdba23ead3f3ea8ed7
tom.puckett <tom.puckett@milliman.com>
2019-06-19 10:15:13 -0400

    Add migration for user agreement acceptance flag

--- MillimanAccessPortal/MillimanAccessPortal/Migrations/20190619141112_AddUserAgreementFlag.Designer.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Migrations/20190619141112_AddUserAgreementFlag.Designer.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using MapDbContextLib.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace MillimanAccessPortal.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190619141112_AddUserAgreementFlag")]
+    partial class AddUserAgreementFlag
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
