5983393cf40ec7d8955ba1e28642b0db328c106f
Jenna Ward <Jenna.Ward@milliman.com>
2019-06-24 14:47:00 -0400

    add functionality to accept button

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/user-agreement.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/user-agreement.ts
@@ -1,4 +1,6 @@
-import '../scss/content-disclaimer.scss';
+import { postData } from './shared';
+
+import '../../src/scss/content-disclaimer.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
     handleScroll();
@@ -6,8 +8,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function handleScroll() {
     const element = document.getElementById('disclaimer-container');
+    const acceptButton = document.getElementById('accept-button');
 
     if (element.scrollHeight - element.scrollTop === element.clientHeight) {
-      (document.getElementById('accept-button') as HTMLButtonElement ).disabled = false;
+        (acceptButton as HTMLButtonElement).disabled = false;
+
+        acceptButton.onclick = async () => {
+            try {
+                await postData('/Account/AcceptUserAgreement', {
+                    AgreementText: (document.getElementById('AgreementText') as HTMLInputElement).value,
+                    IsRenewal: (document.getElementById('IsRenewal') as HTMLInputElement).value,
+                    ReturnUrl: (document.getElementById('ReturnUrl') as HTMLInputElement).value,
+                }, true)
+                    .then((response) => {
+                        const redirectUrl = response.headers.get('NavigateTo');
+                        window.location.replace(redirectUrl);
+                    });
+            } catch (e) {
+                alert('An error has occurred. Please try again.');
+            }
+        };
     }
 }
