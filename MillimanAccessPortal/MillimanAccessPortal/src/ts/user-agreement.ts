4684411997207e4f2f81e68e031988a99920b003
Michael Reisz <Michael.Reisz@milliman.com>
2019-06-27 22:17:44 -0400

    Make the button light up when scrolling to the bottom
    Pull this functionality out to enable it on the content disclaimer as well.

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/user-agreement.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/user-agreement.ts
@@ -1,32 +1,25 @@
-import { postData } from './shared';
+import { enableButtonOnScrollBottom, postData } from './shared';
 
 import '../../src/scss/content-disclaimer.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
-    handleScroll();
+  const scrollElement = document.getElementById('content-disclaimer-text');
+  const acceptButton = document.getElementById('accept-button') as HTMLButtonElement;
+
+  acceptButton.onclick = async () => {
+    await postData('/Account/AcceptUserAgreement', {
+      AgreementText: (document.getElementById('AgreementText') as HTMLInputElement).value,
+      IsRenewal: (document.getElementById('IsRenewal') as HTMLInputElement).value,
+      ReturnUrl: (document.getElementById('ReturnUrl') as HTMLInputElement).value,
+    }, true)
+      .then((response) => {
+        const redirectUrl = response.headers.get('NavigateTo');
+        window.location.replace(redirectUrl);
+      })
+      .catch(() => {
+        alert('An error has occurred. Please try again.');
+      });
+  };
+
+  enableButtonOnScrollBottom(scrollElement, acceptButton);
 });
-
-function handleScroll() {
-    const element = document.getElementById('disclaimer-container');
-    const acceptButton = document.getElementById('accept-button');
-
-    if (element.scrollHeight - element.scrollTop === element.clientHeight) {
-        (acceptButton as HTMLButtonElement).disabled = false;
-
-        acceptButton.onclick = async () => {
-            try {
-                await postData('/Account/AcceptUserAgreement', {
-                    AgreementText: (document.getElementById('AgreementText') as HTMLInputElement).value,
-                    IsRenewal: (document.getElementById('IsRenewal') as HTMLInputElement).value,
-                    ReturnUrl: (document.getElementById('ReturnUrl') as HTMLInputElement).value,
-                }, true)
-                    .then((response) => {
-                        const redirectUrl = response.headers.get('NavigateTo');
-                        window.location.replace(redirectUrl);
-                    });
-            } catch (e) {
-                alert('An error has occurred. Please try again.');
-            }
-        };
-    }
-}
