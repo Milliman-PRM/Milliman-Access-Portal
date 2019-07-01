eba2d8318276747c541f649ec4cc4db16d21ce3e
Michael Reisz <michael_reisz@yahoo.com>
2019-07-01 08:53:18 -0400

    Get the edit/preview buttons working

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/update-user-agreement.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/update-user-agreement.ts
@@ -1,3 +1,34 @@
+import { convertMarkdownToHTML } from '../ts/convert-markdown';
+
 import '../images/map-logo.svg';
 
 import '../../src/scss/update-user-agreement.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('#agreement-container .markdown-select-edit').click(setAgreementToEditMode);
+  $('#agreement-container .markdown-select-preview').click(setAgreementToPreviewMode);
+});
+
+function setAgreementToEditMode() {
+  // Toggle buttons
+  $('#agreement-container .markdown-select-edit').addClass('selected');
+  $('#agreement-container .markdown-select-preview').removeClass('selected');
+  // Toggle preview -> textarea
+  $('#newAgreementText').show();
+  $('#AgreementPreview').hide();
+  // Clear the preview
+  $('AgreementPreview').empty();
+}
+
+function setAgreementToPreviewMode() {
+  // Update markdown from textarea content
+  const rawAgreementMarkdown = (document.getElementById('newAgreementText') as HTMLTextAreaElement).value.trimRight();
+  const processedAgreementHTML = convertMarkdownToHTML(rawAgreementMarkdown);
+  document.getElementById('AgreementPreview').innerHTML = processedAgreementHTML;
+  // Toggle buttons
+  $('#agreement-container .markdown-select-preview').addClass('selected');
+  $('#agreement-container .markdown-select-edit').removeClass('selected');
+  // Toggle textarea -> preview
+  $('#AgreementPreview').show();
+  $('#newAgreementText').hide();
+}
