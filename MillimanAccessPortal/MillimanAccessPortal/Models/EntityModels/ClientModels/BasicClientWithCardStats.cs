eb01c59432cf6c5ca2c1408b253f2bf01dbc5f60
tom.puckett <tom.puckett@milliman.com>
2019-07-22 15:41:54 -0400

    WIP on model changes

--- MillimanAccessPortal/MillimanAccessPortal/Models/EntityModels/ClientModels/BasicClientWithCardStats.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Models/EntityModels/ClientModels/BasicClientWithCardStats.cs
@@ -12,5 +12,10 @@
         /// This count is greater than or equal to the number of content eligible users in the client.
         /// </summary>
         public int UserCount { get; set; }
+
+        /// <summary>
+        /// In the context of a specified Client role, indicates whether a user is authorized to admin this client
+        /// </summary>
+        public bool CanManage { get; set; }
     }
 }
