5834a17708a71a50fb88f314b39e6c92317b33b5
Michael Reisz <michael_reisz@yahoo.com>
2019-07-24 15:42:46 -0400

    Add a Select component to implement dropdowns in forms
    This creates a form select element (a dropdown menu) that matches the new input and text area components stylistically.

---
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/select.tsx
@@ -0,0 +1,47 @@
+﻿import '../../../images/icons/hide-password.svg';
+import '../../../images/icons/show-password.svg';
+
+import '../../../scss/react/shared-components/form-elements.scss';
+
+import * as React from 'react';
+
+interface SelectProps {
+  name: string;
+  label: string;
+  value: string | number;
+  values: Array<{ selectionValue: string | number, selectionLabel: string }>;
+  onChange: (currentTarget: React.FormEvent<HTMLSelectElement>) => void;
+  error: string;
+  placeholderText?: string;
+  autoFocus?: boolean;
+  readOnly?: boolean;
+  hidden?: boolean;
+}
+
+export const Select = React.forwardRef<HTMLSelectElement, SelectProps>((props, ref) => {
+  const { name, label, error, placeholderText, readOnly, hidden, values, children, ...rest } = props;
+  const options = values.map((option, index) => {
+    return (<option key={index} value={option.selectionValue}>{option.selectionLabel}</option>);
+  });
+  return (
+    <div className={'form-element-container' + (readOnly ? ' disabled' : '') + (hidden ? ' hidden' : '')}>
+      <div className={'form-element-select' + (error ? ' error' : '')}>
+        <div className="form-select-container">
+          <select
+            name={name}
+            id={name}
+            ref={ref}
+            className={`form-input ${!props.value ? '' : 'item-selected'}`}
+            disabled={readOnly}
+            {...rest}
+          >
+            <option>{placeholderText || 'Select a ' + label}</option>
+            {options}
+          </select>
+          <label className="form-input-label" htmlFor={name}>{label}</label>
+        </div>
+      </div>
+      {error && <div className="error-message">{error}</div>}
+    </div>
+  );
+});
