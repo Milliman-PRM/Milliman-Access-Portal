b1af56f332a94b28d2fb28334e64246f30182adb
Michael Reisz <michael_reisz@yahoo.com>
2019-07-30 12:28:58 -0400

    Remove contentItemId from status refresh call

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/content-publishing/redux/actions.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/content-publishing/redux/actions.ts
@@ -131,7 +131,6 @@ export interface FetchStatusRefresh {
   type: 'FETCH_STATUS_REFRESH';
   request: {
     clientId: Guid;
-    contentItemId: Guid;
   };
 }
 export interface FetchStatusRefreshSucceeded {
