dad17190758f63c3470f1eee87a3434a1dc7e7d3
tom.puckett <tom.puckett@milliman.com>
2019-08-01 13:54:51 -0400

    Rename Name to DisplayName

--- MillimanAccessPortal/MillimanAccessPortal/Models/ContentAccessAdmin/ContentAccessAdminPageGlobalModel.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Models/ContentAccessAdmin/ContentAccessAdminPageGlobalModel.cs
@@ -7,8 +7,6 @@
 using MillimanAccessPortal.Models.EntityModels.ContentItemModels;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MillimanAccessPortal.Models.ContentAccessAdmin
 {
--- MillimanAccessPortal/MillimanAccessPortal/Models/EntityModels/ContentItemModels/BasicContentType.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Models/EntityModels/ContentItemModels/BasicContentType.cs
@@ -1,17 +1,22 @@
-﻿using MapDbContextLib.Context;
+﻿/*
+ * CODE OWNERS: Tom Puckett
+ * OBJECTIVE: <What and WHY.>
+ * DEVELOPER NOTES: <What future developers need to know.>
+ */
+
+using MapDbContextLib.Context;
 using System;
 using System.Collections.Generic;
 
 namespace MillimanAccessPortal.Models.EntityModels.ContentItemModels
 {
     /// <summary>
-    /// A simplified representation of a ContentType.
-    /// This model is intended to be extended to satisfy front end needs.
+    /// A simplified representation of a ContentType to satisfy front end needs.
     /// </summary>
     public class BasicContentType
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string DisplayName { get; set; }
         public bool CanReduce { get; set; }
         public List<string> FileExtensions { get; set; }
         public ContentTypeEnum TypeEnum { get; set; } = ContentTypeEnum.Unknown;
@@ -22,7 +27,7 @@ namespace MillimanAccessPortal.Models.EntityModels.ContentItemModels
             TypeEnum = t.TypeEnum;
             CanReduce = t.CanReduce;
             FileExtensions = t.FileExtensions;
-            Name = t.TypeEnum.GetDisplayValueString();
+            DisplayName = t.TypeEnum.GetDisplayValueString();
         }
 
     }
