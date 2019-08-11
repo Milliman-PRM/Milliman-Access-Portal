2a1598daf3cfa911c779823bdeb50f97c8a83d17
Michael Reisz <michael_reisz@yahoo.com>
2019-08-10 23:39:38 -0400

    Fix a tslint error

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/upload/generate-unique-identifier.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/upload/generate-unique-identifier.ts
@@ -1,4 +1,4 @@
-import { randomBytes } from "crypto";
+import { randomBytes } from 'crypto';
 
 export function generateUniqueId(purpose: string): string {
   return `${purpose}-${randomBytes(8).toString('hex')}`;
