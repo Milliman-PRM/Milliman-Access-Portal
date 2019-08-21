0df28b30dc70bdd1b1e95d94b88825be6ba91311
Michael Reisz <michael_reisz@yahoo.com>
2019-08-20 23:21:13 -0400

    Move some helper panel components to one file

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/content-panel/content-panel-sections.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export const ContentPanelSectionContent: React.SFC = (props) => (
-  <div className="admin-panel-content-container">{props.children}</div>
-);
-
-export const ContentPanelSectionAction: React.SFC = (props) => (
-  <div className="admin-panel-action-container">{props.children}</div>
-);
--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/content-panel/content-panel.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/content-panel/content-panel.tsx
@@ -1,10 +1,9 @@
 import '../../../../scss/react/shared-components/card-panel.scss';
+import '../../../../scss/react/shared-components/content-panel.scss';
 
 import * as React from 'react';
 
 import { ColumnSpinner } from '../column-spinner';
-import { ContentPanelSectionContent } from './content-panel-sections';
-import { PanelSectionContainer } from './panel-sections';
 
 export interface ContentPanelProps {
   loading?: boolean;
@@ -21,3 +20,23 @@ export class ContentPanel extends React.Component<ContentPanelProps, {}> {
     );
   }
 }
+
+export const PanelSectionContainer: React.SFC = (props) => (
+  <div className="admin-panel-container admin-panel-container flex-item-12-12 flex-item-for-tablet-up-6-12">
+    <div className="admin-panel-list">
+      {props.children}
+    </div>
+  </div>
+);
+
+export const PanelSectionToolbar: React.SFC = (props) => (
+  <div className="admin-panel-toolbar">{props.children}</div>
+);
+
+export const PanelSectionToolbarButtons: React.SFC = (props) => (
+  <div className="admin-panel-action-icons-container">{props.children}</div>
+);
+
+export const ContentPanelSectionContent: React.SFC = (props) => (
+  <div className="admin-panel-content-container">{props.children}</div>
+);
