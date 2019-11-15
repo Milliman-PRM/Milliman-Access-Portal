994a0ede5d1176c0169e93745913b5486b9e780e
Michael Reisz <michael_reisz@yahoo.com>
2019-11-14 22:27:22 -0500

    Flesh out the selection group detail component

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/content-publishing/selection-group-summary.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/content-publishing/selection-group-summary.tsx
@@ -1,16 +1,37 @@
 import * as React from 'react';
 
 import { SelectionGroupSummary } from '../../view-models/content-publishing';
+import { HierarchyDiffs } from './hierarchy-diffs';
 
 interface SelectionGroupDetailsProps {
   selectionGroup: SelectionGroupSummary;
+  changedOnly: boolean;
+  key: number | string;
 }
 
 export class SelectionGroupDetails extends React.Component<SelectionGroupDetailsProps, {}> {
   public render() {
-    const { selectionGroup } = this.props;
+    const { changedOnly, key, selectionGroup } = this.props;
+    const prevStatus = selectionGroup.wasInactive
+      ? <>[<span className="inactive-status">INACTIVE</span>]</>
+      : <>[<span className="active-status">ACTIVE</span>]</>;
+    const newStatus = selectionGroup.isInactive
+      ? <>[<span className="inactive-status">INACTIVE</span>]</>
+      : <>[<span className="active-status">ACTIVE</span>]</>;
+    const statusDetail = (selectionGroup.wasInactive !== selectionGroup.isInactive)
+      ? <span className="status">{prevStatus} > {newStatus}</span>
+      : <span className="status">{newStatus}</span>;
     return (
-      <div className="selection-group-detail">
+      <div key={key} className="selection-group-detail">
+        <h3>{selectionGroup.name} {statusDetail}</h3>
+        {
+          selectionGroup.selectionChanges && (
+            <HierarchyDiffs
+              hierarchy={selectionGroup.selectionChanges}
+              changedOnly={changedOnly}
+            />
+          )
+        }
       </div>
     );
   }
