f1d4eae45026fab0e724a7446182e5bebf742e79
Michael Reisz <michael_reisz@yahoo.com>
2019-11-18 13:17:31 -0500

    Add a toast notification for a successful rejection of a go live summary

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/content-publishing/redux/sagas.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/content-publishing/redux/sagas.ts
@@ -97,6 +97,7 @@ export default function* rootSaga() {
   yield takeEveryToast('PUBLISH_CONTENT_FILES_SUCCEEDED', 'Files successfully published.');
   yield takeEveryToast('DELETE_CONTENT_ITEM_SUCCEEDED', 'Content Item successfully deleted.');
   yield takeEveryToast('CANCEL_PUBLICATION_REQUEST_SUCCEEDED', 'Publication canceled.');
+  yield takeEveryToast('REJECT_GO_LIVE_SUMMARY_SUCCEEDED', 'Publication rejected.');
   yield takeEveryToast('PROMPT_STATUS_REFRESH_STOPPED',
     'Please refresh the page to update reduction status.', 'warning');
   yield takeEveryToast<ErrorPublishingAction>([
