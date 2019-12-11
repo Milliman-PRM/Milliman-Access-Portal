4653a1baf9622b0fb1abec6f57709ea40e156d02
Michael Reisz <michael_reisz@yahoo.com>
2019-12-11 13:31:03 -0500

    Create a tab row component

---
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/tab-row.tsx
@@ -0,0 +1,43 @@
+﻿import '../../../scss/react/shared-components/tab-row.scss';
+
+import * as React from 'react';
+
+export interface Tab {
+  id: string;
+  label: string;
+}
+
+export interface TabRowProps {
+  tabs: Tab[];
+  onTabSelect: (id: string) => void;
+  selectedTab: string;
+  fullWidth: boolean;
+}
+
+export class TabRow extends React.Component<TabRowProps> {
+  public render() {
+    const { fullWidth } = this.props;
+    return (
+      <div className={`tab-row${fullWidth ? ' full-width' : ' min-width'}`}>
+        {this.renderTabs()}
+      </div>
+    );
+  }
+
+  private renderTabs() {
+    const { tabs, onTabSelect, selectedTab } = this.props;
+    return tabs.map((tab) => (
+      <div
+        key={tab.id}
+        className={`tab${tab.id === selectedTab ? ' selected' : ''}`}
+        onClick={() => {
+          if (tab.id !== selectedTab) {
+            onTabSelect(tab.id);
+          }
+        }}
+      >
+        {tab.label}
+      </div>
+    ));
+  }
+}
