96b83accd5ed4405b316bca44644cb4bffe3f6be
tom.puckett <tom.puckett@milliman.com>
2020-01-22 17:00:42 -0500

    Interim sftp solution

---
+++ SftpServer/SftpFrameworkGui/Form1.cs
@@ -0,0 +1,41 @@
+﻿using SftpServerLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SftpFrameworkGui
+{
+    public partial class Form1 : Form
+    {
+        SftpLibApi _SftpApi = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            Button Sender = sender as Button;
+            if (Sender.Text == "Start")
+            {
+                _SftpApi = SftpLibApi.NewInstance();
+
+                _SftpApi.Start("C:\\Users\\tom.puckett\\Desktop\\sftpPrivateKey.OpenSSH.pem");
+                Sender.Text = "Stop";
+            }
+            else
+            {
+                Sender.Text = "Start";
+                _SftpApi.Stop();
+                _SftpApi = null;
+            }
+        }
+    }
+}
---
+++ SftpServer/SftpServerLib/IpWorksSftpServer.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * CODE OWNERS: Tom Puckett
+ * OBJECTIVE: Concrete implementation of the MAP SFTP server
+ * DEVELOPER NOTES: This class is partial.  Implementation is contained in multiple source code files
+ */
+
+using nsoftware.IPWorksSSH;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SftpServerLib
+{
+    internal partial class IpWorksSftpServer : SftpLibApi
+    {
+        protected Sftpserver _sftpServer = default;
+
+        public override void Start(string CertificateFilePath)
+        {
+            Certificate cert = new Certificate(CertificateFilePath);
+            EstablishServerInstance(cert);
+
+            _sftpServer.Listening = true;
+            Debug.WriteLine("Server listening");
+        }
+
+        public async override void Stop()
+        {
+            try
+            {
+                await _sftpServer.ShutdownAsync();
+                _sftpServer = null;
+                Debug.WriteLine("Server instance destroyed");
+            }
+            catch (Exception e)
+            {
+                var ex = e;
+                Debug.WriteLine("Exception while stopping server");
+            }
+        }
+
+    }
+}
