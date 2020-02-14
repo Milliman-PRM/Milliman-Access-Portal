e80244d90daa035ccc618c365197516010ed9911
tom.puckett <tom.puckett@milliman.com>
2020-02-14 12:05:44 -0500

    Add IsSuspended flag to class FileDropCardModel

--- MillimanAccessPortal/MillimanAccessPortal/Models/FileDrop/FileDropCardModel.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Models/FileDrop/FileDropCardModel.cs
@@ -26,6 +26,7 @@ namespace MillimanAccessPortal.Models.FileDropModels
             Name = fileDrop.Name;
             Description = fileDrop.Description;
             ClientId = fileDrop.ClientId;
+            IsSuspended = fileDrop.IsSuspended;
         }
 
         public Guid Id { get; set; }
@@ -35,6 +36,8 @@ namespace MillimanAccessPortal.Models.FileDropModels
         public string Description { get; set; }
 
         public int? UserCount { get; set; }
+
+        public bool IsSuspended { get; set; }
 
         public Guid ClientId { get; set; }
     }
