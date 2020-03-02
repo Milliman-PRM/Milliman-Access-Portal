8b8934db5b252f30f8c2a922f37085c126501cb0
Michael Reisz <michael_reisz@yahoo.com>
2020-03-02 16:00:51 -0500

    Actually draw the table based on the data
    This is currently coming from the hard-coded data in a previous commit, but will work with data from the server in the future.

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/file-drop/permissions-table.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/file-drop/permissions-table.tsx
@@ -1,20 +1,137 @@
+import '../../../images/icons/add-group.svg';
+import '../../../images/icons/add-user.svg';
 import '../../../images/icons/group.svg';
+import '../../../images/icons/remove-circle.svg';
 import '../../../images/icons/user.svg';
 
 import '../../../scss/react/file-drop/permissions-table.scss';
 
 import * as React from 'react';
 
+import { PermissionGroupsReturnModel } from '../models';
 import { ActionIcon, ActionIconButtonContainer } from '../shared-components/action-icon';
 import { Checkbox } from '../shared-components/form/checkbox';
 
-// export interface PermissionsTableProps = {
-//   rawData: PermissionGroupsReturnModel;
-// }
+interface PermissionsTableProps {
+  permissions: PermissionGroupsReturnModel;
+}
 
-export class PermissionsTable extends React.Component {
+export class PermissionsTable extends React.Component<PermissionsTableProps> {
 
   public render() {
+    const { permissionGroups, eligibleUsers } = this.props.permissions;
+    const permissionGroupsMarkup = Object.keys(permissionGroups).map((pgId) => {
+      const thisPG = permissionGroups[pgId];
+      const pgIcon = (thisPG.isPersonalGroup) ? '#user' : '#group';
+      return (
+        <>
+          <tr
+            key={thisPG.id}
+            className={thisPG.isPersonalGroup || thisPG.assignedMapUserIds.length === 0 ? 'last-group-row' : null}
+          >
+            <td><svg className="table-icon"><use xlinkHref={pgIcon} /></svg></td>
+            {
+              (thisPG.isPersonalGroup) ?
+                (
+                  <>
+                    <td><strong>{thisPG.name}</strong></td>
+                    <td>{eligibleUsers[thisPG.assignedMapUserIds[0]].username}</td>
+                  </>
+                ) : (
+                  <td colSpan={2}><strong>{thisPG.name}</strong></td>
+                )
+            }
+            <td className="content-center">
+              <Checkbox
+                hoverText="Allow users to download content from this File Drop"
+                onChange={() => false}
+                key={1}
+                readOnly={false}
+                selected={thisPG.readAccess}
+              />
+            </td>
+            <td className="content-center">
+              <Checkbox
+                hoverText="Allow users to upload content to this File Drop"
+                onChange={() => false}
+                key={2}
+                readOnly={false}
+                selected={thisPG.writeAccess}
+              />
+            </td>
+            <td className="content-center">
+              <Checkbox
+                hoverText="Allow users to delete content from this File Drop"
+                onChange={() => false}
+                key={3}
+                readOnly={false}
+                selected={thisPG.deleteAccess}
+              />
+            </td>
+            <td className="content-right">
+              {
+                !thisPG.isPersonalGroup &&
+                <ActionIconButtonContainer color="blue">
+                  <ActionIcon
+                    action={() => alert('expand')}
+                    icon="expand-card"
+                    label="Expand Permission Group"
+                    inline={true}
+                  />
+                </ActionIconButtonContainer>
+              }
+              {
+                // <ActionIconButtonContainer color="green">
+                //   <ActionIcon
+                //     action={() => alert('edit')}
+                //     icon="edit"
+                //     label="Edit Permission Group Name"
+                //     inline={true}
+                //   />
+                // </ActionIconButtonContainer>
+              }
+              <ActionIconButtonContainer color="red">
+                <ActionIcon
+                  action={() => alert('delete')}
+                  icon="delete"
+                  label="Delete Permission Group"
+                  inline={true}
+                />
+              </ActionIconButtonContainer>
+            </td>
+          </tr>
+          {
+            !thisPG.isPersonalGroup &&
+            thisPG.assignedMapUserIds.map((userId, index) => {
+              const thisUser = eligibleUsers[userId];
+              return (
+                <tr
+                  key={thisUser.id}
+                  className={index === (thisPG.assignedMapUserIds.length - 1) ? 'last-group-row' : null}
+                >
+                  <td><svg className="table-icon"><use xlinkHref="#remove-cirlce" /></svg></td>
+                  <td>{thisUser.firstName + ' ' + thisUser.lastName}</td>
+                  <td colSpan={5}>{thisUser.username}</td>
+                </tr>
+              );
+            })
+          }
+        </>
+      );
+    });
+    const addUserRow = (
+      <tr className="action-row" onClick={() => alert('Add User')}>
+        <td><svg className="table-icon"><use xlinkHref="#add-user" /></svg></td>
+        <td colSpan={6} className="action-text">Add User</td>
+      </tr>
+    );
+    const addGroupRow = (
+      <tr className="action-row" onClick={() => alert('Add Group')}>
+        <td><svg className="table-icon"><use xlinkHref="#add-group" /></svg></td>
+        <td colSpan={6} className="action-text">Add Group</td>
+      </tr>
+    );
+
     return (
       <table className="permission-group-table">
         <thead>
@@ -32,106 +149,9 @@ export class PermissionsTable extends React.Component {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td><svg className="table-icon"><use xlinkHref="#user" /></svg></td>
-            <td>Jane Doe</td>
-            <td>jane.doe@email.com</td>
-            <td className="content-center">
-              <Checkbox
-                hoverText="Allow users to download content from this File Drop"
-                onChange={() => false}
-                key={1}
-                readOnly={false}
-                selected={true}
-              />
-            </td>
-            <td className="content-center">
-              <Checkbox
-                hoverText="Allow users to upload content to this File Drop"
-                onChange={() => false}
-                key={2}
-                readOnly={false}
-                selected={true}
-              />
-            </td>
-            <td className="content-center">
-              <Checkbox
-                hoverText="Allow users to delete content from this File Drop"
-                onChange={() => false}
-                key={3}
-                readOnly={false}
-                selected={true}
-              />
-            </td>
-            <td className="content-right">
-              <ActionIconButtonContainer color="red">
-                <ActionIcon
-                  action={() => alert('delete')}
-                  icon="delete"
-                  label="Delete Permission Group"
-                  inline={true}
-                />
-              </ActionIconButtonContainer>
-            </td>
-          </tr>
-          <tr>
-            <td><svg className="table-icon"><use xlinkHref="#group" /></svg></td>
-            <td>Aetna</td>
-            <td />
-            <td className="content-center">
-              <Checkbox
-                hoverText="Allow users to download content from this File Drop"
-                onChange={() => false}
-                key={1}
-                readOnly={false}
-                selected={true}
-              />
-            </td>
-            <td className="content-center">
-              <Checkbox
-                hoverText="Allow users to upload content to this File Drop"
-                onChange={() => false}
-                key={2}
-                readOnly={false}
-                selected={true}
-              />
-            </td>
-            <td className="content-center">
-              <Checkbox
-                hoverText="Allow users to delete content from this File Drop"
-                onChange={() => false}
-                key={3}
-                readOnly={false}
-                selected={false}
-              />
-            </td>
-            <td className="content-right">
-              <ActionIconButtonContainer color="blue">
-                <ActionIcon
-                  action={() => alert('expand')}
-                  icon="expand-card"
-                  label="Expand Permission Group"
-                  inline={true}
-                />
-              </ActionIconButtonContainer>
-              <ActionIconButtonContainer color="green">
-                <ActionIcon
-                  action={() => alert('edit')}
-                  icon="edit"
-                  label="Edit Permission Group Name"
-                  inline={true}
-                />
-              </ActionIconButtonContainer>
-              <ActionIconButtonContainer color="red">
-                <ActionIcon
-                  action={() => alert('delete')}
-                  icon="delete"
-                  label="Delete Permission Group"
-                  inline={true}
-                />
-              </ActionIconButtonContainer>
-            </td>
-          </tr>
+          {permissionGroupsMarkup}
+          {addUserRow}
+          {addGroupRow}
         </tbody>
       </table>
     );
