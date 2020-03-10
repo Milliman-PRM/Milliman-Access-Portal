3f89d10250d1bb159f28a265da55a6d8009369dd
Michael Reisz <michael_reisz@yahoo.com>
2020-03-10 14:52:56 -0400

    Add some state to control edit mode for Permission Group tab

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/file-drop/redux/store.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/file-drop/redux/store.ts
@@ -50,6 +50,7 @@ export interface FileDropPendingState {
   fileDropToEdit: FileDropWithStats;
   selectedFileDropTab: AvailableFileDropTabs;
   permissionGroupsTab: PermissionGroupsReturnModel;
+  permissionGroupsEditMode: boolean;
 }
 
 /** State representing user-selected entities */
