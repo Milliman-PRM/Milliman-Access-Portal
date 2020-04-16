7e038f48f3a921d1a3205660e80de4ad4a26d669
tom.puckett <tom.puckett@milliman.com>
2020-04-16 17:01:04 -0400

    Use developer secrets when no environment specified

--- ContentPublishingServer/ContentPublishingServiceTests/DatabaseLifetimeFixture.cs
+++ ContentPublishingServer/ContentPublishingServiceTests/DatabaseLifetimeFixture.cs
@@ -87,30 +87,31 @@ namespace ContentPublishingServiceTests
             if (!string.IsNullOrEmpty(environmentName))
             {
                 configurationBuilder.AddJsonFile($"appsettings.{environmentName}.json", true);
+            }
 
-                switch (environmentName)
-                {
-                    case string name when name.Equals("Development", StringComparison.InvariantCultureIgnoreCase):
-                        configurationBuilder.AddUserSecrets<ProcessManager>();
-                        break;
+            switch (environmentName)
+            {
+                case null:
+                case string name when name.Equals("Development", StringComparison.InvariantCultureIgnoreCase):
+                    configurationBuilder.AddUserSecrets<ProcessManager>();
+                    break;
 
-                    case string name when name.Equals("CI", StringComparison.InvariantCultureIgnoreCase):
-                        IConfigurationRoot keyVaultAccessConfig = new ConfigurationBuilder().AddJsonFile($"AzureKeyVault.{environmentName}.json", true).Build();
+                case string name when name.Equals("CI", StringComparison.InvariantCultureIgnoreCase):
+                    IConfigurationRoot keyVaultAccessConfig = new ConfigurationBuilder().AddJsonFile($"AzureKeyVault.{environmentName}.json", true).Build();
 
-                        var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
-                        store.Open(OpenFlags.ReadOnly);
-                        var cert = store.Certificates.Find(X509FindType.FindByThumbprint, keyVaultAccessConfig["AzureCertificateThumbprint"], false);
+                    var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+                    store.Open(OpenFlags.ReadOnly);
+                    var cert = store.Certificates.Find(X509FindType.FindByThumbprint, keyVaultAccessConfig["AzureCertificateThumbprint"], false);
 
-                        if (cert.OfType<X509Certificate2>().Count() == 1)
-                        {
-                            configurationBuilder.AddAzureKeyVault(keyVaultAccessConfig["AzureVaultName"], keyVaultAccessConfig["AzureClientID"], cert.OfType<X509Certificate2>().Single());
-                        }
-                        else
-                        {
-                            throw new ApplicationException($"Found {cert.OfType<X509Certificate2>().Count()} certificate(s) to access Azure Key Vault for environment {environmentName}, expected 1");
-                        }
-                        break;
-                }
+                    if (cert.OfType<X509Certificate2>().Count() == 1)
+                    {
+                        configurationBuilder.AddAzureKeyVault(keyVaultAccessConfig["AzureVaultName"], keyVaultAccessConfig["AzureClientID"], cert.OfType<X509Certificate2>().Single());
+                    }
+                    else
+                    {
+                        throw new ApplicationException($"Found {cert.OfType<X509Certificate2>().Count()} certificate(s) to access Azure Key Vault for environment {environmentName}, expected 1");
+                    }
+                    break;
             }
 
             return configurationBuilder.Build();
