2026d75d4eb744f401c959755089a857ea5a3c9e
Tom Puckett <Tom.Puckett@milliman.com>
2020-05-07 10:14:00 -0400

    Order csv export by descending time

--- MillimanAccessPortal/MillimanAccessPortal/Controllers/FileDropController.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Controllers/FileDropController.cs
@@ -469,7 +469,7 @@ namespace MillimanAccessPortal.Controllers
                 { e => EF.Functions.ILike(e.EventData.ToString(), idCompareString) },
             };
 
-            List<ActivityEventModel> filteredEvents = await _auditLogger.GetAuditEventsAsync(filters, _dbContext);
+            List<ActivityEventModel> filteredEvents = await _auditLogger.GetAuditEventsAsync(filters, _dbContext, true);
 
             return Json(filteredEvents);
         }
@@ -507,7 +507,7 @@ namespace MillimanAccessPortal.Controllers
                 { e => EF.Functions.ILike(e.EventData.ToString(), idCompareString) },
             };
 
-            List<ActivityEventModel> filteredEvents = await _auditLogger.GetAuditEventsAsync(filters, _dbContext, false);
+            List<ActivityEventModel> filteredEvents = await _auditLogger.GetAuditEventsAsync(filters, _dbContext, true);
 
             string tempFilePath = Path.Combine(_applicationConfig.GetValue<string>("Storage:FileDropRoot"), $"{Guid.NewGuid()}.csv");
             
