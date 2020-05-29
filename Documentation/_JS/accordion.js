fba16465171e60e9c964cf8b85e1fdc452680ac5
Kelsie Stevenson <Kelsie.Stevenson@milliman.com>
2020-05-29 09:47:36 -0400

    style the accordion and page
    so that it sits center and is formatted to match the design system

--- Documentation/_JS/accordion.js
+++ Documentation/_JS/accordion.js
@@ -1,15 +1,18 @@
 
 var acc = document.getElementsByClassName("accordion");
+var icon = document.getElementsByClassName("icon");
 var content = document.getElementsByClassName("content");
 
 for (var i = 0; i < acc.length; i++) {
     acc[i].onclick = function() {
         var setClasses = !this.classList.contains('active');
         setClass(acc, 'active', 'remove');
+        setClass(icon, 'rotate', 'remove')
         setClass(content, 'show', 'remove');
 
         if (setClasses) {
             this.classList.toggle("active");
+            this.lastElementChild.classList.toggle("rotate")
             this.nextElementSibling.classList.toggle("show");
         }
     }
