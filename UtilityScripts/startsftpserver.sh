83a1791cf7370aff0c4425027f85d2399300abce
Steve Gredell <Steve.Gredell@milliman.com>
2020-06-02 18:04:04 -0400

    use absolute path for certificate tool so script runs at startup

--- UtilityScripts/startsftpserver.sh
+++ UtilityScripts/startsftpserver.sh
@@ -2,6 +2,6 @@
 
 export ASPNETCORE_ENVIRONMENT=STAGING
 
-./certificate-tool add -f /mnt/filedropshare/SPFileDropStaging.pfx -p $1 -t $2
+/app/certificate-tool add -f /mnt/filedropshare/SPFileDropStaging.pfx -p $1 -t $2
 
 dotnet /app/SftpServer.dll
