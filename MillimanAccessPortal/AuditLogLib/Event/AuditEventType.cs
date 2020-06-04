945297d03af5d01ad1aeb5a70328ae54bfe27b34
tom.puckett <tom.puckett@milliman.com>
2020-06-04 10:16:10 -0400

    Revise 2 audit event names with agreed file drop terminology

--- MillimanAccessPortal/AuditLogLib/Event/AuditEventType.cs
+++ MillimanAccessPortal/AuditLogLib/Event/AuditEventType.cs
@@ -1153,7 +1153,7 @@ namespace AuditLogLib.Event
             });
 
         public static readonly AuditEventType<SftpFileOperationLogModel> SftpFileWriteAuthorized = new AuditEventType<SftpFileOperationLogModel>(
-            8112, "SFTP File Write", (model) => new
+            8112, "SFTP File Upload", (model) => new
             {
                 model.FileName,
                 model.FileDrop,
@@ -1162,7 +1162,7 @@ namespace AuditLogLib.Event
             });
 
         public static readonly AuditEventType<SftpFileOperationLogModel> SftpFileReadAuthorized = new AuditEventType<SftpFileOperationLogModel>(
-            8113, "SFTP File Read", (model) => new
+            8113, "SFTP File Download", (model) => new
             {
                 model.FileName,
                 model.FileDrop,
