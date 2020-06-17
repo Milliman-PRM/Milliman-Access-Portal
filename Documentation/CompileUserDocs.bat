1b7f1df5bcde36f5e2b75250f761c79d723246b8
Michael Reisz <michael_reisz@yahoo.com>
2020-06-16 23:39:52 -0400

    Revamp the way that user guides are generated
    This switch uses a template to load the user guides into, and allows us to compile two separate html files into one for the File Drop Admin user guide.

--- Documentation/CompileUserDocs.bat
+++ Documentation/CompileUserDocs.bat
@@ -17,6 +17,7 @@ pandoc --self-contained --css=".\_CSS\style.css" --metadata=pagetitle:"Milliman 
 
 xcopy /I /D /Y "_CSS" "..\MillimanAccessPortal\MillimanAccessPortal\wwwroot\Documentation\_CSS"
 xcopy /I /D /Y "_JS" "..\MillimanAccessPortal\MillimanAccessPortal\wwwroot\Documentation\_JS"
+xcopy /F /Y "..\MillimanAccessPortal\MillimanAccessPortal\src\images\map-logo-white.svg" "..\MillimanAccessPortal\MillimanAccessPortal\wwwroot\images\map-logo-white.svg"
 
 echo Copying user guides
 for %%x in (
@@ -25,7 +26,10 @@ for %%x in (
  	ContentAccessAdmin
  	ContentPublishing
  	ClientAdmin
+	FileDrop
  	SystemAdmin
  	) do (
- 		copy ".\%%x.html" "..\MillimanAccessPortal\MillimanAccessPortal\wwwroot\Documentation\"
+		pandoc --metadata=pagetitle:"%%x" -o "..\MillimanAccessPortal\MillimanAccessPortal\wwwroot\Documentation\%%x.html" "%%x.html" --template=".\_TEMPLATES\user_guide_pandoc_template.html"
  	)
+
+pandoc --metadata=pagetitle:"File Drop Admin" -o "..\MillimanAccessPortal\MillimanAccessPortal\wwwroot\Documentation\FileDropAdmin.html" "FileDropAdmin.html" "FileDrop.html" --template=".\_TEMPLATES\user_guide_pandoc_template.html"
