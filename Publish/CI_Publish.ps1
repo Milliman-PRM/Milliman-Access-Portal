839281ef60df30029bc76425171bb4bd73c2cbb9
Steve Gredell <Steve.Gredell@milliman.com>
2020-07-06 12:11:44 -0400

    switch to untrimmed branch to match MAP package naming

--- Publish/CI_Publish.ps1
+++ Publish/CI_Publish.ps1
@@ -297,7 +297,7 @@ if ($LASTEXITCODE -ne 0)
 }
 
 $sFTPVersion = get-childitem "$rootpath\SftpServer\out\SftpServer.dll" -Recurse | Select-Object -expandproperty VersionInfo -First 1 | Select-Object -expandproperty ProductVersion
-$sFTPVersion = "$sFTPVersion-$TrimmedBranch"
+$sFTPVersion = "$sFTPVersion-$branchName"
 
 if($runTests) {
     log_statement "Performing MAP unit tests"
