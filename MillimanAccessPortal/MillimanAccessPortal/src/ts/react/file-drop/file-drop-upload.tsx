24d1958d380eb42cfcb54861d0a8cf0bed8a7132
Michael Reisz <michael_reisz@yahoo.com>
2020-07-19 22:55:39 -0400

    Call the appropriate actions to process the uploaded file
    This still needs some love since the polling action currently doesn't ever stop.

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/file-drop/file-drop-upload.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/file-drop/file-drop-upload.tsx
@@ -140,9 +140,23 @@ export class FileDropUpload extends React.Component<FileDropUploadProps, {}> {
       })
         .then((response) => response.json())
         .then((fileGUID: string) => {
+          // Move the file to the File Drop
+          fetch('FileDrop/ProcessUploadedFile', {
+            method: 'POST',
+            headers: Object.assign({}, this.resumableHeaders, {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            }),
+            body: JSON.stringify({
+              fileUploadId: fileGUID,
+              fileDropId: this.props.fileDropId,
+              fileDropDirectoryId: this.props.folderId,
+              fileName: resumableFile.fileName,
+            }),
+          });
           // Start a monitor that polls for status of asynchronous backend data finalization
           this.statusMonitor = new StatusMonitor<{}>(
-            `/FileUpload/FinalizeUpload?fileUploadId=${fileGUID}`,
+            `/FileDrop/GetFileUploadStatus?taskId=${fileGUID}&FileDropId=${this.props.fileDropId}`,
             (fileUpload: FileUpload) => {
               if (fileUpload.status === FileUploadStatus.Complete) {
                 this.progressMonitor.deactivate();
