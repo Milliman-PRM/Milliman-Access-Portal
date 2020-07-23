033236deef0f7c084358619afeb906ca0066e1e5
Michael Reisz <Michael.Reisz@milliman.com>
2020-07-22 23:08:05 -0400

    Make some of the elements of the CardExpansion component optional

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/card/card-expansion.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/card/card-expansion.tsx
@@ -3,9 +3,9 @@ import * as React from 'react';
 import CardButton from './card-button';
 
 export interface CardExpansionProps {
-  label: string;
+  label?: string;
   expanded: boolean;
-  setExpanded: (value: boolean) => void;
+  setExpanded?: (value: boolean) => void;
 }
 export class CardExpansion extends React.Component<CardExpansionProps> {
   public render() {
@@ -13,17 +13,23 @@ export class CardExpansion extends React.Component<CardExpansionProps> {
     return children
       ? (
         <div className={`card-expansion-container${expanded ? ' maximized' : ''}`}>
-          <h4 className="card-expansion-category-label">{label}</h4>
+          {
+            label &&
+            <h4 className="card-expansion-category-label">{label}</h4>
+          }
           {children}
-          <div className="card-button-bottom-container">
-            <CardButton
-              color={'blue'}
-              tooltip={(expanded ? 'Collapse' : 'Expand') + ' card'}
-              onClick={() => setExpanded(!expanded)}
-              icon={'expand-card'}
-              additionalClasses={['card-button-expansion']}
-            />
-          </div>
+          {
+            setExpanded &&
+            <div className="card-button-bottom-container">
+              <CardButton
+                color={'blue'}
+                tooltip={(expanded ? 'Collapse' : 'Expand') + ' card'}
+                onClick={() => setExpanded(!expanded)}
+                icon={'expand-card'}
+                additionalClasses={['card-button-expansion']}
+              />
+            </div>
+          }
         </div>
       )
       : null;
