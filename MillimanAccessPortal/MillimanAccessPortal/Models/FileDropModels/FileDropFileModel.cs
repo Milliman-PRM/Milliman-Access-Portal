27b135b6ed1de6baf44eb0c012a56001d91e2479
tom.puckett <tom.puckett@milliman.com>
2020-07-31 14:15:33 -0400

    File size and upload timestamp included in folder contents model

---
+++ MillimanAccessPortal/MillimanAccessPortal/Models/FileDropModels/FileDropFileModel.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * CODE OWNERS: Tom Puckett
+ * OBJECTIVE: Model class representing properties of a file that are needed by front end code
+ * DEVELOPER NOTES: <What future developers need to know.>
+ */
+
+using MapDbContextLib.Context;
+using System;
+
+namespace MillimanAccessPortal.Models.FileDropModels
+{
+    public class FileDropFileModel
+    {
+        public Guid Id { get; set; }
+
+        public string FileName { get; set; }
+
+        public string Description { get; set; }
+
+        public long Size { get; set; }
+
+        public DateTime? UploadDateTimeUtc { get; set; }
+
+        public FileDropFileModel(FileDropFile source)
+        {
+            Id = source.Id;
+            FileName = source.FileName;
+            Description = source.Description;
+            Size = source.Size;
+            UploadDateTimeUtc = source.UploadDateTimeUtc;
+        }
+    }
+}
