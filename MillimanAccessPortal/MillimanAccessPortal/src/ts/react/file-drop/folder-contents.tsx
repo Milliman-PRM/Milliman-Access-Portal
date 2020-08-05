1f0fadbc78e4b3b79327b5af41590d7936575d62
Michael Reisz <michael_reisz@yahoo.com>
2020-08-04 23:03:59 -0400

    Create a new component for displaying the file drop folder contents

---
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/file-drop/folder-contents.tsx
@@ -0,0 +1,116 @@
+import '../../../scss/react/file-drop/folder-contents.scss';
+
+import '../../../images/icons/file.svg';
+import '../../../images/icons/folder.svg';
+import '../../../images/icons/menu.svg';
+
+import * as React from 'react';
+import { FileDropDirectory, FileDropFile, Guid } from '../models';
+
+interface FolderContentsProps {
+  thisDirectory: FileDropDirectory;
+  directories: FileDropDirectory[];
+  files: FileDropFile[];
+  fileDropName: string;
+  fileDropId: Guid;
+  navigateTo: (fileDropId: Guid, canonicalPath: string) => void;
+}
+
+export class FolderContents extends React.Component<FolderContentsProps> {
+
+  public renderBreadCrumbs() {
+    const { fileDropId, navigateTo, thisDirectory } = this.props;
+    const pathDivider = '/';
+    const breadCrumbArray = thisDirectory.canonicalPath.split(pathDivider);
+    return breadCrumbArray.map((folder, i) => {
+      return (
+        <React.Fragment key={`breadcrumb-${i}`}>
+          <span
+            className={`breadcrumb-link${i === breadCrumbArray.length ? ' current' : null}`}
+            onClick={
+              i < breadCrumbArray.length
+                ? () => navigateTo(fileDropId, breadCrumbArray.slice(0, i).join(pathDivider))
+                : null
+            }
+          >
+            {folder}
+          </span>
+          {
+            i < breadCrumbArray.length &&
+            <span className="breadcrumb-divider">{pathDivider}</span>
+          }
+        </React.Fragment>
+      );
+    });
+  }
+
+  public renderFolders() {
+    const { directories } = this.props;
+    return directories.map((directory) => {
+      const [folderName] = directory.canonicalPath.split('/').slice(-1);
+      return (
+        <tr className="folder-row" key={directory.id}>
+          <td className="folder-icon">
+            <svg className="content-type-icon">
+              <use xlinkHref={'#folder'} />
+            </svg>
+          </td>
+          <td>{folderName}</td>
+          <td />
+          <td />
+          <td className="col-actions">
+            <svg className="menu-icon">
+              <use xlinkHref={'#menu'} />
+            </svg>
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  public renderFiles() {
+    const { files } = this.props;
+    return files.map((file) => {
+      return (
+        <tr key={file.id}>
+          <td className="file-icon">
+            <svg className="content-type-icon">
+              <use xlinkHref={'#file'} />
+            </svg>
+          </td>
+          <td>{file.fileName}</td>
+          <td>{file.size}</td>
+          <td>{file.uploadDateTimeUtc}</td>
+          <td className="col-actions">
+            <svg className="menu-icon">
+              <use xlinkHref={'#menu'} />
+            </svg>
+          </td>
+        </tr >
+      );
+    });
+  }
+
+  public render() {
+
+    return (
+      <div>
+        {this.renderBreadCrumbs()}
+        <table className="folder-content-table">
+          <thead>
+            <tr>
+              <th className="col-name" colSpan={2}>Name</th>
+              <th className="col-size">Size</th>
+              <th className="col-date-modified">Date Modified</th>
+              <th className="col-actions">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderFolders()}
+            {this.renderFiles()}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
