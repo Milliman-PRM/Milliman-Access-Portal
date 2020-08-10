2bd4dcc9041d4f814d7861ca552b98aa101f2b32
tom.puckett <tom.puckett@milliman.com>
2020-08-10 16:30:37 -0400

    Move common functionality to a base class

--- MillimanAccessPortal/MapDbContextLib/Models/ContentAssociatedFile.cs
+++ MillimanAccessPortal/MapDbContextLib/Models/ContentAssociatedFile.cs
@@ -5,19 +5,15 @@
  *                  and that is not one of the standard "related" files (icon, release notes, user guide). 
  */
 
-using MapCommonLib;
-using Serilog;
 using System;
 using System.ComponentModel;
 
 namespace MapDbContextLib.Models
 {
-    public class ContentAssociatedFile
+    public class ContentAssociatedFile : VerifyableFileBase
     {
         [ReadOnly(true)]
         public Guid Id { get; set; } // = Guid.NewGuid();
-
-        public string FullPath { get; set; } = string.Empty;
 
         public string DisplayName { get; set; } = string.Empty;
 
@@ -27,31 +23,16 @@ namespace MapDbContextLib.Models
 
         public ContentAssociatedFileType FileType { get; set; } = ContentAssociatedFileType.Unknown;
 
-        private string _Checksum = null;
         public string Checksum
         {
             get { return _Checksum; }
-            set { _Checksum = value?.ToLower(); }
+            set { _Checksum = value; }
         }
 
-
-        /// <summary>
-        /// Validates the bytes of a file agains the stored checksum property of this ContentAssociatedFile instance
-        /// </summary>
-        /// <returns>true if the stored checksum matches the file content</returns>
-        public bool ValidateChecksum()
+        public string FullPath
         {
-            (string fileChecksum, long length) = GlobalFunctions.GetFileChecksum(FullPath);
-
-            if (Checksum.Equals(fileChecksum, StringComparison.OrdinalIgnoreCase))
-            {
-                return true;
-            }
-            else
-            {
-                Log.Warning($"Checksums do not match: file checksum is {fileChecksum}, length is {length}");
-                return false;
-            }
+            get { return _FullPath; }
+            set { _FullPath = value; }
         }
     }
 }
--- MillimanAccessPortal/MapDbContextLib/Models/ContentRelatedFile.cs
+++ MillimanAccessPortal/MapDbContextLib/Models/ContentRelatedFile.cs
@@ -4,18 +4,10 @@
  * DEVELOPER NOTES: <What future developers need to know.>
  */
 
-using MapCommonLib;
-using Serilog;
-using System;
-
 namespace MapDbContextLib.Models
 {
-    public class ContentRelatedFile
+    public class ContentRelatedFile : VerifyableFileBase
     {
-        private string _Checksum = null;
-
-        public string FullPath { get; set; }
-
         /// <summary>
         /// Standard values: MasterContent, ReducedContent, UserGuide, Thumbnail, ReleaseNotes
         /// </summary>
@@ -24,26 +16,13 @@ namespace MapDbContextLib.Models
         public string Checksum
         {
             get { return _Checksum; }
-            set { _Checksum = value?.ToLower(); }
+            set { _Checksum = value; }
         }
-
-        /// <summary>
-        /// Validates the bytes of a file agains the stored checksum property of this ContentRelatedFile instance
-        /// </summary>
-        /// <returns>true if the stored checksum matches the file content</returns>
-        public bool ValidateChecksum()
+        public string FullPath
         {
-            (string fileChecksum, long length) = GlobalFunctions.GetFileChecksum(FullPath);
-
-            if (Checksum.Equals(fileChecksum, StringComparison.OrdinalIgnoreCase))
-            {
-                return true;
-            }
-            else
-            {
-                Log.Warning($"Checksums do not match: file checksum is {fileChecksum}, length is {length}");
-                return false;
-            }
+            get { return _FullPath; }
+            set { _FullPath = value; }
         }
+
     }
 }
