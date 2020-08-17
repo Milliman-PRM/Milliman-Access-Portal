8461b4e2f64eaa147ae478fd5b8031fb04768ee3
Evan Klein <Evan.Klein@Milliman.com>
2020-08-17 10:39:43 -0400

    Boilerplate UI setup

---
+++ MillimanAccessPortal/MillimanAccessPortal/Models/AccountViewModels/LoginStepTwoViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MillimanAccessPortal.Models.AccountViewModels
+{
+    public class LoginStepTwoViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+    }
+}
---
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/login-step-two/login-step-two.tsx
@@ -0,0 +1,14 @@
+﻿import * as React from 'react';
+
+import '../../../images/map-logo.svg';
+import '../../../scss/map.scss';
+
+export class LoginStepTwo extends React.Component {
+  public render() {
+    return (
+      <>
+        <h3>Login Step Two</h3>
+      </>
+    );
+  }
+}
