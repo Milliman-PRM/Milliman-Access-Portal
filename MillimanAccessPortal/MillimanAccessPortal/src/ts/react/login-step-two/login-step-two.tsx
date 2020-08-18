729cb30fe898b4bec7e0cbe0790cb1a5afebf214
Evan Klein <Evan.Klein@Milliman.com>
2020-08-18 11:02:58 -0400

    Created form for 2FA

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/login-step-two/login-step-two.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/login-step-two/login-step-two.tsx
@@ -1,6 +1,8 @@
-﻿import * as React from 'react';
+﻿import '../../../scss/react/login/login-step-two.scss';
 
-import { Button, Form } from 'carbon-components-react';
+import * as React from 'react';
+
+import { Input } from '../shared-components/form/input';
 
 import '../../../images/map-logo.svg';
 import '../../../scss/map.scss';
@@ -9,10 +11,53 @@ export class LoginStepTwo extends React.Component {
   public render() {
     return (
       <>
-        <h3>Login Step Two</h3>
-        <Form>
-          <Button>Log in</Button>
-        </Form>
+        <div className="form-content-container">
+          <div id="login-logo-container">
+            <svg id="login-logo">
+              <use xlinkHref={'#map-logo'} />
+            </svg>
+          </div>
+          <form>
+            <h3>Enter your authentication code</h3>
+            <p>Check your email to view your authentication code.</p>
+            <label htmlFor="username">Username</label>
+            <Input
+              name="username"
+              label={null}
+              type="text"
+              value={'eklein217@gmail.com'}
+              onChange={() => false}
+              error={''}
+              readOnly={true}
+            />
+            <label htmlFor="code">Authentication Code</label>
+            <Input
+              name="code"
+              label="Authentication code"
+              type="text"
+              value={''}
+              onChange={() => false}
+              error={''}
+            />
+            <div className="button-container">
+              <button
+                type="button"
+                className="white-button"
+                onClick={() => false}
+              >
+                Forgot password
+              </button>
+              <button
+                type="submit"
+                disabled={false}
+                className="blue-button"
+                onClick={() => false}
+              >
+                Log in
+              </button>
+            </div>
+          </form>
+        </div>
       </>
     );
   }
