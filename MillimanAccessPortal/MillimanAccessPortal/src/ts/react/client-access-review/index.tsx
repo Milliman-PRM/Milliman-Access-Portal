b7f4f832907e7e4652a7be5906243ea848f21740
Michael Reisz <Michael.Reisz@milliman.com>
2020-08-28 15:38:34 -0400

    Hook up the ClientAccessReview component to the index entrypoint

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/client-access-review/index.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/client-access-review/index.tsx
@@ -1,29 +1,29 @@
-﻿// declare function require(moduleName: string): any;
-   
-// import '../../../images/map-logo.svg';
-// import '../../../scss/map.scss';
-   
-// import * as React from 'react';
-// import * as ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-   
-// import { ConnectedContentAccessAdmin as Root } from './content-access-admin';
-// import { store } from './redux/store';
-   
-// let ConnectedContentAccessAdmin: typeof Root = require('./content-access-admin').ConnectedContentAccessAdmin;
-   
-// document.addEventListener('DOMContentLoaded', () => {
-   
-//   ReactDOM.render(
-//     <Provider store={store} >
-//       <ConnectedContentAccessAdmin />
-//     </Provider>,
-//     document.getElementById('content-container'),
-//   );
-// });
-   
-// if (module.hot) {
-//   module.hot.accept(['./content-access-admin'], () => {
-//     ConnectedContentAccessAdmin = require('./content-access-admin').ConnectedContentAccessAdmin;
-//   });
-// }
+﻿declare function require(moduleName: string): any;
+
+import '../../../images/map-logo.svg';
+import '../../../scss/map.scss';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import { ConnectedClientAccessReview as Root } from './client-access-review';
+import { store } from './redux/store';
+
+let ConnectedClientAccessReview: typeof Root = require('./client-access-review').ConnectedClientAccessReview;
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  ReactDOM.render(
+    <Provider store={store} >
+      <ConnectedClientAccessReview />
+    </Provider>,
+    document.getElementById('content-container'),
+  );
+});
+
+if (module.hot) {
+  module.hot.accept(['./client-access-review'], () => {
+    ConnectedClientAccessReview = require('./client-access-review').ConnectedClientAccessReview;
+  });
+}
