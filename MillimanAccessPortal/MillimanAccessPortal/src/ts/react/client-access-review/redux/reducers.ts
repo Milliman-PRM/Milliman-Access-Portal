a17e763816f2cc30addcf7fa2a5e32b534f2ea70
Michael Reisz <michael_reisz@yahoo.com>
2020-09-24 11:37:02 -0400

    Address review comment about initial state in reducer

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/client-access-review/redux/reducers.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/client-access-review/redux/reducers.ts
@@ -29,6 +29,12 @@ const _initialPendingData: PendingDataState = {
   approveAccessReview: false,
 };
 
+const _initialClientAccessReviewProgress: ClientAccessReviewProgress = {
+  step: 0,
+  contentItemConfirmations: null,
+  fileDropConfirmations: null,
+};
+
 /**
  * Create reducers for a subtree of the redux store
  */
@@ -132,11 +138,7 @@ const pendingData = createReducer<PendingDataState>(_initialPendingData, {
   }),
 });
 
-const reviewProgress = createReducer<ClientAccessReviewProgress>({
-    step: 0,
-    contentItemConfirmations: null,
-    fileDropConfirmations: null,
-}, {
+const reviewProgress = createReducer<ClientAccessReviewProgress>(_initialClientAccessReviewProgress, {
   FETCH_CLIENT_REVIEW_SUCCEEDED: (_state, action: AccessReviewActions.FetchClientReviewSucceeded) => {
     const contentItemConfirmations: Dict<boolean> = {};
     action.response.contentItems.map((ci) => {
@@ -175,14 +177,10 @@ const reviewProgress = createReducer<ClientAccessReviewProgress>({
     },
   }),
   CANCEL_CLIENT_ACCESS_REVIEW: () => ({
-    step: 0,
-    contentItemConfirmations: null,
-    fileDropConfirmations: null,
+    ..._initialClientAccessReviewProgress,
   }),
   APPROVE_CLIENT_ACCESS_REVIEW_SUCCEEDED: () => ({
-    step: 0,
-    contentItemConfirmations: null,
-    fileDropConfirmations: null,
+    ..._initialClientAccessReviewProgress,
   }),
 });
 
