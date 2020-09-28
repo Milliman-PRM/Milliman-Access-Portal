2860597840d0f0e9229a1447f11861c6edf71be8
Michael Reisz <michael_reisz@yahoo.com>
2020-09-28 16:10:15 -0400

    Add a spot in the state to hold the number for passing into the navbar

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/client-access-review/redux/store.ts
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/client-access-review/redux/store.ts
@@ -150,6 +150,7 @@ export interface AccessReviewStatePending {
   clientAccessReviewProgress: ClientAccessReviewProgress;
   pendingClientSelection: Guid;
   statusTries: number;
+  navBarRenderInt: number;
 }
 
 /**
