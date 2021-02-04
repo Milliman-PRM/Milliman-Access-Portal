1970a42a1939651fa3a552245eab1171ab10e778
Evan Klein <Evan.Klein@Milliman.com>
2021-02-04 13:33:46 -0500

    Include reminder about ToS on login page

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/login/login-form.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/login/login-form.tsx
@@ -102,6 +102,10 @@ export class LoginForm extends Form<{}, LoginFormState> {
           inputIcon="password"
           hidden={!userConfirmed}
         />
+        <p className="terms-reminder" style={{ marginTop: !userConfirmed ? '-5em' : null }}>
+          By continuing to log in, you are agreeing to Milliman Access Portal's
+          <a href="us.milliman.com/legal/terms"> Terms of Use</a>.
+        </p>
         {loginWarning && <div className="error-message">{loginWarning}</div>}
         <div className={'button-container' + (userConfirmed ? ' visible' : ' hidden')}>
           <a href="/Account/ForgotPassword" className="link-button">Forgot Password</a>
