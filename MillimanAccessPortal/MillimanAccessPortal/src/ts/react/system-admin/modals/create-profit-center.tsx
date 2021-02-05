c5c33c9ff444c0918d11a2d742bf21e4bcaccd80
Evan Klein <Evan.Klein@Milliman.com>
2021-02-05 16:26:11 -0500

    Front end changes for profit center quarterly maintenance list item

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/system-admin/modals/create-profit-center.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/system-admin/modals/create-profit-center.tsx
@@ -3,7 +3,13 @@ import '../../../../scss/react/shared-components/modal.scss';
 import * as React from 'react';
 import * as Modal from 'react-modal';
 
-import { postData } from '../../../shared';
+import { isEmailAddressValid, postData } from '../../../shared';
+import { Input, MultiAddInput } from '../../shared-components/form/input';
+
+// Toastr related imports
+import toastr = require('toastr');
+import '../../../lib-options';
+require('toastr/toastr.scss');
 
 interface CreateProfitCenterModalState {
   name: string;
@@ -12,10 +18,10 @@ interface CreateProfitCenterModalState {
   contact: string;
   email: string;
   phone: string;
+  quarterlyMaintenanceEmailRecipients: string[];
 }
 
 export class CreateProfitCenterModal extends React.Component<Modal.Props, CreateProfitCenterModalState> {
-
   private url: string = 'SystemAdmin/CreateProfitCenter';
 
   public constructor(props: Modal.Props) {
@@ -28,6 +34,7 @@ export class CreateProfitCenterModal extends React.Component<Modal.Props, Create
       contact: '',
       email: '',
       phone: '',
+      quarterlyMaintenanceEmailRecipients: [],
     };
 
     this.handleChangeName = this.handleChangeName.bind(this);
@@ -42,80 +49,109 @@ export class CreateProfitCenterModal extends React.Component<Modal.Props, Create
 
   public render() {
     return (
-      <Modal
-        ariaHideApp={false}
-        {...this.props}
-        className="modal"
-        overlayClassName="modal-overlay"
-      >
-        <h3 className="title blue">Create New Profit Center</h3>
-        <span className="modal-text">Profit Center Information</span>
-        <form onSubmit={this.handleSubmit}>
-          <span>
-            <label htmlFor="pcName">Name:</label>
-            <input
+      <>
+        <Modal
+          ariaHideApp={false}
+          {...this.props}
+          className="modal"
+          overlayClassName="modal-overlay"
+        >
+          <h3 className="title blue">Create New Profit Center</h3>
+          <span className="modal-text">Profit Center Information</span>
+          <form onSubmit={this.handleSubmit}>
+            <Input
               name="pcName"
+              label="Name"
+              value={this.state.name}
+              error={null}
               type="text"
               onChange={this.handleChangeName}
             />
-          </span>
-          <span>
-            <label htmlFor="pcCode">Code:</label>
-            <input
+            <Input
+              label="Code"
+              value={this.state.code}
+              error={null}
               name="pcCode"
               type="text"
               onChange={this.handleChangeCode}
             />
-          </span>
-          <span>
-            <label htmlFor="pcOffice">Office:</label>
-            <input
+            <Input
+              label="Office"
+              value={this.state.office}
+              error={null}
               name="pcOffice"
               type="text"
               onChange={this.handleChangeOffice}
             />
-          </span>
-          <span>
-            <label htmlFor="pcContact">Contact:</label>
-            <input
+            <Input
+              label="Contact"
               name="pcContact"
+              value={this.state.contact}
+              error={null}
               type="text"
               onChange={this.handleChangeContact}
             />
-          </span>
-          <span>
-            <label htmlFor="pcEmail">Email:</label>
-            <input
+            <Input
+              label="Email"
+              value={this.state.email}
+              error={null}
               name="pcEmail"
               type="text"
               onChange={this.handleChangeEmail}
             />
-          </span>
-          <span>
-            <label htmlFor="pcPhone">Phone:</label>
-            <input
+            <Input
+              label="Phone"
+              value={this.state.phone}
+              error={null}
               name="pcPhone"
               type="text"
               onChange={this.handleChangePhone}
             />
-          </span>
-          <div className="button-container">
-            <button
-              className="link-button"
-              type="button"
-              onClick={this.cancel}
-            >
-              Cancel
-            </button>
-            <button
-              className="blue-button"
-              type="submit"
-            >
-              Create Profit Center
-            </button>
-          </div>
-        </form>
-      </Modal>
+            <MultiAddInput
+              name="quarterlyMaintenanceEmailRecipients"
+              label="Quarterly Maintenance Email Recipients"
+              type="text"
+              list={this.state.quarterlyMaintenanceEmailRecipients}
+              value={''}
+              addItem={(item: string, _overLimit: boolean, itemAlreadyExists: boolean) => {
+                if (itemAlreadyExists) {
+                  toastr.warning('', 'That email already exists.');
+                } else if (!isEmailAddressValid(item)) {
+                  toastr.warning('', 'Please enter a valid email address (e.g. username@domain.com)');
+                } else {
+                  this.setState({
+                    quarterlyMaintenanceEmailRecipients: this.state.quarterlyMaintenanceEmailRecipients.concat(item),
+                  });
+                }
+              }}
+              removeItemCallback={(index: number) => {
+                this.setState({
+                  quarterlyMaintenanceEmailRecipients: this.state.quarterlyMaintenanceEmailRecipients.slice(0, index)
+                    .concat(this.state.quarterlyMaintenanceEmailRecipients.slice(index + 1)),
+                });
+              }}
+              readOnly={false}
+              onBlur={() => { return; }}
+              error={null}
+            />
+            <div className="button-container">
+              <button
+                className="link-button"
+                type="button"
+                onClick={this.cancel}
+              >
+                Cancel
+              </button>
+              <button
+                className="blue-button"
+                type="submit"
+              >
+                Create Profit Center
+              </button>
+            </div>
+          </form>
+        </Modal>
+      </>
     );
   }
 
@@ -164,6 +200,7 @@ export class CreateProfitCenterModal extends React.Component<Modal.Props, Create
       contactName: this.state.contact,
       contactEmail: this.state.email,
       contactPhone: this.state.phone,
+      quarterlyMaintenanceEmailRecipients: this.state.quarterlyMaintenanceEmailRecipients,
     })
     .then(() => {
       alert('Profit center created.');
