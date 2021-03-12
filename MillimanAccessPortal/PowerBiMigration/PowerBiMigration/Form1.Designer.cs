617aa848a6c160980c533dca69bc2ca6c0eee3d0
tom.puckett <tom.puckett@milliman.com>
2021-03-11 19:11:20 -0500

    WIP on pbi migration tooling

---
+++ MillimanAccessPortal/PowerBiMigration/PowerBiMigration/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace PowerBiMigration
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetAllDocs = new System.Windows.Forms.Button();
+            this.lstContentItems = new System.Windows.Forms.ListBox();
+            this.txtContentItemDetail = new System.Windows.Forms.TextBox();
+            this.btnAccessSelectedContent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGetAllDocs
+            // 
+            this.btnGetAllDocs.Location = new System.Drawing.Point(12, 12);
+            this.btnGetAllDocs.Name = "btnGetAllDocs";
+            this.btnGetAllDocs.Size = new System.Drawing.Size(199, 23);
+            this.btnGetAllDocs.TabIndex = 0;
+            this.btnGetAllDocs.Text = "Get All Documents";
+            this.btnGetAllDocs.UseVisualStyleBackColor = true;
+            this.btnGetAllDocs.Click += new System.EventHandler(this.BtnGetAllDocs_Click);
+            // 
+            // lstContentItems
+            // 
+            this.lstContentItems.FormattingEnabled = true;
+            this.lstContentItems.ItemHeight = 15;
+            this.lstContentItems.Location = new System.Drawing.Point(13, 42);
+            this.lstContentItems.Name = "lstContentItems";
+            this.lstContentItems.Size = new System.Drawing.Size(198, 94);
+            this.lstContentItems.TabIndex = 1;
+            this.lstContentItems.SelectedIndexChanged += new System.EventHandler(this.LstContentItems_SelectedIndexChanged);
+            // 
+            // txtContentItemDetail
+            // 
+            this.txtContentItemDetail.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtContentItemDetail.Location = new System.Drawing.Point(217, 42);
+            this.txtContentItemDetail.Multiline = true;
+            this.txtContentItemDetail.Name = "txtContentItemDetail";
+            this.txtContentItemDetail.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtContentItemDetail.Size = new System.Drawing.Size(571, 178);
+            this.txtContentItemDetail.TabIndex = 2;
+            // 
+            // btnAccessSelectedContent
+            // 
+            this.btnAccessSelectedContent.Location = new System.Drawing.Point(12, 226);
+            this.btnAccessSelectedContent.Name = "btnAccessSelectedContent";
+            this.btnAccessSelectedContent.Size = new System.Drawing.Size(199, 23);
+            this.btnAccessSelectedContent.TabIndex = 3;
+            this.btnAccessSelectedContent.Text = "Access Selected Content";
+            this.btnAccessSelectedContent.UseVisualStyleBackColor = true;
+            this.btnAccessSelectedContent.Click += new System.EventHandler(this.BtnAccessSelectedContent_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAccessSelectedContent);
+            this.Controls.Add(this.txtContentItemDetail);
+            this.Controls.Add(this.lstContentItems);
+            this.Controls.Add(this.btnGetAllDocs);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetAllDocs;
+        private System.Windows.Forms.ListBox lstContentItems;
+        private System.Windows.Forms.TextBox txtContentItemDetail;
+        private System.Windows.Forms.Button btnAccessSelectedContent;
+    }
+}
+
---
+++ MillimanAccessPortal/PowerBiMigration/PowerBiMigration/Form1.cs
@@ -0,0 +1,97 @@
+﻿using MapDbContextLib.Context;
+using MapDbContextLib.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PowerBiLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerBiMigration
+{
+    public partial class Form1 : Form
+    {
+        private string _mapConnectionString = null;
+        private DbContextOptions<ApplicationDbContext> _dbOptions = null;
+        private PowerBiConfig _pbiConfig = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            IConfigurationBuilder configBuilder = new ConfigurationBuilder();
+            configBuilder.AddJsonFile("appSettings.json", false);
+            IConfigurationRoot _appConfig = configBuilder.Build();
+
+            _pbiConfig = new PowerBiConfig
+            {
+                PbiGrantType = _appConfig.GetValue<string>("PbiGrantType"),
+                PbiAuthenticationScope = _appConfig.GetValue<string>("PbiAuthenticationScope"),
+                PbiAzureADClientId = _appConfig.GetValue<string>("PbiAzureADClientID"),
+                PbiAzureADClientSecret = _appConfig.GetValue<string>("PbiAzureADClientSecret"),
+                PbiAzureADUsername = _appConfig.GetValue<string>("PbiAzureADUsername"),
+                PbiAzureADPassword = _appConfig.GetValue<string>("PbiAzureADPassword"),
+                PbiTenantId = _appConfig.GetValue<string>("PbiTenantId"),
+            };
+
+            _mapConnectionString = "Server=localhost;Database=MillimanAccessPortalWithGuids;User Id=postgres;Password=postgres;";
+            _dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>().UseNpgsql(_mapConnectionString).Options;
+
+            lstContentItems.DisplayMember = "ContentName";
+            btnAccessSelectedContent.Enabled = false;
+        }
+
+        private void BtnGetAllDocs_Click(object sender, EventArgs e)
+        {
+            using (var db = new ApplicationDbContext(_dbOptions))
+            {
+                ContentType powerBiContentType = db.ContentType.Single(t => t.TypeEnum == ContentTypeEnum.PowerBi);
+
+                List<RootContentItem> pbiContentItems = db.RootContentItem
+                                                          //.Include(c => c.TypeSpecificDetailObject)
+                                                          .Where(c => c.ContentTypeId == powerBiContentType.Id)
+                                                          .ToList();
+
+                lstContentItems.Items.AddRange(pbiContentItems.Select(c => new { c.ContentName, c }).ToArray());
+            }
+
+        }
+
+        private void LstContentItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var lstItem = lstContentItems.SelectedItem;
+            btnAccessSelectedContent.Enabled = false;
+            if (lstItem != null)
+            {
+                Type itemType = lstItem.GetType();
+                PropertyInfo contentItemPropertyInfo = itemType.GetProperty("c");
+                var contentItem = contentItemPropertyInfo.GetValue(lstItem) as RootContentItem;
+
+                txtContentItemDetail.Text = JsonSerializer.Serialize(contentItem, contentItem.GetType(), new JsonSerializerOptions { WriteIndented = true, IgnoreReadOnlyProperties = true });
+
+                btnAccessSelectedContent.Enabled = true;
+            }
+        }
+
+        private void BtnAccessSelectedContent_Click(object sender, EventArgs e)
+        {
+            var lstItem = lstContentItems.SelectedItem;
+            Type itemType = lstItem.GetType();
+            PropertyInfo contentItemPropertyInfo = itemType.GetProperty("c");
+            RootContentItem contentItem = contentItemPropertyInfo.GetValue(lstItem) as RootContentItem;
+            TypeSpecificContentItemProperties metaData = contentItem.TypeSpecificDetailObject;
+
+            Task<PowerBiLibApi> pbiApiTask = Task.Run(() => new PowerBiLibApi(_pbiConfig).InitializeAsync());
+            while (!pbiApiTask.IsCompleted) Thread.Sleep(100);
+            var pbiApi = pbiApiTask.Result;
+        }
+    }
+}
