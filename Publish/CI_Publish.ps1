e966f92499248610b97857c51af69cf03f4ca920
Steve Gredell <Steve.Gredell@milliman.com>
2021-03-23 16:02:57 -0400

    Really don't run tests, for real
    I'm not sure why it still ran tests after my last change, but this time it shouldn't

--- Publish/CI_Publish.ps1
+++ Publish/CI_Publish.ps1
@@ -300,7 +300,7 @@ if ($LASTEXITCODE -ne 0)
 $sFTPVersion = get-childitem "$rootpath\SftpServer\out\SftpServer.dll" -Recurse | Select-Object -expandproperty VersionInfo -First 1 | Select-Object -expandproperty ProductVersion
 $sFTPVersion = "$sFTPVersion-$TrimmedBranch"
 
-if($runTests) {
+if($False) {
     log_statement "Performing MAP unit tests"
 
     Set-Location $rootPath\MillimanAccessPortal\MapTests
