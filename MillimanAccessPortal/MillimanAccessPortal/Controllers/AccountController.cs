36342cef473c3a0ee873b9626c74920ea71763dc
tom.puckett <tom.puckett@milliman.com>
2021-07-08 11:20:48 -0400

    Substitute support email for placeholder text

--- MillimanAccessPortal/MillimanAccessPortal/Controllers/AccountController.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Controllers/AccountController.cs
@@ -229,7 +229,8 @@ namespace MillimanAccessPortal.Controllers
                     NotifyUserAboutDisabledAccount(user);
                     Log.Information($"{ControllerContext.ActionDescriptor.DisplayName}, user {model.Username} disabled, local login rejected");
                     _auditLogger.Log(AuditEventType.LoginFailure.ToEvent(model.Username, (await _authentService.Schemes.GetDefaultAuthenticateSchemeAsync()).Name, LoginFailureReason.UserAccountDisabled), null);
-                    Response.Headers.Add("Warning", UserMessageEnum.AccountDisabled.GetDisplayDescriptionString());
+                    Response.Headers.Add("Warning", UserMessageEnum.AccountDisabled.GetDisplayDescriptionString().Replace("{MapSupportEmail}", 
+                                                                                                                          $"<a href =\"mailto:{GlobalFunctions.MillimanSupportEmailAlias}\">{GlobalFunctions.MillimanSupportEmailAlias}</a>"));
                     return Ok();
                 }
 
@@ -238,7 +239,8 @@ namespace MillimanAccessPortal.Controllers
                     _auditLogger.Log(AuditEventType.LoginIsSuspended.ToEvent(user.UserName), null);
                     Log.Information($"{ControllerContext.ActionDescriptor.DisplayName}, User {user.UserName} suspended, local login rejected");
 
-                    Response.Headers.Add("Warning", UserMessageEnum.AccountSuspended.GetDisplayDescriptionString());
+                    Response.Headers.Add("Warning", UserMessageEnum.AccountSuspended.GetDisplayDescriptionString().Replace("{MapSupportEmail}", 
+                                                                                                                           $"<a href =\"mailto:{GlobalFunctions.MillimanSupportEmailAlias}\">{GlobalFunctions.MillimanSupportEmailAlias}</a>"));
                     return Ok();
                 }
 
