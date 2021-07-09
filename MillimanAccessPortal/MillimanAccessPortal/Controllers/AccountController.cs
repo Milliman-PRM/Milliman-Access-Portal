229ae39a4975478d07780a50130d2ceca567221d
Tom Puckett <Tom.Puckett@milliman.com>
2021-07-09 17:47:17 -0400

    Reusable function replaces multiple identical string manipulations

--- MillimanAccessPortal/MillimanAccessPortal/Controllers/AccountController.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Controllers/AccountController.cs
@@ -229,8 +229,9 @@ namespace MillimanAccessPortal.Controllers
                     NotifyUserAboutDisabledAccount(user);
                     Log.Information($"{ControllerContext.ActionDescriptor.DisplayName}, user {model.Username} disabled, local login rejected");
                     _auditLogger.Log(AuditEventType.LoginFailure.ToEvent(model.Username, (await _authentService.Schemes.GetDefaultAuthenticateSchemeAsync()).Name, LoginFailureReason.UserAccountDisabled), null);
-                    Response.Headers.Add("Warning", UserMessageEnum.AccountDisabled.GetDisplayDescriptionString().Replace("{MapSupportEmail}", 
-                                                                                                                          $"<a href =\"mailto:{GlobalFunctions.MillimanSupportEmailAlias}\">{GlobalFunctions.MillimanSupportEmailAlias}</a>"));
+
+                    Response.Headers.Add("Warning", new UserMessageModel(UserMessageEnum.AccountDisabled).PrimaryMessages.First());
+
                     return Ok();
                 }
 
@@ -239,8 +240,8 @@ namespace MillimanAccessPortal.Controllers
                     _auditLogger.Log(AuditEventType.LoginIsSuspended.ToEvent(user.UserName), null);
                     Log.Information($"{ControllerContext.ActionDescriptor.DisplayName}, User {user.UserName} suspended, local login rejected");
 
-                    Response.Headers.Add("Warning", UserMessageEnum.AccountSuspended.GetDisplayDescriptionString().Replace("{MapSupportEmail}", 
-                                                                                                                           $"<a href =\"mailto:{GlobalFunctions.MillimanSupportEmailAlias}\">{GlobalFunctions.MillimanSupportEmailAlias}</a>"));
+                    Response.Headers.Add("Warning", new UserMessageModel(UserMessageEnum.AccountSuspended).PrimaryMessages.First());
+
                     return Ok();
                 }
 
