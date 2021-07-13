928813ffa5089860b71afc59210e9fd7a6925c7c
Justin Rice <justin.rice@milliman.com>
2021-07-13 16:13:59 -0400

    Update create-initial-user.tsx

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/create-initial-user/create-initial-user.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/create-initial-user/create-initial-user.tsx
@@ -7,10 +7,8 @@ import '../../../scss/map.scss';
 export class CreateInitialUser extends React.Component {
     public render() {
         return (
-        <div id="content-container">
-          <div className="form-content-container flex-item-for-tablet-up-10-12 flex-item-for-desktop-up-6-12">
-            <InitialUserForm />
-          </div>
+        <div className="form-content-container flex-item-for-tablet-up-10-12 flex-item-for-desktop-up-6-12">
+          <InitialUserForm />
         </div>
         );
     }
