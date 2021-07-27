9f3372cbbda01400fe0529c771ba6cde95a69043
Justin Rice <justin.rice@milliman.com>
2021-07-27 11:15:05 -0400

    added update user agreement index file

---
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/system-admin/update-user-agreement/index.tsx
@@ -0,0 +1,20 @@
+﻿declare function require(moduleName: string): any;
+
+import '../../../../images/map-logo.svg';
+import '../../../../scss/map.scss';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { UpdateUserAgreement as Root} from './update-user-agreement';
+
+let UpdateUserAgreement: typeof Root = require('./update-user-agreement').UpdateUserAgreement;
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(<UpdateUserAgreement />, document.getElementById('content-container'));
+});
+
+if (module.hot) {
+    module.hot.accept(['./update-user-agreement'], () => {
+        UpdateUserAgreement = require('./update-user-agreement').SystemAdmin;
+    });
+}
