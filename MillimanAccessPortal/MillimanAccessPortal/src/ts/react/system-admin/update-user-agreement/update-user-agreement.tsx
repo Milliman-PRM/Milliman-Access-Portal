ef11016ebf9c9f1f21f1b869176f92c70ecafb69
Justin Rice <justin.rice@milliman.com>
2021-07-27 15:09:53 -0400

    added toggle

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/system-admin/update-user-agreement/update-user-agreement.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/system-admin/update-user-agreement/update-user-agreement.tsx
@@ -1,23 +1,55 @@
 ﻿import * as React from 'react';
+
 import '../../../../../src/scss/update-user-agreement.scss';
 import '../../../../images/map-logo.svg';
 import '../../../../scss/map.scss';
+import { convertMarkdownToHTML } from '../../../../ts/convert-markdown';
 
 export class UpdateUserAgreement extends React.Component {
-    public constructor(props: {}) {
-        super(props);
+    public state = {
+        visible: true ,
+    };
+
+    public handleToggle = () => {
+        this.setState({ visible: !this.state.visible });
+
     }
+    public setToEdit = () => {
+        const rawAgreementMarkdown = (document.getElementById('newAgreementText') as HTMLTextAreaElement).value;
+        const processedAgreementHTML = convertMarkdownToHTML(rawAgreementMarkdown);
+        document.getElementById('AgreementPreview').innerHTML = processedAgreementHTML;
+    }
+
     public render() {
+        const { visible } = this.state;
         return (
             <div id="agreement-container" className="admin-panel-content-container">
                 <form id="user-agreement-form" action="updateUserAgreement" method="POST">
                     <h3 className="section-title">User Agreement Text</h3>
                     <div className="markdown-view-button-container">
-                        <span className="markdown-view-toggle markdown-select-edit selected">Edit</span>
-                        <span className="markdown-view-toggle markdown-select-preview">Preview</span>
+                        <span
+                            className={
+                              `markdown-view-toggle markdown-select-edit'${visible ? ' selected' : ''}`}
+                            onClick={() => {
+                              this.handleToggle();
+                              this.setToEdit();
+                            }}
+                        >
+                        Edit
+                        </span>
+                        <span
+                          className={
+                            `markdown-view-toggle markdown-select-edit'${!visible ? ' selected' : ''}`}
+                          onClick={this.handleToggle}
+                        >
+                        Preview
+                        </span>
                     </div>
                     <div id="user-agreement-text">
-                        <div id="AgreementPreview" /* style="display: none;" */ />
+                      <textarea
+                            id="newAgreementText" // need to change
+                       // disabled={visible ? false : true}
+                      />
                     </div>
                     <div className="button-container">
                         <button id="update-button" type="submit" className="green-button">Update</button>
