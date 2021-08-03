e1532e9329dae9a17ea85dc1d7722de9ab30f8f3
Justin Rice <justin.rice@milliman.com>
2021-08-03 11:42:16 -0400

    removed interface file / fixed create initial user formatting

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/create-initial-user/create-initial-user.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/create-initial-user/create-initial-user.tsx
@@ -1,16 +1,12 @@
 ﻿import * as React from 'react';
 import { InitialUserForm } from './create-initial-user-form';
 
-import '../../../images/map-logo.svg';
-import '../../../scss/map.scss';
-
 export class CreateInitialUser extends React.Component {
     public render() {
         return (
-        <div className="form-content-container flex-item-for-tablet-up-10-12 flex-item-for-desktop-up-6-12">
-          <InitialUserForm />
-        </div>
+           <div className="form-content-container flex-item-for-tablet-up-10-12 flex-item-for-desktop-up-6-12">
+              <InitialUserForm />
+           </div>
         );
     }
-
 }
