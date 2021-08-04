24b9b1031917f7d5a00f7901722a11b50a4d6e52
Justin Rice <justin.rice@milliman.com>
2021-08-04 16:27:35 -0400

    addressing change requests

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/system-admin/update-user-agreement/update-user-agreement.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/system-admin/update-user-agreement/update-user-agreement.tsx
@@ -1,13 +1,16 @@
 ﻿import * as React from 'react';
 
 import '../../../../../src/scss/update-user-agreement.scss';
+
 import { convertMarkdownToHTML } from '../../../../ts/convert-markdown';
 import { postData } from '../../../shared';
 
 interface UpdateUserAgreementState {
-    visible: boolean;
+    viewSelect: 'edit' | 'preview';
     originalAgreementText: string;
     newAgreement: string;
+    previewHTML: string;
+    errorMessage: string;
 }
 
 export class UpdateUserAgreement extends React.Component<{}, UpdateUserAgreementState> {
@@ -16,18 +19,18 @@ export class UpdateUserAgreement extends React.Component<{}, UpdateUserAgreement
       super(props);
 
       this.state = {
-        visible: true,
+        viewSelect: 'edit',
         originalAgreementText: '',
         newAgreement: '',
+        previewHTML: '',
+        errorMessage: '',
       };
-
-      this.handleChangeMessage = this.handleChangeMessage.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
 
     }
 
     public render() {
-      const { visible } = this.state;
+      const { viewSelect } = this.state;
       return (
         <div id="agreement-container" className="admin-panel-content-container">
           <form id="user-agreement-form" onSubmit={this.handleSubmit}>
@@ -35,34 +38,38 @@ export class UpdateUserAgreement extends React.Component<{}, UpdateUserAgreement
             <div className="markdown-view-button-container">
               <span
                 className={
-                `markdown-view-toggle markdown-select-edit'${visible ? ' selected' : ''}`}
-                onClick={this.handleToggle}
+                `markdown-view-toggle markdown-select-edit'${viewSelect === 'edit' ? ' selected' : ''}`}
+                onClick={this.setToEdit}
               >
                 Edit
               </span>
               <span
+               // TODO change span to tab row
                 className={
-                `markdown-view-toggle markdown-select-edit'${!visible ? ' selected' : ''}`}
-                onClick={() => {
-                this.handleToggle();
-                this.setToPreview();
-                }}
+                `markdown-view-toggle markdown-select-edit'${viewSelect === 'preview' ? ' selected' : ''}`}
+                onClick={this.setToPreview}
               >
                 Preview
               </span>
             </div>
             <div id="user-agreement-text">
-              <textarea
-                id="newAgreementText"
-                name="newAgreementText"
-                style={{ display: !visible ? 'none' : '' }}
-                onChange={this.handleChangeMessage}
-                value={this.state.newAgreement}
-              />
-              <div
-                id="AgreementPreview"
-                style={{display: visible ? 'none' : ''}}
-              />
+              {
+                this.state.errorMessage &&
+                <div className="error-message">
+                {this.state.errorMessage}
+                </div>
+               }
+               {
+                viewSelect === 'edit' ?
+                  <textarea
+                    id="newAgreementText"
+                    name="newAgreementText"
+                    onChange={(evt) => this.handleChangeMessage(evt)}
+                    value={this.state.newAgreement}
+                  />
+                  :
+                  <div dangerouslySetInnerHTML={{ __html: this.state.previewHTML }} />
+              }
             </div>
             <div className="button-container">
               <button
@@ -70,6 +77,7 @@ export class UpdateUserAgreement extends React.Component<{}, UpdateUserAgreement
                 type="submit"
                 className="green-button"
                 disabled={this.state.originalAgreementText === this.state.newAgreement}
+
               >
                 Update
               </button>
@@ -79,23 +87,18 @@ export class UpdateUserAgreement extends React.Component<{}, UpdateUserAgreement
         );
     }
 
-    public componentDidMount() {
-        const newAgreementText = (document.getElementById('userAgreementText') as HTMLTextAreaElement).value;
+    private setToEdit = () => {
         this.setState({
-            originalAgreementText: newAgreementText,
-            newAgreement: newAgreementText,
+            viewSelect: 'edit',
         });
-        document.getElementById('userAgreementText').innerHTML = newAgreementText;
-    }
-
-    private handleToggle = () => {
-      this.setState({ visible: !this.state.visible });
     }
 
     private setToPreview = () => {
-        const rawAgreementMarkdown = (document.getElementById('newAgreementText') as HTMLTextAreaElement).value;
-        const processedAgreementHTML = convertMarkdownToHTML(rawAgreementMarkdown);
-        document.getElementById('AgreementPreview').innerHTML = processedAgreementHTML;
+        const processedAgreementHTML = convertMarkdownToHTML(this.state.newAgreement);
+        this.setState({
+            previewHTML: processedAgreementHTML,
+            viewSelect: 'preview',
+        });
     }
 
     private handleChangeMessage(event: React.ChangeEvent<HTMLTextAreaElement>) {
@@ -106,12 +109,16 @@ export class UpdateUserAgreement extends React.Component<{}, UpdateUserAgreement
 
     private handleSubmit(event: React.MouseEvent<HTMLFormElement> | React.KeyboardEvent<HTMLFormElement>) {
         event.preventDefault();
-        event.persist();
         postData('/SystemAdmin/UpdateUserAgreement', { newAgreementText: this.state.newAgreement }, true)
             .then((response) => {
                 if (response.ok) {
                     window.location.replace('/');
                 }
+            })
+            .catch(() => {
+                this.setState({
+                    errorMessage: 'an error occured',
+                });
             });
     }
 }
