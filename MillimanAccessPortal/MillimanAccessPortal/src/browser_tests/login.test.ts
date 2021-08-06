c43e0515014ff7231239a795d5911b738be7377f
Michael Reisz <michael_reisz@yahoo.com>
2021-08-06 14:26:07 -0400

    Get rid of unused (and likely seriously outdated) browser tests

--- MillimanAccessPortal/MillimanAccessPortal/src/browser_tests/login.test.ts
+++
@@ -1,9 +0,0 @@
-import { Selector } from 'testcafe';
-
-fixture('Login')
-  .page('https://localhost:44336');
-
-test('loads properly', async (t) => {
-  const loginContainer = Selector('#login-container');
-  await t.expect(loginContainer.exists).ok();
-});
