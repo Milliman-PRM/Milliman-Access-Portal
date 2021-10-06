64c43f930535da0d2a71ab3ba15b6e603045ebf7
Shubhi Sethi <Shubhi.Sethi@milliman.com>
2021-10-06 17:04:38 -0400

    Doing this to remove extra output of API.

--- Publish/CI_Publish.ps1
+++ Publish/CI_Publish.ps1
@@ -165,8 +165,10 @@ if ($buildType -ne "Release")
 log_statement "Restoring packages and building MAP"
 
 # Switch to the correct version of Node.js using NVM
-$url = "http://localhost:8042/nvm_use?version=14.18.0"
-Invoke-Webrequest $url
+$url   = "http://localhost:8042/nvm_use?version=14.18.0"
+$result = Invoke-Webrequest $url
+log_statement "Status code: $($result.StatusCode)"
+log_statement "Content: $($result.Content)"
 
 if ($LASTEXITCODE -ne 0) {
     log_statement "ERROR: Switching to Node.js v14.18.0 failed"
