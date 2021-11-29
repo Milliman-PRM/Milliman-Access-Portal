3a8bd17d0a4952dfa079a9acee5c55f737ecfb72
Michael Reisz <michael_reisz@yahoo.com>
2021-11-29 15:22:31 -0500

    Add new container app content type and migration

---
+++ MillimanAccessPortal/MillimanAccessPortal/Migrations/20211129183451_AddContainerAppContentType.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MillimanAccessPortal.Migrations
+{
+    public partial class AddContainerAppContentType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TYPE content_type_enum ADD VALUE 'container_app'", suppressTransaction: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TYPE content_type_enum RENAME TO content_type_enum_old; " +
+                "CREATE TYPE content_type_enum AS ENUM('unknown', 'qlikview', 'html', 'pdf', 'file_download', 'power_bi'); " +
+                "DELETE FROM \"ContentType\" WHERE \"TypeEnum\" = 'container_app'; " +
+                "ALTER TABLE \"ContentType\" " +
+                "   ALTER COLUMN \"TypeEnum\" DROP DEFAULT, " +
+                "   ALTER COLUMN \"TypeEnum\" TYPE content_type_enum USING \"TypeEnum\"::text::content_type_enum, " +
+                "   ALTER COLUMN \"TypeEnum\" SET DEFAULT 'unknown'; " +
+                "DROP TYPE content_type_enum_old; ", 
+                suppressTransaction: true);
+        }
+    }
+}
