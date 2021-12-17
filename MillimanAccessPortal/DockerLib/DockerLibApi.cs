c473731bd816d9a376901b898d36c070417d4e58
Evan Klein <Evan.Klein@Milliman.com>
2021-12-17 13:19:44 -0500

    Added repository get request + removed demo

--- MillimanAccessPortal/DockerLib/DockerLibApi.cs
+++ MillimanAccessPortal/DockerLib/DockerLibApi.cs
@@ -20,8 +20,9 @@ namespace DockerLib
 {
     public class DockerLibApi
     {
-        public DockerLibApiConfig Config { get; set; }
-        private string _acrToken { get; set; }
+        public DockerLibApiConfig Config { get; private set; }
+        private ContainerRegistryClient _containerRegistryClient;
+        private string _acrToken;
 
         public DockerLibApi(DockerLibApiConfig config)
         {
@@ -37,6 +38,12 @@ namespace DockerLib
             try
             {
                 await GetAccessTokenAsync();
+
+                ContainerRegistryClient client = new ContainerRegistryClient(
+                    new Uri(Config.RegistryUrl),
+                    new DefaultAzureCredential(), // TODO 
+                    new ContainerRegistryClientOptions() { Audience = ContainerRegistryAudience.AzureResourceManagerPublicCloud }
+                );
             }
             catch (Exception ex)
             {
@@ -66,6 +73,28 @@ namespace DockerLib
             catch (Exception ex)
             {
                 Log.Warning(ex, "Exception attempting to get ACR access token");
+                throw;
+            }
+        }
+
+        public async Task<List<ContainerRepository>> GetRepositories()
+        {
+            try
+            {
+                AsyncPageable<string> repositoryNames = _containerRegistryClient.GetRepositoryNamesAsync();
+
+                List<ContainerRepository> containerRepositories = new();
+                await foreach (string repositoryName in repositoryNames)
+                {
+                    ContainerRepository repository = _containerRegistryClient.GetRepository(repositoryName);
+                    containerRepositories.Add(repository);
+                }
+
+                return containerRepositories;
+            }
+            catch (Exception ex)
+            {
+                Log.Warning(ex, "Exception attempting to fetch repositories.");
                 throw;
             }
         }
@@ -112,7 +141,6 @@ namespace DockerLib
         {
             // temp
             string hardCodedRepoName = "new-repo";
-            string hardCodedManifest = "{\"Config\":\"feb5d9fea6a5e9606aa995e879d862b825965ba48de054caab5ef356dc6b3412.json\",\"RepoTags\":[\"prmcontainertest.azurecr.io / hello - world:latest\"],\"Layers\":[\"e07ee1baac5fae6a26f30cabfe54a36d3402f96afda318fe0a96cec4ca393359.tar\"]}";
             string hardCodedLayerDigest = "sha256:e07ee1baac5fae6a26f30cabfe54a36d3402f96afda318fe0a96cec4ca393359";
             string hardCodedImageDigest = "sha256:b9935d4e8431fb1a7f0989304ec86b3329a99a25f5efdc7f09f3f8c41434ca6d";
             string hardCodedLayerLocation = @"C:\Users\Evan.Klein\source\Misc\hello-world\e07ee1baac5fae6a26f30cabfe54a36d3402f96afda318fe0a96cec4ca393359.tar";
@@ -120,12 +148,13 @@ namespace DockerLib
 
             #region Compile layers
             List<string> layerNames;
+            JObject manifestObj;
             FileStream fs = File.OpenRead(hardCodedManifestLocation);
             using (StreamReader streamReader = new StreamReader(fs))
             {
                 string fileContents = streamReader.ReadToEnd().Trim(new char[] { '[', ']' });
-                JObject manifestJson = JObject.Parse(fileContents);
-                layerNames = manifestJson.SelectToken("Layers").ToObject<List<string>>();
+                manifestObj = JObject.Parse(fileContents);
+                layerNames = manifestObj.SelectToken("Layers").ToObject<List<string>>();
             }
             #endregion
 
@@ -133,10 +162,10 @@ namespace DockerLib
             {
                 foreach (string layerName in layerNames)
                 {
-                    string digest = layerName.Substring(0, layerName.Length - 5); // Strips .tar
+                    string digest = Path.GetFileNameWithoutExtension(layerName);
                     if (!(await LayerDoesExist(repositoryName, digest)))
                     {
-                        await UploadLayer(hardCodedRepoName, hardCodedLayerDigest, digest, hardCodedLayerLocation, JObject.Parse(hardCodedManifest));
+                        await UploadLayer(hardCodedRepoName, hardCodedLayerDigest, digest, hardCodedLayerLocation, manifestObj);
                     }
                 }
             }
@@ -241,68 +270,6 @@ namespace DockerLib
             {
                 Log.Warning(ex, "Failed to upload layer.");
                 throw;
-            }
-        }
-
-        public async Task Demonstrate()
-        {
-            try
-            {
-                // Initialize.
-                ContainerRegistryClient client = new ContainerRegistryClient(new Uri(Config.RegistryUrl), 
-                    new DefaultAzureCredential(), 
-                    new ContainerRegistryClientOptions()
-                    {
-                        Audience = ContainerRegistryAudience.AzureResourceManagerPublicCloud,
-                    }
-                );
-
-                // Get list of all repositories contained inside defined registry.
-                AsyncPageable<string> repositoryNames = client.GetRepositoryNamesAsync();
-
-                // Compile all repository details into a defined list.
-                List<ContainerRepository> containerRepositories = new List<ContainerRepository>();
-                await foreach (string repositoryName in repositoryNames)
-                {
-                    ContainerRepository repository = client.GetRepository(repositoryName);
-                    containerRepositories.Add(repository);
-                }
-
-                // Grab a random container repo (first is fine).
-                ContainerRepository containerRepository = containerRepositories[0];
-                ContainerRegistryBlobClient blobClient = new ContainerRegistryBlobClient(new Uri(Config.RegistryUrl), new DefaultAzureCredential(), containerRepository.Name, new ContainerRegistryClientOptions()
-                {
-                    Audience = ContainerRegistryAudience.AzureResourceManagerPublicCloud,
-                });
-
-                Pageable<ArtifactManifestProperties> imageManifests = containerRepository.GetManifestPropertiesCollection(orderBy: ArtifactManifestOrderBy.LastUpdatedOnDescending);
-                foreach (ArtifactManifestProperties imageManifest in imageManifests)
-                {
-                    RegistryArtifact image = containerRepository.GetArtifact(imageManifest.Digest);
-                    var manifestResponse = blobClient.DownloadManifest(new DownloadManifestOptions(digest: imageManifest.Digest));
-                    var blobResponse = blobClient.DownloadBlob(imageManifest.Digest);
-
-                    // Download Blob to a local destination
-                    using (FileStream file = new FileStream("C:\\Users\\Evan.Klein\\Documents\\BlobFile", FileMode.Create, System.IO.FileAccess.Write))
-                    {
-                        byte[] bytes = new byte[blobResponse.Value.Content.Length];
-                        blobResponse.Value.Content.Read(bytes, 0, (int) blobResponse.Value.Content.Length);
-                        file.Write(bytes, 0, bytes.Length);
-                        blobResponse.Value.Content.Close();
-                    }
-
-                    foreach (var tagName in imageManifest.Tags)
-                    {
-                        Console.WriteLine($"{imageManifest.RepositoryName}:{tagName}");
-
-                        // image.DeleteTag(tagName); // Deletes tag.
-                    }
-                    // image.Delete(); // Delete image altogether.
-                }
-            }
-            catch (Exception ex)
-            {
-                ;
             }
         }
 
