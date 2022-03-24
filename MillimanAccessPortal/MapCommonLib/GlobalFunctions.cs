35aef051b8cf3ae34de6c4eaf5f5b0ed99bd40f9
tom.puckett <tom.puckett@milliman.com>
2022-03-24 12:22:48 -0400

    Revise and comments for HexMd5String methods

--- MillimanAccessPortal/MapCommonLib/GlobalFunctions.cs
+++ MillimanAccessPortal/MapCommonLib/GlobalFunctions.cs
@@ -205,7 +205,13 @@ namespace MapCommonLib
             return dateTime.ToString($"ddd, dd MMM yyyy hh':'mm tt', {timeZoneString}'");
         }
 
-        public static string HexMd5String(byte[] source)
+        /// <summary>
+        /// Returns an MD5 hash of the input source byte array
+        /// </summary>
+        /// <param name="source">array of bytes to be hashed</param>
+        /// <param name="forceCase">true for upper case, false for lower case, null for no intervention</param>
+        /// <returns></returns>
+        public static string HexMd5String(byte[] source, bool? forceCase = null)
         {
 #if NETSTANDARD
             byte[] hashBytes = MD5.Create().ComputeHash(source);
@@ -215,12 +221,27 @@ namespace MapCommonLib
             byte[] hashBytes = MD5.HashData(source);
             string returnVal = Convert.ToHexString(hashBytes);
 #endif
+            if (forceCase.HasValue)
+            {
+                returnVal = forceCase.Value switch
+                {
+                    true => returnVal.ToUpper(),
+                    false => returnVal.ToLower()
+                };
+            }
+
             return returnVal;
         }
 
-        public static string HexMd5String(Guid guid)
+        /// <summary>
+        /// Returns an MD5 hash of the input source byte array
+        /// </summary>
+        /// <param name="source">array of bytes to be hashed</param>
+        /// <param name="forceCase">true for upper case, false for lower case, null for no intervention</param>
+        /// <returns></returns>
+        public static string HexMd5String(Guid guid, bool? forceCase = null)
         {
-            return HexMd5String(guid.ToByteArray());    
+            return HexMd5String(guid.ToByteArray(), forceCase);    
         }
 
         /// <summary>
@@ -257,7 +278,7 @@ namespace MapCommonLib
                         }
                         break;
 
-                    case string name when name.EndsWith(".tar.gz", StringComparison.InvariantCultureIgnoreCase):
+                    case string name when name.EndsWith(".gz", StringComparison.InvariantCultureIgnoreCase):
                         using (Stream gzipStream = new GZipInputStream(rawFileStream))
                         {
                             using (TarArchive tarArchive = TarArchive.CreateInputTarArchive(gzipStream, null)) // If name encoding changes, note this in the method comment
