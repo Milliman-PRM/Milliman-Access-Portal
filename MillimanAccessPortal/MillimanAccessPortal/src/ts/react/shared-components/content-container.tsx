b69dd0b02c07c2ea5ab89d263cd5347c42ed7030
Evan Klein <Evan.Klein@Milliman.com>
2022-04-08 10:08:10 -0400

    Abide by the lint rules or perish

--- MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/content-container.tsx
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/react/shared-components/content-container.tsx
@@ -102,7 +102,9 @@ export class ContentContainer extends React.Component<ContentContainerProps, Con
         {this.props.children}
         {frame}
         {this.props.contentType === ContentTypeEnum.ContainerApp &&
-          <p className="container-note">Note: Container applications may take up to several minutes to display the preview.</p>
+          <p className="container-note">
+            Note: Container applications may take up to several minutes to display the preview.
+          </p>
         }
       </div>
     );
