f630796d41c9d5c70a776e175ebaf90cb1f5401c
Michael Reisz <michael_reisz@yahoo.com>
2022-04-11 16:10:13 -0400

    Create a new model to represent the desired resource tags

---
+++ MillimanAccessPortal/ContainerizedAppLib/AzureRestApiModels/ContainerGroupResourceTags.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ContainerizedAppLib.AzureRestApiModels
+{
+    public class ContainerGroupResourceTags
+    {
+        [JsonProperty(PropertyName = "profitCenterId")]
+        public Guid ProfitCenterId { get; set; }
+
+        [JsonProperty(PropertyName = "profitCenterName")]
+        public string ProfitCenterName { get; set; }
+
+        [JsonProperty(PropertyName = "clientId")]
+        public Guid ClientId { get; set; }
+
+        [JsonProperty(PropertyName = "clientName")]
+        public string ClientName { get; set; }
+
+        [JsonProperty(PropertyName = "contentItemId")]
+        public Guid ContentItemId { get; set; }
+
+        [JsonProperty(PropertyName = "contentItemName")]
+        public string ContentItemName { get; set; }
+
+        [JsonProperty(PropertyName = "selectionGroupId")]
+        public Guid? SelectionGroupId { get; set; }
+
+        [JsonProperty(PropertyName = "selectionGroupName")]
+        public string SelectionGroupName { get; set; }
+    }
+}
