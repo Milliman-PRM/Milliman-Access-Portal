7162cb95c3a5b7fd535cf981ac994b5ab1c18e5c
Michael Reisz <michael_reisz@yahoo.com>
2022-04-11 16:10:52 -0400

    Supply the resource tags to the call to create a container group

--- MillimanAccessPortal/ContainerizedAppLib/ContainerizedAppLibApi.cs
+++ MillimanAccessPortal/ContainerizedAppLib/ContainerizedAppLibApi.cs
@@ -473,7 +473,7 @@ namespace ContainerizedAppLib
             return "";
         }
 
-        public async Task<bool> CreateContainerGroup(string containerGroupName, string containerImageName, string ipType, int cpuCoreCount, double memorySizeInGB, string vnetId = null, string vnetName = null, params ushort[] containerPorts)
+        public async Task<bool> CreateContainerGroup(string containerGroupName, string containerImageName, string ipType, int cpuCoreCount, double memorySizeInGB, ContainerGroupResourceTags resourceTags, string vnetId = null, string vnetName = null, params ushort[] containerPorts)
         {
             string createContainerGroupEndpoint = $"https://management.azure.com/subscriptions/{Config.AciSubscriptionId}/resourceGroups/{Config.AciResourceGroupName}/providers/Microsoft.ContainerInstance/containerGroups/{containerGroupName}?api-version={Config.AciApiVersion}";
 
@@ -531,14 +531,15 @@ namespace ContainerizedAppLib
                                 Name = vnetName,
                             }
                         }
-                    }
+                    },
+                    Tags = resourceTags
                 };
 
-                string serializedRequestModel = JsonConvert.SerializeObject(requestModel);
+                string serializedRequestModel = JsonConvert.SerializeObject(requestModel, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
                 IFlurlResponse response = await createContainerGroupEndpoint
                                 .WithHeader("Authorization", $"Bearer {_aciToken}")
                                 .WithHeader("Content-Type", "application/json")
-                                .PutJsonAsync(requestModel);
+                                .PutStringAsync(serializedRequestModel);
 
                 var x = response.GetStringAsync();
 
