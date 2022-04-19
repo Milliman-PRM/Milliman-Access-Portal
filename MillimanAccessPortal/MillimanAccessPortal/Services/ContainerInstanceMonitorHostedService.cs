d0265952360854d751c048f06eb90c6f8343c5e5
Evan Klein <Evan.Klein@Milliman.com>
2022-04-19 11:33:41 -0400

    Create ContainerInstanceMonitorHostedService.cs

---
+++ MillimanAccessPortal/MillimanAccessPortal/Services/ContainerInstanceMonitorHostedService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MillimanAccessPortal.Services
+{
+    public class ContainerInstanceMonitorHostedService : BackgroundService
+    {
+        private readonly IServiceProvider _services;
+        private readonly IConfiguration _appConfig;
+
+        public ContainerInstanceMonitorHostedService(IServiceProvider servicesArg, IConfiguration appConfigArg)
+        {
+            _services = servicesArg;
+            _appConfig = appConfigArg;
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Yield();
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+
+            }
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
