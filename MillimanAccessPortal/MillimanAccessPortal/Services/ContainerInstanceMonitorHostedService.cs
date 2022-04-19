fa464e77b8211ae49ad21a84e21e012070399d49
Evan Klein <Evan.Klein@Milliman.com>
2022-04-19 14:04:16 -0400

    Start finding running container instances

--- MillimanAccessPortal/MillimanAccessPortal/Services/ContainerInstanceMonitorHostedService.cs
+++ MillimanAccessPortal/MillimanAccessPortal/Services/ContainerInstanceMonitorHostedService.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using AuditLogLib.Services;
+using ContainerizedAppLib;
+using MapDbContextLib.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,11 +15,13 @@ namespace MillimanAccessPortal.Services
     {
         private readonly IServiceProvider _services;
         private readonly IConfiguration _appConfig;
+        private readonly ContainerizedAppLibApiConfig _containerizedAppLibApiConfig;
 
-        public ContainerInstanceMonitorHostedService(IServiceProvider servicesArg, IConfiguration appConfigArg)
+        public ContainerInstanceMonitorHostedService(IServiceProvider servicesArg, IConfiguration appConfigArg, IOptions<ContainerizedAppLibApiConfig> containerizedAppLibApiConfigArg)
         {
             _services = servicesArg;
             _appConfig = appConfigArg;
+            _containerizedAppLibApiConfig = containerizedAppLibApiConfigArg.Value;
         }
 
         protected async override Task ExecuteAsync(CancellationToken stoppingToken)
@@ -27,9 +34,15 @@ namespace MillimanAccessPortal.Services
             }
         }
 
-        private async Task StopLongRunningPreviewInstances()
+        private async Task FindRunningContainerInstances() // todo change name
         {
-            
-        }
+            using (var scope = _services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                var auditLogger = scope.ServiceProvider.GetRequiredService<IAuditLogger>();
+                var containerizedAppLibApi = await new ContainerizedAppLibApi(_containerizedAppLibApiConfig).InitializeAsync(null);
+
+                var containerGroups = containerizedAppLibApi.ListContainerGroupsInResourceGroup();
+            }
     }
 }
