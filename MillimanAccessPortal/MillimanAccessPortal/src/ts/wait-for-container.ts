26c7a104ee6d0b675e55deb8054cffb4a4893bc0
Michael Reisz <michael_reisz@yahoo.com>
2022-05-24 15:36:35 -0400

    Add a typescript entrypoint for the WaitForContainer view

---
+++ MillimanAccessPortal/MillimanAccessPortal/src/ts/wait-for-container.ts
@@ -0,0 +1,24 @@
+﻿require('../scss/map.scss');
+
+const currentUrl: string = window.location.href;
+let errorCount: number = 0;
+
+// Set up a loop to poll for a redirect
+const checkContainerStatus = () => {
+  setTimeout(() => {
+    fetch(currentUrl, { method: 'GET' })
+      .then((response) => {
+        if (response.redirected) {
+          location.reload();
+        }
+        checkContainerStatus();
+      })
+      .catch((_error) => {
+        // console.log(_error);
+        errorCount++;
+      });
+  }, 5000);
+};
+
+// Initialize the polling
+checkContainerStatus();
