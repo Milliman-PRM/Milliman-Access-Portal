6a21680c6e0ff0a5daa8806f1ad6413415a21b88
Evan Klein <Evan.Klein@Milliman.com>
2022-06-03 11:33:40 -0400

    Add supporting data types

---
+++ MillimanAccessPortal/ContainerizedAppLib/AzureRestApiModels/ResourceGroup.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace ContainerizedAppLib.AzureRestApiModels
+{
+    public class ResourceGroup
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "location")]
+        public string Location { get; set; }
+
+        [JsonProperty(PropertyName = "managedBy")]
+        public string ManagedBy { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "properties")]
+        public ResourceGroupProperties Properties { get; set; }
+
+        [JsonProperty(PropertyName = "tags")]
+        public object Tags { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+    }
+}
---
+++ MillimanAccessPortal/ContainerizedAppLib/AzureRestApiModels/ResourceGroupProperties.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace ContainerizedAppLib.AzureRestApiModels
+{
+    public class ResourceGroupProperties
+    {
+        [JsonProperty(PropertyName = "provisioningState")]
+        public string ProvisioningState { get; set; }
+    }
+}
