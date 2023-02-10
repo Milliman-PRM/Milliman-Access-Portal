41a082fc97ccc7a89f469057084df26fc3631ee6
tom-puckett <75088730+tom-puckett@users.noreply.github.com>
2023-02-10 11:34:11 -0500

    Updated IpWorks component to latest 2022 edition

--- SftpServer/SftpServerLib/IpWorksSftpServer.cs
+++ SftpServer/SftpServerLib/IpWorksSftpServer.cs
@@ -136,11 +136,6 @@ namespace SftpServerLib
             //[Description("Shows the progress of the secure connection.")]
             _sftpServer.OnSSHStatus += IpWorksSftpServerEventHandlers.OnSSHStatus;
 
-            _sftpServer.ShutdownCompleted += IpWorksSftpServerEventHandlers.ShutdownCompleted;
-            _sftpServer.SetFileListCompleted += IpWorksSftpServerEventHandlers.SetFileListCompleted;
-            _sftpServer.ExchangeKeysCompleted += IpWorksSftpServerEventHandlers.ExchangeKeysCompleted;
-            _sftpServer.DisconnectCompleted += IpWorksSftpServerEventHandlers.DisconnectCompleted;
-
             Log.Verbose("SFTP Server event handlers assigned");
             #endregion
         }
